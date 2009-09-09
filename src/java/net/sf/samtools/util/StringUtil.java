dc95d1d51028572e8203b3933feb84e5bd99f069
Kathleen Tibbetts <ktibbett@gmail.com>
2009-09-09 18:20:24 +0000

    isBlank method added by Andre

--- src/java/net/sf/samtools/util/StringUtil.java
+++ src/java/net/sf/samtools/util/StringUtil.java
@@ -351,4 +351,32 @@ public class StringUtil {
         sb.reverse();
         return sb.toString();
     }
+
+    /**
+     * <p>Checks if a String is whitespace, empty ("") or null.</p>
+     *
+     * <pre>
+     * StringUtils.isBlank(null)      = true
+     * StringUtils.isBlank("")        = true
+     * StringUtils.isBlank(" ")       = true
+     * StringUtils.isBlank("sam")     = false
+     * StringUtils.isBlank("  sam  ") = false
+     * </pre>
+     *
+     * @param str  the String to check, may be null
+     * @return <code>true</code> if the String is null, empty or whitespace
+     */
+    public static boolean isBlank(String str) {
+        int strLen;
+        if (str == null || (strLen = str.length()) == 0) {
+            return true;
+        }
+        for (int i = 0; i < strLen; i++) {
+            if (!Character.isWhitespace(str.charAt(i)) ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
