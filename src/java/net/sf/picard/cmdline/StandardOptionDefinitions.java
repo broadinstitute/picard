d2d86d8f2a8bae2e326bc4e8e05b3ab5942f9f9e
Martha Borkan <mborkan@broadinstitute.org>
2009-12-28 14:54:06 +0000

    PIC-302 QualityYieldMetrics

--- src/java/net/sf/picard/cmdline/StandardOptionDefinitions.java
+++ src/java/net/sf/picard/cmdline/StandardOptionDefinitions.java
@@ -44,4 +44,5 @@ public class StandardOptionDefinitions {
     public static final String PROGRAM_RECORD_ID_SHORT_NAME = "PG";
     public static final String MINIMUM_LOD_SHORT_NAME = "LOD";
     public static final String SORT_ORDER_SHORT_NAME = "SO";
+    public static final String USE_ORIGINAL_QUALITIES_SHORT_NAME = "OQ";
 }
