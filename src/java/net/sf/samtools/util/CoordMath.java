6bf8f022f58770bbfd9cccb6685e6f394eeb7525
Matt Hanna <hanna@broadinstitute.org>
2010-06-04 22:04:56 +0000

    Merge changes in a few files forward to make sure picard private builds.

--- src/java/net/sf/samtools/util/CoordMath.java
+++ src/java/net/sf/samtools/util/CoordMath.java
@@ -29,57 +29,28 @@ package net.sf.samtools.util;
  */
 public class CoordMath {
 
-    public static long getLength(final long start, final long end) {
+    public static int getLength(final int start, final int end) {
         return (end - start) + 1;
     }
 
-    public static long getStart(final long end, final long length) {
+    public static int getStart(final int end, final int length) {
         return end - length + 1;
     }
 
-    public static long getEnd(final long start, final long length) {
+    public static int getEnd(final int start, final int length) {
         return start + length - 1;
-    }
-
-    /**
-     * Offsets are meant to exclude the 'offset' number of bases
-     */
-    public static long getStartFromOffset(final long offset, final long length) {
-        return offset + 1;
-    }
-
-    public static long getEndFromOffset(final long offset, final long length) {
-        return length - offset;
-    }
-
-    public static long getLengthFromOffsets(final long startOffset, final long endOffset, final long length) {
-        return getLength(getStartFromOffset(startOffset, length),
-                         getEndFromOffset(endOffset, length));
-    }
-
-    /**
-     * Gets a sub-sequence from a java.lang.String (which is zero based) using one based
-     * sequence coordinated.  The base at the end coordinate will be included.
-     *
-     * @param sequence The String of base pairs
-     * @param begin The one based start coordinate
-     * @param end The one based end coordinate
-     * @return The subsequence specified
-     */
-    public static String getSubsequence(final String sequence, final int begin, final int end) {
-        return sequence.substring(begin-1, end);
     }
 
     /**
      * Checks to see if the two sets of coordinates have any overlap.
      */
-    public static boolean overlaps(final long start, final long end, final long start2, final long end2) {
+    public static boolean overlaps(final int start, final int end, final int start2, final int end2) {
         return (start2 >= start && start2 <= end) || (end2 >=start && end2 <= end) ||
                 encloses(start2, end2, start, end);
     }
 
     /** Returns true if the "inner" coords and totally enclosed by the "outer" coords. */
-    public static boolean encloses(final long outerStart, final long outerEnd, final long innerStart, final long innerEnd) {
+    public static boolean encloses(final int outerStart, final int outerEnd, final int innerStart, final int innerEnd) {
         return innerStart >= outerStart && innerEnd <= outerEnd;
     }
 
@@ -87,7 +58,19 @@ public class CoordMath {
      * Determines the amount of overlap between two coordinate ranges. Assumes that the two ranges
      * actually do overlap and therefore may produce strange results when they do not!
      */
-    public static long getOverlap(final long start, final long end, final long start2, final long end2) {
+    public static int getOverlap(final int start, final int end, final int start2, final int end2) {
         return getLength(Math.max(start, start2), Math.min(end, end2));
     }
+    
+    /** 
+     * Determines the read cycle number for the base
+     * 
+     *  @param isNegativeStrand true if the read is negative strand
+     *  @param readLength
+     *  @param readBaseIndex the 0-based index of the read base in question
+     */
+    public static int getCycle(boolean isNegativeStrand, int readLength, final int readBaseIndex) {
+        return isNegativeStrand ? readLength - readBaseIndex : readBaseIndex + 1;
+    }
+    
 }
