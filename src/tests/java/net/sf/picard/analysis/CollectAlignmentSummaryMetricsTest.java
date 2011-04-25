2a4605f22b86f8fcf52a26767843830a44297817
Jonathan Burke <unknown@unknown.com>
2011-04-25 21:38:30 +0000

    Simplified CollectAlignmentSummaryMetrics.java and added PF_ALIGNED_BASES, and PF_MISMATCH_RATE to AlignedSummaryMetrics, CollectAlignmentSummaryMetrics, and the test

--- src/tests/java/net/sf/picard/analysis/CollectAlignmentSummaryMetricsTest.java
+++ src/tests/java/net/sf/picard/analysis/CollectAlignmentSummaryMetricsTest.java
@@ -46,15 +46,19 @@ public class CollectAlignmentSummaryMetricsTest {
     
     @Test
     public void test() throws IOException {
-        CollectAlignmentSummaryMetrics program = new CollectAlignmentSummaryMetrics();
-        program.INPUT = new File(TEST_DATA_DIR, "summary_alignment_stats_test.sam");
-        program.OUTPUT = File.createTempFile("alignmentMetrics", ".txt");
-        program.OUTPUT.deleteOnExit();
-        program.REFERENCE_SEQUENCE = new File(TEST_DATA_DIR, "summary_alignment_stats_test.fasta");
-        Assert.assertEquals(program.testDoWork(), 0);
+        File input = new File(TEST_DATA_DIR, "summary_alignment_stats_test.sam");
+        File reference = new File(TEST_DATA_DIR, "summary_alignment_stats_test.fasta");
+        File outfile   = File.createTempFile("alignmentMetrics", ".txt");
+        outfile.deleteOnExit();
+        int result = new CollectAlignmentSummaryMetrics().instanceMain(new String[] {
+                "INPUT="  + input.getAbsolutePath(),
+                "OUTPUT=" + outfile.getAbsolutePath(),
+                "REFERENCE_SEQUENCE=" + reference.getAbsolutePath(),
+        });
+        Assert.assertEquals(result, 0);
         
         MetricsFile<AlignmentSummaryMetrics, Comparable<?>> output = new MetricsFile<AlignmentSummaryMetrics, Comparable<?>>();
-        output.read(new FileReader(program.OUTPUT));
+        output.read(new FileReader(outfile));
         
         for (AlignmentSummaryMetrics metrics : output.getMetrics()) {
             Assert.assertEquals(metrics.MEAN_READ_LENGTH, 101.0);
@@ -66,6 +70,8 @@ public class CollectAlignmentSummaryMetricsTest {
                 Assert.assertEquals(metrics.PF_HQ_ALIGNED_READS, 3);
                 Assert.assertEquals(metrics.PF_HQ_ALIGNED_Q20_BASES, 59);
                 Assert.assertEquals(metrics.PF_HQ_MEDIAN_MISMATCHES, 19.0);
+                Assert.assertEquals(metrics.PF_ALIGNED_BASES, 303);
+                Assert.assertEquals(metrics.PF_MISMATCH_RATE, /*58D/303D*/0.191419);
                 Assert.assertEquals(metrics.BAD_CYCLES, 19);
                 break;
             case SECOND_OF_PAIR:
@@ -75,6 +81,8 @@ public class CollectAlignmentSummaryMetricsTest {
                 Assert.assertEquals(metrics.PF_HQ_ALIGNED_READS, 7);
                 Assert.assertEquals(metrics.PF_HQ_ALIGNED_Q20_BASES, 239);
                 Assert.assertEquals(metrics.PF_HQ_MEDIAN_MISMATCHES, 3.0);
+                Assert.assertEquals(metrics.PF_ALIGNED_BASES, 707);
+                Assert.assertEquals(metrics.PF_MISMATCH_RATE, /*19D/707D*/0.026874);
                 Assert.assertEquals(metrics.BAD_CYCLES, 3);
                 break;
             case PAIR:
@@ -84,6 +92,8 @@ public class CollectAlignmentSummaryMetricsTest {
                 Assert.assertEquals(metrics.PF_HQ_ALIGNED_READS, 10);
                 Assert.assertEquals(metrics.PF_HQ_ALIGNED_Q20_BASES, 298);
                 Assert.assertEquals(metrics.PF_HQ_MEDIAN_MISMATCHES, 3.0);
+                Assert.assertEquals(metrics.PF_ALIGNED_BASES, 1010);
+                Assert.assertEquals(metrics.PF_MISMATCH_RATE, /*77D/1010D*/0.076238);
                 Assert.assertEquals(metrics.BAD_CYCLES, 22);
                 break;
             case UNPAIRED:
@@ -95,20 +105,23 @@ public class CollectAlignmentSummaryMetricsTest {
 
     @Test
     public void testBisulfite() throws IOException {
-        CollectAlignmentSummaryMetrics program = new CollectAlignmentSummaryMetrics();
-        program.INPUT = new File(TEST_DATA_DIR, "summary_alignment_bisulfite_test.sam");
-        program.OUTPUT = File.createTempFile("alignmentMetrics", ".txt");
-        program.OUTPUT.deleteOnExit();
-        program.REFERENCE_SEQUENCE = new File(TEST_DATA_DIR, "summary_alignment_stats_test.fasta");
-        program.IS_BISULFITE_SEQUENCED = true;
+        File input = new File(TEST_DATA_DIR, "summary_alignment_bisulfite_test.sam");
+        File reference = new File(TEST_DATA_DIR, "summary_alignment_stats_test.fasta");
+        File outfile   = File.createTempFile("alignmentMetrics", ".txt");
+        outfile.deleteOnExit();
+        int result = new CollectAlignmentSummaryMetrics().instanceMain(new String[] {
+                "INPUT="  + input.getAbsolutePath(),
+                "OUTPUT=" + outfile.getAbsolutePath(),
+                "REFERENCE_SEQUENCE=" + reference.getAbsolutePath(),
+                "IS_BISULFITE_SEQUENCED=true"
+        });
+        Assert.assertEquals(result, 0);
+
         NumberFormat format =  NumberFormat.getInstance();
         format.setMaximumFractionDigits(4);
 
-        
-        Assert.assertEquals(program.testDoWork(), 0);
-
         MetricsFile<AlignmentSummaryMetrics, Comparable<?>> output = new MetricsFile<AlignmentSummaryMetrics, Comparable<?>>();
-        output.read(new FileReader(program.OUTPUT));
+        output.read(new FileReader(outfile));
 
         for (AlignmentSummaryMetrics metrics : output.getMetrics()) {
             Assert.assertEquals(metrics.MEAN_READ_LENGTH, 101.0);
@@ -119,6 +132,8 @@ public class CollectAlignmentSummaryMetricsTest {
                 Assert.assertEquals(metrics.PF_READS, 1);
                 Assert.assertEquals(metrics.PF_HQ_ALIGNED_BASES, 101);
                 Assert.assertEquals(metrics.PF_HQ_MEDIAN_MISMATCHES, 20.0);
+                Assert.assertEquals(metrics.PF_ALIGNED_BASES, 101);
+                Assert.assertEquals(metrics.PF_MISMATCH_RATE, 20D/100D);
                 Assert.assertEquals(metrics.BAD_CYCLES, 20);
                 Assert.assertEquals(format.format(metrics.PF_HQ_ERROR_RATE), format.format(20/(double)100));
                 break;
@@ -128,6 +143,8 @@ public class CollectAlignmentSummaryMetricsTest {
                 Assert.assertEquals(metrics.PF_READS, 1);
                 Assert.assertEquals(metrics.PF_HQ_ALIGNED_BASES, 101);
                 Assert.assertEquals(metrics.PF_HQ_MEDIAN_MISMATCHES, 3.0);
+                Assert.assertEquals(metrics.PF_ALIGNED_BASES, 101);
+                Assert.assertEquals(metrics.PF_MISMATCH_RATE, /*3D/99D*/0.030303);
                 Assert.assertEquals(metrics.BAD_CYCLES, 3);
                 Assert.assertEquals(format.format(metrics.PF_HQ_ERROR_RATE), format.format(3/(double)99));
                 break;
@@ -136,6 +153,8 @@ public class CollectAlignmentSummaryMetricsTest {
                 Assert.assertEquals(metrics.PF_READS, 2);
                 Assert.assertEquals(metrics.PF_HQ_ALIGNED_BASES, 202);
                 Assert.assertEquals(metrics.PF_HQ_MEDIAN_MISMATCHES, 3.0);
+                Assert.assertEquals(metrics.PF_ALIGNED_BASES, 202);
+                Assert.assertEquals(metrics.PF_MISMATCH_RATE, /*23D/199D*/0.115578);
                 Assert.assertEquals(metrics.BAD_CYCLES, 23);
                 Assert.assertEquals(format.format(metrics.PF_HQ_ERROR_RATE), format.format(23/(double)199));
                 break;
@@ -149,15 +168,17 @@ public class CollectAlignmentSummaryMetricsTest {
 
     @Test
     public void testNoReference() throws IOException {
-        CollectAlignmentSummaryMetrics program = new CollectAlignmentSummaryMetrics();
-        program.INPUT = new File(TEST_DATA_DIR, "summary_alignment_stats_test.sam");
-        program.OUTPUT = File.createTempFile("alignmentMetrics", ".txt");
-        program.OUTPUT.deleteOnExit();
-        program.REFERENCE_SEQUENCE = null;
-        Assert.assertEquals(program.testDoWork(), 0);
+        File input = new File(TEST_DATA_DIR, "summary_alignment_stats_test.sam");
+        File outfile   = File.createTempFile("alignmentMetrics", ".txt");
+        outfile.deleteOnExit();
+        int result = new CollectAlignmentSummaryMetrics().instanceMain(new String[] {
+                "INPUT="  + input.getAbsolutePath(),
+                "OUTPUT=" + outfile.getAbsolutePath(),
+        });
+        Assert.assertEquals(result, 0);
 
         MetricsFile<AlignmentSummaryMetrics, Comparable<?>> output = new MetricsFile<AlignmentSummaryMetrics, Comparable<?>>();
-        output.read(new FileReader(program.OUTPUT));
+        output.read(new FileReader(outfile));
 
         for (AlignmentSummaryMetrics metrics : output.getMetrics()) {
             Assert.assertEquals(metrics.MEAN_READ_LENGTH, 101.0);
@@ -169,6 +190,8 @@ public class CollectAlignmentSummaryMetricsTest {
                 Assert.assertEquals(metrics.PF_HQ_ALIGNED_READS, 0);
                 Assert.assertEquals(metrics.PF_HQ_ALIGNED_Q20_BASES, 0);
                 Assert.assertEquals(metrics.PF_HQ_MEDIAN_MISMATCHES, 0.0);
+                Assert.assertEquals(metrics.PF_ALIGNED_BASES, 0);
+                Assert.assertEquals(metrics.PF_MISMATCH_RATE, 0.0);
                 Assert.assertEquals(metrics.BAD_CYCLES, 19);
                 break;
             case SECOND_OF_PAIR:
@@ -178,6 +201,8 @@ public class CollectAlignmentSummaryMetricsTest {
                 Assert.assertEquals(metrics.PF_HQ_ALIGNED_READS, 0);
                 Assert.assertEquals(metrics.PF_HQ_ALIGNED_Q20_BASES, 0);
                 Assert.assertEquals(metrics.PF_HQ_MEDIAN_MISMATCHES, 0.0);
+                Assert.assertEquals(metrics.PF_ALIGNED_BASES, 0);
+                Assert.assertEquals(metrics.PF_MISMATCH_RATE, 0.0);
                 Assert.assertEquals(metrics.BAD_CYCLES, 3);
                 break;
             case PAIR:
@@ -187,6 +212,8 @@ public class CollectAlignmentSummaryMetricsTest {
                 Assert.assertEquals(metrics.PF_HQ_ALIGNED_READS, 0);
                 Assert.assertEquals(metrics.PF_HQ_ALIGNED_Q20_BASES, 0);
                 Assert.assertEquals(metrics.PF_HQ_MEDIAN_MISMATCHES, 0.0);
+                Assert.assertEquals(metrics.PF_ALIGNED_BASES, 0);
+                Assert.assertEquals(metrics.PF_MISMATCH_RATE, 0.0);
                 Assert.assertEquals(metrics.BAD_CYCLES, 22);
                 break;
             case UNPAIRED:
@@ -198,15 +225,17 @@ public class CollectAlignmentSummaryMetricsTest {
 
     @Test
     public void testZeroLengthReads() throws IOException {
-        CollectAlignmentSummaryMetrics program = new CollectAlignmentSummaryMetrics();
-        program.INPUT = new File(TEST_DATA_DIR, "summary_alignment_stats_test2.sam");
-        program.OUTPUT = File.createTempFile("alignmentMetrics", ".txt");
-        program.OUTPUT.deleteOnExit();
-        program.REFERENCE_SEQUENCE = null;
-        Assert.assertEquals(program.testDoWork(), 0);
+        File input = new File(TEST_DATA_DIR, "summary_alignment_stats_test2.sam");
+        File outfile   = File.createTempFile("alignmentMetrics", ".txt");
+        outfile.deleteOnExit();
+        int result = new CollectAlignmentSummaryMetrics().instanceMain(new String[] {
+                "INPUT="  + input.getAbsolutePath(),
+                "OUTPUT=" + outfile.getAbsolutePath(),
+        });
+        Assert.assertEquals(result, 0);
 
         MetricsFile<AlignmentSummaryMetrics, Comparable<?>> output = new MetricsFile<AlignmentSummaryMetrics, Comparable<?>>();
-        output.read(new FileReader(program.OUTPUT));
+        output.read(new FileReader(outfile));
         for (AlignmentSummaryMetrics metrics : output.getMetrics()) {
             // test that it doesn't blow up
         }
