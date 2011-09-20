b75735b5a817b5cf6cc33bca50d7e3a755dc6939
Alec Wysoker <alecw@broadinstitute.org>
2011-09-20 21:03:34 +0000

    Make temp directories world readable and writable.

--- src/java/net/sf/picard/cmdline/CommandLineProgram.java
+++ src/java/net/sf/picard/cmdline/CommandLineProgram.java
@@ -151,8 +151,8 @@ public abstract class CommandLineProgram {
             // Intentially not checking the return values, because it may be that the program does not
             // need a tmp_dir. If this fails, the problem will be discovered downstream.
             if (!f.exists()) f.mkdirs();
-            f.setReadable(true, true);
-            f.setWritable(true, true);
+            f.setReadable(true, false);
+            f.setWritable(true, false);
             System.setProperty("java.io.tmpdir", f.getAbsolutePath()); // in loop so that last one takes effect
         }
 
