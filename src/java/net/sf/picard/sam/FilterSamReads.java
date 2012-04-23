c611346d44d322c1566da872e51d0b46f100c4ba
Cristyn Kells <cristynkells@gmail.com>
2012-04-23 23:27:23 +0000

    changed command-line options: FILTER, READ_LIST_FILE

--- src/java/net/sf/picard/sam/FilterSamReads.java
+++ src/java/net/sf/picard/sam/FilterSamReads.java
@@ -51,44 +51,48 @@ import java.text.DecimalFormat;
 /**
  * From a SAM or BAM file, produce a new SAM or BAM by filtering aligned reads or a list of read
  * names provided in a file (one readname per line)
+ * <p/>
+ * $Id$
  */
 public class FilterSamReads extends CommandLineProgram {
 
     private static final Log log = Log.getInstance(FilterSamReads.class);
 
+    private static enum Filter {
+        includeAligned("OUTPUT SAM/BAM will contain aligned reads only. INPUT SAM/BAM must be in queryname SortOrder. (Note that *both* first and second of paired reads must be aligned to be included in the OUTPUT SAM or BAM)"),
+        excludeAligned("OUTPUT SAM/BAM will contain un-mapped reads only. INPUT SAM/BAM must be in queryname SortOrder. (Note that *both* first and second of pair must be aligned to be excluded from the OUTPUT SAM or BAM)"),
+        includeReadList("OUTPUT SAM/BAM will contain reads that are supplied in the READ_LIST_FILE file"),
+        excludeReadList("OUTPUT bam will contain reads that are *not* supplied in the READ_LIST_FILE file");
+
+        private final String description;
+
+        Filter(final String description) {
+            this.description = description;
+        }
+
+        @Override
+        public String toString() {
+            return this.name() + " [" + description + "]";
+        }
+    }
+
     @Usage
     public String USAGE =
-        "Produces a new SAM or BAM file by including or excluding aligned reads " +
-            "or a list of specific reads names from a SAM or BAM file.\n";
+            "Produces a new SAM or BAM file by including or excluding aligned reads " +
+                    "or a list of reads names supplied in the READ_LIST_FILE from the INPUT SAM or BAM file.\n";
 
-    @Option(doc = "The SAM or BAM file that will be read excluded",
+    @Option(doc = "The SAM or BAM file that will be filtered.",
         optional = false,
         shortName = StandardOptionDefinitions.INPUT_SHORT_NAME)
     public File INPUT;
 
-    @Option(doc = "Include aligned reads in the OUTPUT SAM or BAM\n" +
-        "Note that *both* first and second of paired reads must be aligned to be included in the OUTPUT SAM or BAM",
-        mutex = { "EXCLUDE_ALIGNED", "EXCLUDE_READS", "INCLUDE_READS" }, optional = false,
-        shortName = "IA")
-    public boolean INCLUDE_ALIGNED = false;
+    @Option(doc = "Filter.", optional = false)
+    public Filter FILTER = null;
 
-    @Option(doc = "Exclude aligned reads from the OUTPUT SAM or BAM\n" +
-        "Note that *both* first and second of pair must be aligned to be excluded from the OUTPUT SAM or BAM",
-        mutex = { "INCLUDE_ALIGNED", "EXCLUDE_READS", "INCLUDE_READS" }, optional = false,
-        shortName = "EA")
-    public boolean EXCLUDE_ALIGNED = false;
-
-    @Option(
-        doc = "File containing read names that will be included in the OUTPUT SAM or BAM",
-        mutex = { "EXCLUDE_READS", "INCLUDE_ALIGNED", "EXCLUDE_ALIGNED" }, optional = false,
-        shortName = "IR")
-    public File INCLUDE_READS = null;
-
-    @Option(
-        doc = "File containing read names that will be excluded from the OUTPUT SAM or BAM",
-        mutex = { "INCLUDE_READS", "INCLUDE_ALIGNED", "EXCLUDE_ALIGNED" }, optional = false,
-        shortName = "ER")
-    public File EXCLUDE_READS = null;
+    @Option(doc = "Read List File containing reads that will be included or excluded from the OUTPUT SAM or BAM file.",
+            optional = true,
+            shortName = "RLF")
+    public File READ_LIST_FILE;
 
     @Option(
         doc = "SortOrder of the OUTPUT SAM or BAM file, otherwise use the SortOrder of the INPUT file.",
@@ -105,20 +109,21 @@ public class FilterSamReads extends CommandLineProgram {
     public File OUTPUT;
 
     private void filterReads(final FilteringIterator filteringIterator) {
+
         // get OUTPUT header from INPUT and owerwrite it if necessary
         final SAMFileReader inputReader = new SAMFileReader(INPUT);
         final SAMFileHeader.SortOrder inputSortOrder = inputReader.getFileHeader().getSortOrder();
-        final SAMFileHeader outPutHeader = inputReader.getFileHeader();
+        final SAMFileHeader outputHeader = inputReader.getFileHeader();
         if (SORT_ORDER != null) {
-            outPutHeader.setSortOrder(SORT_ORDER);
+            outputHeader.setSortOrder(SORT_ORDER);
         }
-        final boolean presorted = inputSortOrder.equals(outPutHeader.getSortOrder());
+        final boolean presorted = inputSortOrder.equals(outputHeader.getSortOrder());
         log.info("Filtering [presorted=" + presorted + "] " + INPUT.getName() + " -> OUTPUT=" +
-            OUTPUT.getName() + " [sortorder=" + outPutHeader.getSortOrder().name() + "]");
+            OUTPUT.getName() + " [sortorder=" + outputHeader.getSortOrder().name() + "]");
 
         // create OUTPUT file
         final SAMFileWriter outputWriter =
-            new SAMFileWriterFactory().makeSAMOrBAMWriter(outPutHeader, presorted, OUTPUT);
+            new SAMFileWriterFactory().makeSAMOrBAMWriter(outputHeader, presorted, OUTPUT);
 
         int count = 0;
         while (filteringIterator.hasNext()) {
@@ -143,50 +148,49 @@ public class FilterSamReads extends CommandLineProgram {
      * @param samOrBamFile The SAM or BAM file for which we are going to write out a file of its
      *                     containing read names
      */
-    private void writeReadsFile(final File samOrBamFile) {
-        try {
-            final SAMFileReader reader = new SAMFileReader(samOrBamFile);
-            final File readsFile =
-                new File(OUTPUT.getParentFile(), IoUtil.basename(samOrBamFile) + ".reads");
-            IoUtil.assertFileIsWritable(readsFile);
-            final BufferedWriter bw = IoUtil.openFileForBufferedWriting(readsFile, false);
+    private void writeReadsFile(final File samOrBamFile) throws IOException {
+        final SAMFileReader reader = new SAMFileReader(samOrBamFile);
+        final File readsFile =
+            new File(OUTPUT.getParentFile(), IoUtil.basename(samOrBamFile) + ".reads");
+        IoUtil.assertFileIsWritable(readsFile);
+        final BufferedWriter bw = IoUtil.openFileForBufferedWriting(readsFile, false);
 
-            for (final SAMRecord rec : reader) {
-                bw.write(rec.toString() + "\n");
-            }
-
-            bw.close();
-            reader.close();
-            IoUtil.assertFileIsReadable(readsFile);
-        } catch (IOException e) {
-            log.error(e);
+        for (final SAMRecord rec : reader) {
+            bw.write(rec.toString() + "\n");
         }
+
+        bw.close();
+        reader.close();
+        IoUtil.assertFileIsReadable(readsFile);
     }
 
     @Override
     protected int doWork() {
-
-        if (INPUT.equals(OUTPUT)) {
-            throw new PicardException("INPUT file and OUTPUT file must differ!");
-        }
 
         try {
             IoUtil.assertFileIsReadable(INPUT);
             IoUtil.assertFileIsWritable(OUTPUT);
             if (WRITE_READS_FILES) writeReadsFile(INPUT);
 
-            if (INCLUDE_READS != null) {
-                filterReads(new FilteringIterator(new SAMFileReader(INPUT).iterator(),
-                    new ReadNameFilter(INCLUDE_READS, true)));
-            } else if (EXCLUDE_READS != null) {
-                filterReads(new FilteringIterator(new SAMFileReader(INPUT).iterator(),
-                    new ReadNameFilter(EXCLUDE_READS, false)));
-            } else if (INCLUDE_ALIGNED) {
-                filterReads(new FilteringIterator(new SAMFileReader(INPUT).iterator(),
+            switch (FILTER) {
+                case includeAligned:
+                    filterReads(new FilteringIterator(new SAMFileReader(INPUT).iterator(),
                     new AlignedFilter(true), true));
-            } else if (EXCLUDE_ALIGNED) {
-                filterReads(new FilteringIterator(new SAMFileReader(INPUT).iterator(),
+                    break;
+                case excludeAligned:
+                    filterReads(new FilteringIterator(new SAMFileReader(INPUT).iterator(),
                     new AlignedFilter(false), true));
+                    break;
+                case includeReadList:
+                    filterReads(new FilteringIterator(new SAMFileReader(INPUT).iterator(),
+                    new ReadNameFilter(READ_LIST_FILE, true)));
+                    break;
+                case excludeReadList:
+                    filterReads(new FilteringIterator(new SAMFileReader(INPUT).iterator(),
+                    new ReadNameFilter(READ_LIST_FILE, false)));
+                    break;
+                default:
+                    throw new UnsupportedOperationException(FILTER.name() + " has not been implemented!");
             }
 
             IoUtil.assertFileIsReadable(OUTPUT);
@@ -203,10 +207,26 @@ public class FilterSamReads extends CommandLineProgram {
         }
     }
 
+    @Override
+    protected String[] customCommandLineValidation() {
+        if (INPUT.equals(OUTPUT)) {
+            return new String[]{"INPUT file and OUTPUT file must differ!"};
+        }
+
+        if ((FILTER.equals(Filter.includeReadList) ||
+                FILTER.equals(Filter.excludeReadList)) &&
+                READ_LIST_FILE == null) {
+            return new String[]{"A READ_LIST_FILE must be specified when using the " + FILTER.name() + " option"};
+
+        }
+
+        return super.customCommandLineValidation();
+    }
+
     /**
      * Stock main method.
      *
-     * @param args main arguements
+     * @param args main arguments
      */
     public static void main(final String[] args) {
         System.exit(new FilterSamReads().instanceMain(args));
