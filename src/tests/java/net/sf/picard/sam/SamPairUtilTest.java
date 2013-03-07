33f3e79072e17453bd20deafcce66a61ba69279d
Alec Wysoker <alecw@broadinstitute.org>
2013-03-07 16:56:46 +0000

    Revert previous change to getPairOrientation. Improve unit tests.

--- src/tests/java/net/sf/picard/sam/SamPairUtilTest.java
+++ src/tests/java/net/sf/picard/sam/SamPairUtilTest.java
@@ -23,7 +23,9 @@
  */
 package net.sf.picard.sam;
 
+import net.sf.samtools.SAMFileHeader;
 import net.sf.samtools.SAMRecord;
+import net.sf.samtools.SAMSequenceRecord;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -32,50 +34,72 @@ import java.util.ArrayList;
 
 public class SamPairUtilTest {
 
-    @Test(dataProvider = "testGetPairOrientation")
-    public void testGetPairOrientation(final SAMRecord rec, final SamPairUtil.PairOrientation expectedOrientation)
-    throws Exception {
-        Assert.assertEquals(SamPairUtil.getPairOrientation(rec), expectedOrientation);
-        final SAMRecord negatedInsertSize = (SAMRecord)rec.clone();
-        negatedInsertSize.setInferredInsertSize(-rec.getInferredInsertSize());
-        Assert.assertEquals(SamPairUtil.getPairOrientation(negatedInsertSize), expectedOrientation);
 
+    @Test(dataProvider = "testGetPairOrientation")
+    public void testGetPairOrientation(final String testName,
+                                       final int read1Start, final int read1Length, final boolean read1Reverse,
+                                       final int read2Start, final int read2Length, final boolean read2Reverse,
+                                       final SamPairUtil.PairOrientation expectedOrientation) {
+        final SAMFileHeader header = new SAMFileHeader();
+        header.addSequence(new SAMSequenceRecord("chr1", 100000000));
+        final SAMRecord rec1 = makeSamRecord(header, read1Start, read1Length, read1Reverse, true);
+        final SAMRecord rec2 = makeSamRecord(header, read2Start, read2Length, read2Reverse, false);
+        SamPairUtil.setMateInfo(rec1, rec2, header);
+        Assert.assertEquals(SamPairUtil.getPairOrientation(rec1), expectedOrientation, testName + " first end");
+        Assert.assertEquals(SamPairUtil.getPairOrientation(rec2), expectedOrientation, testName + " second end");
+    }
+
+    private SAMRecord makeSamRecord(final SAMFileHeader header, final int alignmentStart, final int readLength,
+                                    final boolean reverse, final boolean firstOfPair) {
+        final SAMRecord rec = new SAMRecord(header);
+        rec.setReferenceIndex(0);
+        final StringBuilder sb = new StringBuilder();
+        final byte[] quals = new byte[readLength];
+        for (int i = 0; i < readLength; ++i) {
+            sb.append("A");
+            quals[i] = 20;
+        }
+        rec.setReadString(sb.toString());
+        rec.setBaseQualities(quals);
+        rec.setAlignmentStart(alignmentStart);
+        rec.setCigarString(readLength + "M");
+        rec.setReadPairedFlag(true);
+        rec.setReadNegativeStrandFlag(reverse);
+        if (firstOfPair) rec.setFirstOfPairFlag(true);
+        else rec.setSecondOfPairFlag(true);
+        return rec;
     }
 
     @DataProvider(name = "testGetPairOrientation")
-    public Object[][] testGetPairOrientationDataProvider() throws Exception{
-        final ArrayList<Object[]> ret = new ArrayList<Object[]>();
+    public Object[][] testGetPairOrientationDataProvider() {
+        /**
+         * @param testName
+         * @param read1Start
+         * @param read1Length
+         * @param read1Reverse
+         * @param read2Start
+         * @param read2Length
+         * @param read2Reverse
+         * @param expectedOrientation
+         */
+        return new Object[][] {
+                {"normal innie", 1, 100, false, 500, 100, true, SamPairUtil.PairOrientation.FR},
+                {"overlapping innie", 1, 100, false, 50, 100, true, SamPairUtil.PairOrientation.FR},
+                {"second end enclosed innie", 1, 100, false, 50, 50, true, SamPairUtil.PairOrientation.FR},
+                {"first end enclosed innie", 1, 50, false, 1, 100, true, SamPairUtil.PairOrientation.FR},
+                {"completely overlapping innie", 1, 100, false, 1, 100, true, SamPairUtil.PairOrientation.FR},
 
-        SAMRecord rec = new SAMRecord(null);
-        rec.setReadString("CTGTGCAGAGACAATACGGCTGGCCCGCACTGTGAGAAGTGCAGTGATGGGTACTATGGAGATTCAACTGCAGGCACCTCCTCCGATTGCCAACCCTGTC");
-        rec.setBaseQualities(new byte[rec.getReadLength()]);
-        rec.setReadPairedFlag(true);
-        rec.setProperPairFlag(true);
-        rec.setFirstOfPairFlag(true);
-        rec.setReadNegativeStrandFlag(true);
-        rec.setCigarString("3S97M");
-        rec.setReferenceName("chr1");
-        rec.setAlignmentStart(200);
-        rec.setMateReferenceName("chr1");
-        rec.setMateAlignmentStart(200);
-        rec.setInferredInsertSize(-97);
-        ret.add(new Object[]{rec, SamPairUtil.PairOrientation.FR});
+                {"normal outie", 1, 100, true, 500, 100, false, SamPairUtil.PairOrientation.RF},
+                {"nojump outie", 1, 100, true, 101, 100, false, SamPairUtil.PairOrientation.RF},
 
-        rec = new SAMRecord(null);
-        rec.setReadString("GTGCAGAGACAATACGGCTGGGCCGCACTGTGAGAAGTGCAG");
-        rec.setBaseQualities(new byte[rec.getReadLength()]);
-        rec.setReadPairedFlag(true);
-        rec.setProperPairFlag(true);
-        rec.setSecondOfPairFlag(true);
-        rec.setMateNegativeStrandFlag(true);
-        rec.setCigarString("1S39M2S");
-        rec.setReferenceName("chr1");
-        rec.setAlignmentStart(200);
-        rec.setMateReferenceName("chr1");
-        rec.setMateAlignmentStart(200);
-        rec.setInferredInsertSize(97);
-        ret.add(new Object[]{rec, SamPairUtil.PairOrientation.FR});
-
-        return ret.toArray(new Object[0][]);
+                {"forward tandem", 1, 100, true, 500, 100, true, SamPairUtil.PairOrientation.TANDEM},
+                {"reverse tandem", 1, 100, false, 500, 100, false, SamPairUtil.PairOrientation.TANDEM},
+                {"overlapping forward tandem", 1, 100, true, 50, 100, true, SamPairUtil.PairOrientation.TANDEM},
+                {"overlapping reverse tandem", 1, 100, false, 50, 100, false, SamPairUtil.PairOrientation.TANDEM},
+                {"second end enclosed forward tandem", 1, 100, true, 50, 50, true, SamPairUtil.PairOrientation.TANDEM},
+                {"second end enclosed reverse tandem", 1, 100, false, 50, 50, false, SamPairUtil.PairOrientation.TANDEM},
+                {"first end enclosed forward tandem", 1, 50, true, 1, 100, true, SamPairUtil.PairOrientation.TANDEM},
+                {"first end enclosed reverse tandem", 1, 50, false, 1, 100, false, SamPairUtil.PairOrientation.TANDEM},
+        };
     }
 }
