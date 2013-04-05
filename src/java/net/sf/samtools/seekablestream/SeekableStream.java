1f634d050f9c278d111c1a9875fe4f0a5eeb594f
Jacob Silterra <jacob@broadinstitute.org>
2013-04-05 20:44:50 +0000

    Remove redundant SeekableStream class
    Add "readFully" method back to SeekableStream class, much in IGV depended on it.
    Add registration and getting of URLHelper to ParsingUtils

--- src/java/net/sf/samtools/seekablestream/SeekableStream.java
+++ src/java/net/sf/samtools/seekablestream/SeekableStream.java
@@ -1,19 +1,29 @@
 /*
- * The Broad Institute
- * SOFTWARE COPYRIGHT NOTICE AGREEMENT
- * This is copyright (2007-2009) by the Broad Institute/Massachusetts Institute
- * of Technology.  It is licensed to You under the Gnu Public License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- *  the License.  You may obtain a copy of the License at
+ * The MIT License
  *
- *    http://www.opensource.org/licenses/gpl-2.0.php
+ * Copyright (c) 2013 The Broad Institute
  *
- * This software is supplied without any warranty or guaranteed support
- * whatsoever. Neither the Broad Institute nor MIT can be responsible for its
- * use, misuse, or functionality.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package net.sf.samtools.seekablestream;
 
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -37,19 +47,23 @@ public abstract class SeekableStream extends InputStream {
     public abstract String getSource();
 
     /**
-     * Not sure why this is here, or what uses it
+     * Read enough bytes to fill the input buffer.
+     * @param b
+     * @throws EOFException If EOF is reached before buffer is filled
      */
-//    public void readFully(byte b[]) throws IOException {
-//        int len = b.length;
-//        if (len < 0)
-//            throw new IndexOutOfBoundsException();
-//        int n = 0;
-//        while (n < len) {
-//            int count = read(b, n, len - n);
-//            if (count < 0)
-//                throw new EOFException();
-//            n += count;
-//        }
-//    }
+    public void readFully(byte b[]) throws IOException {
+        int len = b.length;
+        if (len < 0){
+            throw new IndexOutOfBoundsException();
+        }
+        int n = 0;
+        while (n < len) {
+            int count = read(b, n, len - n);
+            if (count < 0){
+                throw new EOFException();
+            }
+            n += count;
+        }
+    }
 
 }
