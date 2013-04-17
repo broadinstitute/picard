1f1713ab35a1f35213bcbb2740840469e122c458
Alec Wysoker <alecw@broadinstitute.org>
2013-04-17 16:32:17 +0000

    Replace PicardException with SamException because net.sf.samtools code cannot reference net.sf.picard code.

--- src/java/net/sf/samtools/util/TestUtil.java
+++ src/java/net/sf/samtools/util/TestUtil.java
@@ -21,31 +21,9 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package net.sf.samtools.util;/*
- * The MIT License
- *
- * Copyright (c) 2010 The Broad Institute
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+package net.sf.samtools.util;
 
-import net.sf.picard.PicardException;
+import net.sf.samtools.SAMException;
 
 import java.io.File;
 import java.io.IOException;
@@ -57,12 +35,12 @@ public class TestUtil {
         try {
             tempDirectory = File.createTempFile(prefix, suffix);
         } catch (IOException e) {
-            throw new PicardException("Failed to create temporary file.", e);
+            throw new SAMException("Failed to create temporary file.", e);
         }
         if (!tempDirectory.delete())
-            throw new PicardException("Failed to delete file: " + tempDirectory);
+            throw new SAMException("Failed to delete file: " + tempDirectory);
         if (!tempDirectory.mkdir())
-            throw new PicardException("Failed to make directory: " + tempDirectory);
+            throw new SAMException("Failed to make directory: " + tempDirectory);
         tempDirectory.deleteOnExit();
         return tempDirectory;
     }
