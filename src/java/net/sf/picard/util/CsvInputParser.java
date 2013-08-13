7368dd835cac2b86cac148599e078718b608a9a4
Jeff Gentry <geoffjentry@gmail.com>
2013-08-13 18:10:40 +0000

    Added DelimitedTextFileWithHeaderParser, an attempt to transition toward a replacement for TabbedTextFileWithHeaderParser with more flexible delimiter possibilities. Currently nearly a line for line copy of TabbedTextFileWithHeaderParser although the intention is to transition the latter to be a wrapper around the former.

---
+++ src/java/net/sf/picard/util/CsvInputParser.java
@@ -0,0 +1,35 @@
+package net.sf.picard.util;
+
+import java.io.File;
+import java.io.InputStream;
+
+public class CsvInputParser extends BasicInputParser {
+    /**
+     * Constructor
+     *
+     * @param stream  The input stream(s) to parse
+     */
+    public CsvInputParser(final boolean treatGroupedDelimitersAsOne, final InputStream... stream) {
+        super(treatGroupedDelimitersAsOne, stream);
+    }
+
+    /**
+     * Constructor
+     *
+     * @param file  The file(s) to parse
+     */
+    public CsvInputParser(final boolean treatGroupedDelimitersAsOne, final File... file) {
+        super(treatGroupedDelimitersAsOne, file);
+    }
+
+    /**
+     * Determines whether a given character is a delimiter
+     *
+     * @param b the character to evaluate
+     * @return  true if <code>b</code> is a delimiter; otherwise false
+     */
+    protected boolean isDelimiter(byte b) {
+        return b == ',';
+    }
+
+}
