6a41609c987177c04282ba9bf3da89365da58155
Tim Fennell <tfenne@tfenne.com>
2013-10-17 10:52:23 +0000

    Tided up a little bit and then added a method to be able to query by region from the VCFFileReader.

--- src/java/org/broadinstitute/variant/vcf/VCFFileReader.java
+++ src/java/org/broadinstitute/variant/vcf/VCFFileReader.java
@@ -13,7 +13,10 @@ import java.io.Closeable;
 import java.io.File;
 import java.io.IOException;
 
-public class VCFFileReader implements Closeable {
+/**
+ * Simplified interface for reading from VCF/BCF files.
+ */
+public class VCFFileReader implements Closeable, Iterable<VariantContext> {
 
 	private final FeatureReader<VariantContext> reader;
 
@@ -33,37 +36,44 @@ public class VCFFileReader implements Closeable {
 		return dict;
 	}
 
+    /** Constructs a VCFFileReader that requires the index to be present. */
 	public VCFFileReader(final File file) {
 		this(file, true);
 	}
 
-	public VCFFileReader(final File file, boolean requireIndex) {
-		this.reader =
-				AbstractFeatureReader.getFeatureReader(
+    /** Allows construction of a VCFFileReader that will or will not assert the presence of an index as desired. */
+	public VCFFileReader(final File file, final boolean requireIndex) {
+		this.reader = AbstractFeatureReader.getFeatureReader(
 						file.getAbsolutePath(),
-						isBCF(file)
-								? new BCF2Codec()
-								: new VCFCodec(),
+						isBCF(file) ? new BCF2Codec() : new VCFCodec(),
 						requireIndex);
 	}
 
+    /** Returns the VCFHeader associated with this VCF/BCF file. */
 	public VCFHeader getFileHeader() {
 		return (VCFHeader) reader.getHeader();
 	}
 
+    /** Returns an iterator over all records in this VCF/BCF file. */
 	public CloseableIterator<VariantContext> iterator() {
-		try {
-			return reader.iterator();
-		} catch (final IOException ioe) {
-			throw new TribbleException("Could not create an iterator from a feature reader: " + ioe.getMessage(), ioe);
+		try { return reader.iterator(); }
+        catch (final IOException ioe) {
+			throw new TribbleException("Could not create an iterator from a feature reader.", ioe);
 		}
 	}
 
+    /** Queries for records within the region specified. */
+    public CloseableIterator<VariantContext> query(final String chrom, final int start, final int end) {
+        try { return reader.query(chrom, start, end); }
+        catch (final IOException ioe) {
+            throw new TribbleException("Could not create an iterator from a feature reader.", ioe);
+        }
+    }
+
 	public void close() {
-		try {
-			this.reader.close();
-		} catch (final IOException ioe) {
-			throw new TribbleException("Could not close a variant context feature reader: " + ioe.getMessage(), ioe);
+		try { this.reader.close(); }
+        catch (final IOException ioe) {
+			throw new TribbleException("Could not close a variant context feature reader.", ioe);
 		}
 	}
 }
