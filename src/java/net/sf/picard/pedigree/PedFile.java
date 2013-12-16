875e10b78aa5052b738e31db3ede0fd15cdb9ff3
Jeff Gentry <geoffjentry@gmail.com>
2013-12-16 17:39:41 +0000

    Conditionally allow for tabs in ped file fields

--- src/java/net/sf/picard/pedigree/PedFile.java
+++ src/java/net/sf/picard/pedigree/PedFile.java
@@ -18,9 +18,21 @@ import java.util.regex.Pattern;
  *
  * Stores the information in memory as a map of individualId -> Pedigree information for that individual
  */
-public class PedFile extends TreeMap<String,PedTrio> {
+public class PedFile extends TreeMap<String, PedFile.PedTrio> {
     private static final Log log = Log.getInstance(PedFile.class);
     static final Pattern WHITESPACE = Pattern.compile("\\s+");
+    static final Pattern TAB = Pattern.compile("\\t");
+    private final Pattern delimiterPattern;
+    private final String delimiterString; // A textual representation of the delimiter, for output purposes
+
+    // These two are really for PedTrio, but they can't be static in there and need to be accessed outside of PedFile
+    public static final Number NO_PHENO = new Integer(-9);
+    public static final Sex UNKNOWN_SEX = Sex.Unknown;
+
+    public PedFile(final boolean isTabMode) {
+        delimiterPattern = isTabMode ? TAB : WHITESPACE;
+        delimiterString = isTabMode ? "tabs" : "whitespace";
+    }
 
     /** Adds a trio to the PedFile keyed by the individual id. */
     public void add(final PedTrio trio) {
@@ -52,7 +64,7 @@ public class PedFile extends TreeMap<String,PedTrio> {
 
             out.close();
         }
-        catch (IOException ioe) {
+        catch (final IOException ioe) {
             throw new RuntimeIOException("IOException while writing to file " + file.getAbsolutePath(), ioe);
         }
     }
@@ -60,28 +72,28 @@ public class PedFile extends TreeMap<String,PedTrio> {
     /**
      * Attempts to read a pedigree file into memory.
      */
-    public static PedFile fromFile(final File file) {
-        final PedFile pedfile = new PedFile();
+    public static PedFile fromFile(final File file, final boolean isTabMode) {
+        final PedFile pedFile = new PedFile(isTabMode);
 
         IoUtil.assertFileIsReadable(file);
         for (final String line : IoUtil.readLines(file)) {
-            final String[] fields = WHITESPACE.split(line);
+            final String[] fields = pedFile.delimiterPattern.split(line);
             if (fields.length != 6) {
                 log.error("Ped file line contained invalid number of fields, skipping: " + line);
                 continue;
             }
 
-            final PedTrio trio = new PedTrio(fields[0],
-                                             fields[1],
-                                             fields[2],
-                                             fields[3],
-                                             Sex.fromCode(Integer.parseInt(fields[4])),
-                                             fields[5].contains(".") ? Double.parseDouble(fields[5]) : Integer.parseInt(fields[5])
-                                            );
-            pedfile.add(trio);
+            final PedTrio trio = pedFile.new PedTrio(fields[0],
+                    fields[1],
+                    fields[2],
+                    fields[3],
+                    Sex.fromCode(Integer.parseInt(fields[4])),
+                    fields[5].contains(".") ? Double.parseDouble(fields[5]) : Integer.parseInt(fields[5])
+            );
+            pedFile.add(trio);
         }
 
-        return pedfile;
+        return pedFile;
     }
 
     /**
@@ -95,5 +107,41 @@ public class PedFile extends TreeMap<String,PedTrio> {
         }
 
         return this;
+    }
+
+    public class PedTrio {
+        private final String familyId;
+        private final String individualId;
+        private final String paternalId;
+        private final String maternalId;
+        private final Sex sex;
+        private final Number phenotype;
+
+        /** Constructs a TRIO that cannot be modified after the fact. */
+        public PedTrio(final String familyId, final String individualId, final String paternalId, final String maternalId, final Sex sex, final Number phenotype) {
+            if (delimiterPattern.split(familyId).length != 1)     throw new IllegalArgumentException("FamilyID     cannot contain " + delimiterString + ": [" + familyId     + "]");
+            if (delimiterPattern.split(individualId).length != 1) throw new IllegalArgumentException("IndividualID cannot contain " + delimiterString + ": [" + individualId + "]");
+            if (delimiterPattern.split(paternalId).length != 1)   throw new IllegalArgumentException("PaternalID   cannot contain " + delimiterString + ": [" + paternalId   + "]");
+            if (delimiterPattern.split(maternalId).length != 1)   throw new IllegalArgumentException("MaternalID   cannot contain " + delimiterString + ": [" + maternalId   + "]");
+
+            this.familyId = familyId;
+            this.individualId = individualId;
+            this.paternalId = paternalId;
+            this.maternalId = maternalId;
+            this.sex = sex;
+            this.phenotype = phenotype;
+        }
+
+        /** True if this record has paternal and maternal ids, otherwise false. */
+        public boolean hasBothParents() {
+            return this.paternalId != null && this.maternalId != null;
+        }
+
+        public String getFamilyId() { return familyId; }
+        public String getIndividualId() { return individualId; }
+        public String getPaternalId() { return paternalId; }
+        public String getMaternalId() { return maternalId; }
+        public Sex getSex() { return sex; }
+        public Number getPhenotype() { return phenotype; }
     }
 }
