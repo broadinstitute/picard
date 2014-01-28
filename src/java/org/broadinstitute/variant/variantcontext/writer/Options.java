7252024e27e511b54df7d97bd788875736d16469
George Grant <ggrant@gmail.com>
2014-01-28 21:28:18 +0000

    Enable async I/O for writing bgzipped VCFs

--- src/java/org/broadinstitute/variant/variantcontext/writer/Options.java
+++ src/java/org/broadinstitute/variant/variantcontext/writer/Options.java
@@ -35,5 +35,6 @@ public enum Options {
     INDEX_ON_THE_FLY,
     DO_NOT_WRITE_GENOTYPES,
     ALLOW_MISSING_FIELDS_IN_HEADER,
-    FORCE_BCF
+    FORCE_BCF,
+    USE_ASYNC_IO            // Turn on or off the use of asynchronous IO for writing output VCF files.
 }
