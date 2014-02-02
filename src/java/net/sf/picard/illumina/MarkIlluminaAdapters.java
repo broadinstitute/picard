0614f37d0ca2b498a2f783f2323722b3efee8967
Tim Fennell <tfenne@tfenne.com>
2014-02-02 21:14:41 +0000

    Improved MarkIlluminaAdapters to handle files with mixes of paired-end and single-end reads, clip for multiple different adapter sequences in one pass and correct progress logging and metrics that were only counting one read out of each pair.

--- src/java/net/sf/picard/illumina/MarkIlluminaAdapters.java
+++ src/java/net/sf/picard/illumina/MarkIlluminaAdapters.java
@@ -34,11 +34,14 @@ import net.sf.picard.metrics.MetricsFile;
 import net.sf.picard.sam.ReservedTagConstants;
 import net.sf.picard.util.*;
 import net.sf.samtools.*;
+import net.sf.samtools.util.CollectionUtil;
 import net.sf.samtools.util.SequenceUtil;
 import net.sf.samtools.util.StringUtil;
+import static net.sf.picard.util.IlluminaUtil.IlluminaAdapterPair;
 
 import java.io.File;
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Command line program to mark the location of adapter sequences.
@@ -58,53 +61,45 @@ public class MarkIlluminaAdapters extends CommandLineProgram {
 
     @Option(shortName=StandardOptionDefinitions.INPUT_SHORT_NAME)
     public File INPUT;
+
     @Option(doc="If output is not specified, just the metrics are generated",
             shortName=StandardOptionDefinitions.OUTPUT_SHORT_NAME, optional=true)
     public File OUTPUT;
+
     @Option(doc="Histogram showing counts of bases_clipped in how many reads", shortName="M")
     public File METRICS;
-    @Option(doc="The minimum number of bases that must match the adapter that will be clipped. Defaults to " +
-            ClippingUtility.MIN_MATCH_PE_BASES + " if paired-end, otherwise" + ClippingUtility.MIN_MATCH_BASES +
-            "/nThe stricter match used when matching 2 reads will be twice this.",
-            optional=true)
-    public Integer MIN_MATCH_BASES;
-    @Option(doc="The percentage of errors allowed when matching the adapter sequence. Defaults to " +
-            ClippingUtility.MAX_PE_ERROR_RATE + " if paired-end, otherwise " + ClippingUtility.MAX_ERROR_RATE,
-            optional=true)
-    public Double MAX_ERROR_RATE;
-    @Option(doc="Whether this is a paired-end run. ", shortName="PE")
-    public Boolean PAIRED_RUN;
-    @Option(doc="Which adapters to use, PAIRED_END, INDEXED, or SINGLE_END",
-            mutex={"FIVE_PRIME_ADAPTER", "THREE_PRIME_ADAPTER"})
-    // this probably only makes sense for paired_run where you need to specify either PAIRED_END or INDEXED?
-    //                         or for non-paired_run where you need to specify either SINGLE_END or INDEXED?
-    // but we won't enforce this.
-    public IlluminaUtil.IlluminaAdapterPair ADAPTERS;
 
-    @Option(doc="For specifying adapters other than standard Illumina", mutex = {"ADAPTERS"})
+    @Option(doc="The minimum number of bases to match over when clipping single-end reads.")
+    public int MIN_MATCH_BASES_SE = ClippingUtility.MIN_MATCH_BASES;
+
+    @Option(doc="The minimum number of bases to match over (per-read) when clipping paired-end reads.")
+    public int MIN_MATCH_BASES_PE = ClippingUtility.MIN_MATCH_PE_BASES;
+
+    @Option(doc="The maximum mismatch error rate to tolerate when clipping single-end reads.")
+    public double MAX_ERROR_RATE_SE = ClippingUtility.MAX_ERROR_RATE;
+
+    @Option(doc="The maximum mismatch error rate to tolerate when clipping paired-end reads.")
+    public double MAX_ERROR_RATE_PE = ClippingUtility.MAX_PE_ERROR_RATE;
+
+    @Option(doc="DEPRECATED. Whether this is a paired-end run. No longer used.", shortName="PE", optional=true)
+    public Boolean PAIRED_RUN;
+
+    @Option(doc="Which adapters sequences to attempt to identify and clip.")
+    public List<IlluminaAdapterPair> ADAPTERS =
+            CollectionUtil.makeList(IlluminaAdapterPair.INDEXED,
+                    IlluminaAdapterPair.DUAL_INDEXED,
+                    IlluminaAdapterPair.PAIRED_END
+                    );
+
+    @Option(doc="For specifying adapters other than standard Illumina", optional=true)
     public String FIVE_PRIME_ADAPTER;
-    @Option(doc="For specifying adapters other than standard Illumina", mutex = {"ADAPTERS"})
+    @Option(doc="For specifying adapters other than standard Illumina", optional=true)
     public String THREE_PRIME_ADAPTER;
 
     private static final Log log = Log.getInstance(MarkIlluminaAdapters.class);
 
-    @Override
-    protected String[] customCommandLineValidation() {
-        // set default thresholds based on what kind of run
-        if (PAIRED_RUN){
-            if (MIN_MATCH_BASES == null) MIN_MATCH_BASES = ClippingUtility.MIN_MATCH_PE_BASES;
-            if (MAX_ERROR_RATE == null) MAX_ERROR_RATE = ClippingUtility.MAX_PE_ERROR_RATE;
-            // For paired runs, you may actually want to specify all 4 thresholds
-            // so the stricter test when mismatch can be controlled.
-            // We'll assume that the stricter test will be twice the min_match_bases
-        } else {
-            if (MIN_MATCH_BASES == null) MIN_MATCH_BASES = ClippingUtility.MIN_MATCH_BASES;
-            if (MAX_ERROR_RATE == null) MAX_ERROR_RATE = ClippingUtility.MAX_ERROR_RATE;
-        }
-        return null;
-    }
-
-    public static void main(String[] args) {
+    // Stock main method
+    public static void main(final String[] args) {
         System.exit(new MarkIlluminaAdapters().instanceMain(args));
     }
 
@@ -113,94 +108,91 @@ public class MarkIlluminaAdapters extends CommandLineProgram {
         IoUtil.assertFileIsReadable(INPUT);
         IoUtil.assertFileIsWritable(METRICS);
 
-        SAMFileReader in = new SAMFileReader(INPUT);
+        final SAMFileReader in = new SAMFileReader(INPUT);
+        final SAMFileHeader.SortOrder order = in.getFileHeader().getSortOrder();
         SAMFileWriter out = null;
         if (OUTPUT != null) {
             IoUtil.assertFileIsWritable(OUTPUT);
             out = new SAMFileWriterFactory().makeSAMOrBAMWriter(in.getFileHeader(), true, OUTPUT);
         }
 
-        Histogram<Integer> histo = new Histogram<Integer>("clipped_bases", "read_count");
+        final Histogram<Integer> histo = new Histogram<Integer>("clipped_bases", "read_count");
 
-        // check sort order in the header - must be queryName for paired end runs
-        if (PAIRED_RUN && !in.getFileHeader().getSortOrder().equals(SAMFileHeader.SortOrder.queryname)) {
-            throw new PicardException("Input BAM file must be sorted by queryname");
+        // Combine any adapters and custom adapter pairs from the command line into an array for use in clipping
+        final AdapterPair[] adapters;
+        {
+            final List<AdapterPair> tmp = new ArrayList<AdapterPair>();
+            tmp.addAll(ADAPTERS);
+            if (FIVE_PRIME_ADAPTER != null && THREE_PRIME_ADAPTER != null) {
+                tmp.add(new CustomAdapterPair(FIVE_PRIME_ADAPTER, THREE_PRIME_ADAPTER));
+            }
+            adapters = tmp.toArray(new AdapterPair[tmp.size()]);
         }
 
-        final AdapterPair adapters;
-        if (ADAPTERS != null) {
-            adapters = ADAPTERS;
-        } else {
-            adapters = new CustomAdapterPair(FIVE_PRIME_ADAPTER, THREE_PRIME_ADAPTER);
-        }
-        // The following loop is roughly the same as "for (SAMRecord rec : in){"
+        ////////////////////////////////////////////////////////////////////////
+        // Main loop that consumes reads, clips them and writes them to the output
+        ////////////////////////////////////////////////////////////////////////
         final ProgressLogger progress = new ProgressLogger(log, 1000000, "Read");
-        for (Iterator<SAMRecord> iter = in.iterator(); iter.hasNext();) {
-            SAMRecord rec = iter.next();
+        final SAMRecordIterator iterator = in.iterator();
 
-            //  clear any existing trim on rec
+        while (iterator.hasNext()) {
+            final SAMRecord rec = iterator.next();
+            final SAMRecord rec2 = rec.getReadPairedFlag() && iterator.hasNext() ? iterator.next() : null;
             rec.setAttribute(ReservedTagConstants.XT, null);
 
-            SAMRecord rec2 = null;
-            if (PAIRED_RUN) {
-                if (rec.getFirstOfPairFlag() || rec.getSecondOfPairFlag()) {
-                    // the secondOfPair should be the next record
-                    rec2 = iter.hasNext() ? iter.next() : null;
-                    if (rec2 == null) {
-                        throw new PicardException("Missing second read for " + rec);
-                    }
-
-                    // clear any existing trim on rec2
-                    rec2.setAttribute(ReservedTagConstants.XT, null);
-                    if (!rec.getReadName().equals(rec2.getReadName())){
-                        throw new PicardException("read names of two paired reads differs : " +
-                                rec.getReadName() + ", " + rec2.getReadName());
-                    }
-
-                    // establish which of pair is first and which second
-                    SAMRecord firstRead;
-                    SAMRecord secondRead;
-                    if (rec.getFirstOfPairFlag()){
-                        firstRead = rec;
-                        secondRead = rec2;
-                    } else {
-                        firstRead = rec2;
-                        secondRead = rec;
-                    }
-                    if (!firstRead.getFirstOfPairFlag()){
-                        throw new PicardException("first of two reads doesn't have getFirstOfPairFlag()");
-                    }
-                    if (!secondRead.getSecondOfPairFlag()){
-                        throw new PicardException("second of two reads doesn't have getSecondOfPairFlag()");
-                    }
-
-                    String warnString = ClippingUtility.adapterTrimIlluminaPairedReads(firstRead, secondRead,
-                            adapters, MIN_MATCH_BASES, MAX_ERROR_RATE);
-                    if (warnString != null) {
-                        log.info("Adapter trimming " + warnString);
-                    }
-                } else {
-                    throw new PicardException("Non-paired reads in a paired run " + rec);
+            // Do the clipping one way for PE and another for SE reads
+            if (rec.getReadPairedFlag()) {
+                // Assert that the input file is in query name order only if we see some PE reads
+                if (order != SAMFileHeader.SortOrder.queryname) {
+                    throw new PicardException("Input BAM file must be sorted by queryname");
                 }
-            } else { // not a paired run
-                ClippingUtility.adapterTrimIlluminaSingleRead(rec,
-                        adapters, MIN_MATCH_BASES, MAX_ERROR_RATE);
+
+                if (rec2 == null) throw new PicardException("Missing mate pair for paired read: " + rec.getReadName());
+                rec2.setAttribute(ReservedTagConstants.XT, null);
+
+                // Assert that we did in fact just get two mate pairs
+                if (!rec.getReadName().equals(rec2.getReadName())){
+                    throw new PicardException("Adjacent reads expected to be mate-pairs have different names: " +
+                            rec.getReadName() + ", " + rec2.getReadName());
+                }
+
+                // establish which of pair is first and which second
+                final SAMRecord first, second;
+
+                if (rec.getFirstOfPairFlag() && rec2.getSecondOfPairFlag()){
+                    first = rec;
+                    second = rec2;
+                }
+                else if (rec.getSecondOfPairFlag() && rec2.getFirstOfPairFlag()) {
+                    first = rec2;
+                    second = rec;
+                }
+                else {
+                    throw new PicardException("Two reads with same name but not correctly marked as 1st/2nd of pair: " + rec.getReadName());
+                }
+
+                ClippingUtility.adapterTrimIlluminaPairedReads(first, second, MIN_MATCH_BASES_PE, MAX_ERROR_RATE_PE, adapters);
+            }
+            else {
+                ClippingUtility.adapterTrimIlluminaSingleRead(rec, MIN_MATCH_BASES_SE, MAX_ERROR_RATE_SE, adapters);
             }
 
-            if (out != null) out.addAlignment(rec);
-            if (out != null && rec2 != null) out.addAlignment(rec2);
+            // Then output the records, update progress and metrics
+            for (final SAMRecord r : new SAMRecord[] {rec, rec2}) {
+                if (r != null) {
+                    progress.record(r);
+                    if (out != null) out.addAlignment(r);
 
-            Integer trimPoint = rec.getIntegerAttribute(ReservedTagConstants.XT);
-            if (trimPoint != null) {
-                histo.increment(rec.getReadLength() - trimPoint + 1);
+                    final Integer clip = rec.getIntegerAttribute(ReservedTagConstants.XT);
+                    if (clip != null) histo.increment(rec.getReadLength() - clip + 1);
+                }
             }
-
-            progress.record(rec);
         }
 
         if (out != null) out.close();
 
-        MetricsFile<?,Integer> metricsFile = getMetricsFile();
+        // Lastly output the metrics to file
+        final MetricsFile<?,Integer> metricsFile = getMetricsFile();
         metricsFile.setHistogram(histo);
         metricsFile.write(METRICS);
 
