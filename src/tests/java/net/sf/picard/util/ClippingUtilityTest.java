d1062d4732fc792336353c2a69ef8f38e0021190
Alec Wysoker <alecw@broadinstitute.org>
2014-03-26 19:46:56 +0000

    Add some more tests.

--- src/tests/java/net/sf/picard/util/ClippingUtilityTest.java
+++ src/tests/java/net/sf/picard/util/ClippingUtilityTest.java
@@ -23,13 +23,18 @@
  */
 package net.sf.picard.util;
 
+import net.sf.picard.sam.ReservedTagConstants;
 import net.sf.picard.util.IlluminaUtil.IlluminaAdapterPair;
 import net.sf.samtools.SAMFileHeader;
 import net.sf.samtools.SAMRecord;
-import org.testng.annotations.Test;
-import org.testng.annotations.DataProvider;
-import org.testng.Assert;
+import net.sf.samtools.util.SequenceUtil;
 import net.sf.samtools.util.StringUtil;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -43,7 +48,55 @@ public class ClippingUtilityTest {
 
        final int result = ClippingUtility.findIndexOfClipSequence(r, c, minMatch, errRate);
        Assert.assertEquals(result, expected, testName);
+
    }
+
+
+
+    @Test(dataProvider = "clipTestData")
+    public void testSingleEndSamRecordClip(final String testName, final String read, final String clip, final int minMatch,
+                                           final double errRate, final int expected) {
+        if (read == null) return; // Silly case
+
+        final SingleEndAdapter adapter = new SingleEndAdapter(testName, clip);
+
+        for (final boolean reverse : new boolean[]{false, true}) {
+            final SAMRecord rec = new SAMRecord(null);
+            if (reverse) {
+                rec.setReadString(SequenceUtil.reverseComplement(read));
+                rec.setReadNegativeStrandFlag(true);
+            } else {
+                rec.setReadString(read);
+            }
+
+            final AdapterPair matchedAdapter = ClippingUtility.adapterTrimIlluminaSingleRead(rec, minMatch, errRate, adapter);
+            if (expected == -1) {
+                Assert.assertNull(matchedAdapter, testName);
+                Assert.assertNull(rec.getAttribute(ReservedTagConstants.XT), testName);
+            } else {
+                Assert.assertEquals(matchedAdapter, adapter, testName);
+                Assert.assertEquals(rec.getAttribute(ReservedTagConstants.XT), expected + 1, testName);
+
+                // Test that if minMatch is decreased, it still matches
+                for (int i = 1; i < minMatch - 3; ++i) {
+                    Assert.assertEquals(ClippingUtility.adapterTrimIlluminaSingleRead(rec, minMatch - i, errRate, adapter), adapter, testName);
+                }
+
+                // Skip this test for high error rates, because almost anything will match.
+                if (errRate < 0.5) {
+                    // Test that if minMatch is increased, it now fails to match
+                    Assert.assertNull(ClippingUtility.adapterTrimIlluminaSingleRead(rec, minMatch + 1, errRate, adapter), testName);
+
+                    // Test that if errRate is increased, it still matches
+                    Assert.assertNotNull(ClippingUtility.adapterTrimIlluminaSingleRead(rec, minMatch, errRate + 0.1, adapter), testName);
+                }
+
+                // Very low error threshold should cause match failure
+                Assert.assertNull(ClippingUtility.adapterTrimIlluminaSingleRead(rec, minMatch, 0.01, adapter), testName);
+            }
+        }
+
+    }
 
    @Test(dataProvider="clipPairedTestData")
    public void testPairedEndClip(final String testName, final String read1, final String read2, final AdapterPair expected) {
@@ -56,14 +109,59 @@ public class ClippingUtilityTest {
        rec2.setSecondOfPairFlag(true);
 
        final AdapterPair result = ClippingUtility.adapterTrimIlluminaPairedReads(rec1, rec2,
-               new AdapterPair[] { IlluminaAdapterPair.INDEXED, IlluminaAdapterPair.PAIRED_END });
+               IlluminaAdapterPair.INDEXED, IlluminaAdapterPair.PAIRED_END);
        if (result != null) {
            Assert.assertEquals(result.getName(), expected.getName(), testName);
        }
        else {
-           Assert.assertEquals(result, expected, testName);
+           Assert.assertNull(expected, testName);
        }
    }
+
+    @Test
+    public void testOneSidedMatchSupersededByTwoSidedMatch() {
+        final int readLength = 36;
+        final int adapterLength = 30;
+        final String read1 = patchAdapterSubsequenceIntoRead(makeBogusReadString(readLength), IlluminaAdapterPair.SINGLE_END.get3PrimeAdapterInReadOrder(), adapterLength);
+        final String read2 = patchAdapterSubsequenceIntoRead(makeBogusReadString(readLength), IlluminaAdapterPair.SINGLE_END.get5PrimeAdapterInReadOrder(), adapterLength);
+        final SAMRecord rec1 = new SAMRecord(null); rec1.setReadString(read1);
+        final SAMRecord rec2 = new SAMRecord(null); rec2.setReadString(read2);
+        AdapterPair result = ClippingUtility.adapterTrimIlluminaPairedReads(rec1, rec2, adapterLength - 2, 0.1,
+                IlluminaAdapterPair.PAIRED_END, IlluminaAdapterPair.SINGLE_END);
+        Assert.assertEquals(result, IlluminaAdapterPair.SINGLE_END);
+
+        // Confirm that without SINGLE_END, PAIRED_END would one-sided match, if match length is short enough.
+        result = ClippingUtility.adapterTrimIlluminaPairedReads(rec1, rec2, adapterLength/2, 0.1, IlluminaAdapterPair.PAIRED_END);
+        Assert.assertEquals(result, IlluminaAdapterPair.PAIRED_END);
+
+        // However, without shorter match length, one-sided match will fail.
+        Assert.assertNull(ClippingUtility.adapterTrimIlluminaPairedReads(rec1, rec2, adapterLength - 2, 0.1, IlluminaAdapterPair.PAIRED_END));
+    }
+
+    @Test(dataProvider = "testAdapterInAllReadPositionsDataProvider")
+    public void testAdapterInAllReadPositions(final int readLength) {
+        final int minAdapterLength = 6;
+        for (int adapterPosition = 0; adapterPosition < readLength; ++adapterPosition) {
+            final String adapterString = IlluminaAdapterPair.PAIRED_END.get3PrimeAdapterInReadOrder();
+            final int replacementLength = Math.min(adapterString.length(), readLength - adapterPosition);
+            final String adapterSubstring = adapterString.substring(0, replacementLength);
+            final String readBases = replaceSubstring(makeBogusReadString(readLength), adapterSubstring, adapterPosition, adapterSubstring.length());
+            final SAMRecord rec = new SAMRecord(null);  rec.setReadString(readBases);
+            final AdapterPair matchedAdapter = ClippingUtility.adapterTrimIlluminaSingleRead(rec, minAdapterLength, 0.1, IlluminaAdapterPair.PAIRED_END);
+            if (adapterPosition <= readLength - minAdapterLength) {
+                Assert.assertEquals(matchedAdapter, IlluminaAdapterPair.PAIRED_END, "Failed at position " + adapterPosition);
+                Assert.assertEquals(rec.getIntegerAttribute(ReservedTagConstants.XT), Integer.valueOf(adapterPosition + 1));
+            } else {
+                Assert.assertNull(matchedAdapter);
+                Assert.assertNull(rec.getAttribute(ReservedTagConstants.XT));
+            }
+        }
+    }
+
+    @DataProvider(name="testAdapterInAllReadPositionsDataProvider")
+    public Object[][] testAdapterInAllReadPositionsDataProvider() {
+        return new Object[][]{{100}, {36}};
+    }
 
     @DataProvider(name="clipTestData")
     public Object[][] getClipTestData() {
@@ -71,38 +169,124 @@ public class ClippingUtilityTest {
         final String SE_FORWARD = IlluminaAdapterPair.SINGLE_END.get3PrimeAdapter();
         final String REVERSE = IlluminaAdapterPair.PAIRED_END.get5PrimeAdapterInReadOrder();
         return new Object[][] { 
-            new Object[] {"Simple test 1", "AAAAACCCCCAGATCGGAAGAGCG", "AGATCGGAAGAGCG", 6, 0.15, 10},
-            new Object[] {"Simple test 2", "AAAAACCCCCGGGGGAGATCGGAAGAGCG", "AGATCGGAAGAGCG", 6, 0.15, 15},
-            new Object[] {"No adapter", "AAAAACCCCCGGGGGTTTTT", "AGATCGGAAGAGCG", 6, 0.15, -1},
-            new Object[] {"Partial adapter", "AAAAACCCCCAGATCGGAA", "AGATCGGAAGAGCG", 6, 0.15, 10},
+            {"Simple test 1", "AAAAACCCCCAGATCGGAAGAGCA", "AGATCGGAAGAGCG", 14, 0.15, 10},
+            {"Simple test 2", "AAAAACCCCCGGGGGAGATCGGAAGAGCA", "AGATCGGAAGAGCG", 14, 0.15, 15},
+            {"No adapter", "AAAAACCCCCGGGGGTTTTT", "AGATCGGAAGAGCG", 6, 0.15, -1},
+            {"Partial adapter", "AAAAACCCCCTGATCGGAA", "AGATCGGAAGAGCG", 9, 0.15, 10},
 // no longer support clips in middle of read
-//          new Object[] {"Adapter+Primer", "AAAAACCCCCAGATCGGAAGAGCGGTTCAGCAGGAATGCCGAGACCGATCTCGTATGCCGTCTTCTGCTTG", "AGATCGGAAGAGCG", 6, 0.15, 10},
-            new Object[] {"No sequence", null, "AGATCGGAAGAGCG", 6, 0.15, -1},
-            new Object[] {"Read too short", "AGATCGGAAG", "AGATCGGAAGAGCG", 11, 0.15, -1},
-            new Object[] {"All No Calls", "AAACCCNNNNNNNNNNNNNNNNNNNN", "AGATCGGAAGAGCG", 6, 0.15, -1},
-            new Object[] {"From Test Data1", "CGGCATTCCTGCTGAACCGAGATCGGAAGAGCGTCGTGTAGGGAAAGGGGGTGGATCTCGGTGGGCGGCGTGTTGT", REVERSE, 5, 0.15, 19},
-            new Object[] {"From Test Data2a", "CGGAAGAGCGGTTCAGCAGGAATGCCGAGATCGGAA", REVERSE, 5, 0.14, 27},  // XT:i:28
-            new Object[] {"From Test Data2b", "CGGAAGAGCGGTTCAGCAGGAATGCCGAGATCGGAA", REVERSE, 10, 0.14, -1},  // only matches 9
-            new Object[] {"From PE Test Data1", "CGGTTCAGCAGGAATGCCGAGATCGGAAGAGCGGGT", FORWARD, 5, 0.14, 19},
-            new Object[] {"From PE Test Data2", "CGGTTCAGCAGGAATGCCGAGATCGGAAGAGCGGGT", REVERSE, 5, 0.14, -1},
-            new Object[] {"From Test 8-clipped", "TGGGGTGGTTATTGTTGATTTTTGTTTGTGTGTTAGGTTGTTTGTGTTAGTTTTTTATTTTATTTTCGAGATCGGA", FORWARD, 8, 0.14, 68},
-            new Object[] {"50% can be bad", "AAAAACCCCCAGATCGGAAGAGCG", "AGATCGGAAGAGCG", 5, 0.5, 18},        // 18?
+//          {"Adapter+Primer", "AAAAACCCCCAGATCGGAAGAGCGGTTCAGCAGGAATGCCGAGACCGATCTCGTATGCCGTCTTCTGCTTG", "AGATCGGAAGAGCG", 6, 0.15, 10},
+            {"No sequence", null, "AGATCGGAAGAGCG", 6, 0.15, -1},
+            {"Read too short", "AGATCGGAAG", "AGATCGGAAGAGCG", 11, 0.15, -1},
+            {"All No Calls", "AAACCCNNNNNNNNNNNNNNNNNNNN", "AGATCGGAAGAGCG", 6, 0.15, -1},
+            {"From Test Data1", "CGGCATTCCTGCTGAACCGAGATCGGAAGAGCGTCGTGTAGGGAAAGGGGGTGGATCTCGGTGGGCGGCGTGTTGT", REVERSE, 57, 0.15, 19},
+            {"From Test Data2a", "CGGAAGAGCGGTTCAGCAGGAATGCCGAGATCCGAA", REVERSE, 9, 0.14, 27},  // XT:i:28
+            {"From Test Data2b", "CGGAAGAGCGGTTCAGCAGGAATGCCGAGATCGGAA", REVERSE, 10, 0.14, -1},  // only matches 9
+            {"From PE Test Data1", "CGGTTCAGCAGGAATGCCGAGATCGGAAGAGCGGGT", FORWARD, 17, 0.14, 19},
+            {"From PE Test Data2", "CGGTTCAGCAGGAATGCCGAGATCGGAAGAGCGGGT", REVERSE, 5, 0.14, -1},
+            new Object[] {"From Test 8-clipped", "TGGGGTGGTTATTGTTGATTTTTGTTTGTGTGTTAGGTTGTTTGTGTTAGTTTTTTATTTTATTTTCGAGATCGAA", FORWARD, 8, 0.14, 68},
+            {"50% can be bad", "AAAAACCCCCAGGTCGGAAGAGCG", "AGATCGGAAGAGCG", 5, 0.5, 18},        // 18?
 
-            new Object[] {"From 30E54AAXX.5.a", "ATATCTGAAGATCTCGTATGCCGTCTTCTGCTTG", "AGATCGGAAGAGCTCGTATGCCGTCTTCTGCTTG", 10, 0.14, 0},  // 0!? From KT test case
-            new Object[] {"From 30E54AAXX.5.b", "ATATCTGAAGATCTCGTATGCCGTCTTCTGCTTG", SE_FORWARD, 10, 0.14, 0},  // 1?? From KT test case
+            {"From 30E54AAXX.5.a", "ATATCTGAAGATCTCGTATGCCGTCTTCTGCTTG", "AGATCGGAAGAGCTCGTATGCCGTCTTCTGCTTG", 34, 0.14, 0},  // 0!? From KT test case
+            {"From 30E54AAXX.5.b", "ATATCTGAAGATCTCGTATGCCGTCTTCTGCTTG", SE_FORWARD, 34, 0.14, 0},  // 1?? From KT test case
         };
     }
 
     @DataProvider(name="clipPairedTestData")
     public Object[][] getClipPairedTestData() {
         return new Object[][] {
-            new Object[] {"Basic positive paired test matching",    "CTACTGGCGCTGAAACTGAGCAGCCAAGCAGATCGG", "GCTTGGCTGCTCAGTTTCAGCGCCAGTAGAGATCGG", IlluminaAdapterPair.INDEXED},
-            // Tbis matches on one side and matches at higher stringency on that one side
-            new Object[] {"Basic positive one-sided test matching", "CTACTGGCGCTGAAAAGATCGGAAGAGCGGTTCAGC", "AAAAAATTTTTTCCCCCCGGGGGGAAAAAATTTTTT", IlluminaAdapterPair.PAIRED_END},
-            // Tbis matches on one side and does not match at higher stringency on that one side
-            new Object[] {"Basic negative one-sided test matching", "GGCGCTGAAACTACTGGCGCTGAAAAGATCGGAAGA", "AAAAAATTTTTTCCCCCCGGGGGGAAAAAATTTTTT", null},
-            // These match but at different positions.  No clip should be done.
-            new Object[] {"Mis-match paired test matching",    "CTACTGGCGCTGAAACTGAGCAGCCAAGCAGATCGG", "AGCTTGGCTGCTCAGTTTCAGCGCCAGTAGAGATCG", null},
+                {"Basic positive paired test matching",    "CTACTGGCGCTGAAACTGAGCAGCCAAGCAGATCGG", "GCTTGGCTGCTCAGTTTCAGCGCCAGTAGAGATCGG", IlluminaAdapterPair.INDEXED},
+                // This matches on first end and matches at higher stringency on that one side
+                {"Basic positive first end one-sided test matching", "CTACTGGCGCTGAAAAGATCGGAAGAGCGGTTCAGC", "AAAAAATTTTTTCCCCCCGGGGGGAAAAAATTTTTT", IlluminaAdapterPair.PAIRED_END},
+                // This matches on second end and matches at higher stringency on that one side
+                {"Basic positive second end one-sided test matching", "AAAAAATTTTTTCCCCCCGGGGGGAAAAAATTTTTT", "AAAAAATTTTTTCCCAGATCGGAAGAGCGTCGTGTA", IlluminaAdapterPair.PAIRED_END},
+                // This matches on one side and does not match at higher stringency on that one side
+                {"Basic negative one-sided test matching", "GGCGCTGAAACTACTGGCGCTGAAAAGATCGGAAGA", "AAAAAATTTTTTCCCCCCGGGGGGAAAAAATTTTTT", null},
+                // These match but at different positions.  No clip should be done.
+                {"Mis-match paired test matching",    "CTACTGGCGCTGAAACTGAGCAGCCAAGCAGATCGG", "AGCTTGGCTGCTCAGTTTCAGCGCCAGTAGAGATCG", null},
         };
+    }
+
+    private static class SingleEndAdapter implements AdapterPair {
+        private final String name;
+        private final String threePrimeAdapter;
+
+        private SingleEndAdapter(final String name, final String threePrimeAdapter) {
+            this.name = name;
+            this.threePrimeAdapter = threePrimeAdapter;
+        }
+
+        @Override
+        public String get3PrimeAdapter() {
+            return threePrimeAdapter;
+        }
+
+        @Override
+        public String get3PrimeAdapterInReadOrder() {
+            return threePrimeAdapter;
+        }
+
+        @Override
+        public byte[] get3PrimeAdapterBytes() {
+            return StringUtil.stringToBytes(threePrimeAdapter);
+        }
+
+        @Override
+        public byte[] get3PrimeAdapterBytesInReadOrder() {
+            return get3PrimeAdapterBytes();
+        }
+
+        @Override
+        public String get5PrimeAdapter() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public String get5PrimeAdapterInReadOrder() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public byte[] get5PrimeAdapterBytes() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public byte[] get5PrimeAdapterBytesInReadOrder() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public String getName() {
+            return name;
+        }
+
+    }
+
+    /**
+     * @return A String of given length with 6 As, 6 Cs, 6 Gs, etc. until length is reached.
+     */
+    private static String makeBogusReadString(final int len) {
+        final StringBuilder builder = new StringBuilder(len);
+        final Map<Character, Character> nextChar = new HashMap<Character, Character>();
+        nextChar.put('A', 'C');
+        nextChar.put('C', 'G');
+        nextChar.put('G', 'T');
+        nextChar.put('T', 'A');
+        for (char curChar = 'A'; true; curChar = nextChar.get(curChar)) {
+            for (int i = 0; i < 6; ++i) {
+                if (builder.length() == len) return builder.toString();
+                builder.append(curChar);
+            }
+        }
+    }
+
+    private static String patchAdapterSubsequenceIntoRead(final String read, final String adapter, final int length) {
+        return replaceSubstring(read, adapter.substring(0, length), read.length() - length, read.length());
+    }
+
+    private static String replaceSubstring(final String s, final String replacement, final int position, final int charsToReplace) {
+        final StringBuilder sb = new StringBuilder(s);
+        sb.replace(position, position + charsToReplace, replacement);
+        return sb.toString();
     }
 }
