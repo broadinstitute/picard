f5ac9d158ed6c7f26bf386c6d5a262f4a87d82a8
Martin Pollard <mp15@sanger.ac.uk>
2014-09-02 17:54:19 +0100

    DownsampleSam for BWA MEM and non-Illumina platforms
    Add discard level option to DownsampleSam and ensure non-primary reads are retained by default.
    Switch from using hashtable to store readnames to using hash projection to identify read
    names to remove.
    
    Signed-off-by: Martin Pollard <mp15@sanger.ac.uk>

--- src/java/picard/sam/DownsampleSam.java
+++ src/java/picard/sam/DownsampleSam.java
@@ -1,9 +1,11 @@
 package picard.sam;
 
+import htsjdk.samtools.DiscardLevel;
 import htsjdk.samtools.SAMFileReader;
 import htsjdk.samtools.SAMFileWriter;
 import htsjdk.samtools.SAMFileWriterFactory;
 import htsjdk.samtools.SAMRecord;
+import htsjdk.samtools.util.HashDecisionProjector;
 import htsjdk.samtools.util.IOUtil;
 import htsjdk.samtools.util.Log;
 import htsjdk.samtools.util.ProgressLogger;
@@ -13,8 +15,6 @@ import picard.cmdline.StandardOptionDefinitions;
 import picard.cmdline.Usage;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Random;
 
 /**
@@ -23,9 +23,9 @@ import java.util.Random;
  */
 public class DownsampleSam extends CommandLineProgram {
     @Usage public final String USAGE = getStandardUsagePreamble() + " Randomly down-sample a SAM or BAM file to retain " +
-            "a random subset of the reads. Mate-pairs are either both kept or both discarded. Reads marked as not primary " +
-            "alignments are all discarded. Each read is given a probability P of being retained - results with the exact " +
-            "same input in the same order and with the same value for RANDOM_SEED will produce the same results.";
+            "a random subset of the reads. Mate-pairs and reads marked as not primary are either all kept or all " +
+            "discarded. Each read is given a probability P of being retained - results with the exact same input in " +
+            "the same order and with the same value for RANDOM_SEED will produce the same results.";
 
     @Option(shortName=StandardOptionDefinitions.INPUT_SHORT_NAME, doc="The input SAM or BAM file to downsample.")
     public File INPUT;
@@ -35,10 +35,13 @@ public class DownsampleSam extends CommandLineProgram {
 
     @Option(shortName="R", doc="Random seed to use if reproducibilty is desired.  " +
             "Setting to null will cause multiple invocations to produce different results.")
-    public Long RANDOM_SEED = 1L;
+    public Integer RANDOM_SEED = 1;
 
     @Option(shortName="P", doc="The probability of keeping any individual read, between 0 and 1.")
     public double PROBABILITY = 1;
+
+    @Option(shortName="D", doc="Whether or not to discard non-primary alignments.")
+    public DiscardLevel DISCARD_LEVEL = DiscardLevel.NONE;
 
     private final Log log = Log.getInstance(DownsampleSam.class);
 
@@ -51,10 +54,9 @@ public class DownsampleSam extends CommandLineProgram {
         IOUtil.assertFileIsReadable(INPUT);
         IOUtil.assertFileIsWritable(OUTPUT);
 
-        final Random r = RANDOM_SEED == null ? new Random() : new Random(RANDOM_SEED);
         final SAMFileReader in = new SAMFileReader(INPUT);
         final SAMFileWriter out = new SAMFileWriterFactory().makeSAMOrBAMWriter(in.getFileHeader(), true, OUTPUT);
-        final Map<String,Boolean> decisions = new HashMap<String,Boolean>();
+        final HashDecisionProjector decision = new HashDecisionProjector(RANDOM_SEED == null ? new Random().nextInt() : RANDOM_SEED, PROBABILITY);
 
         long total = 0;
         long kept  = 0;
@@ -62,31 +64,22 @@ public class DownsampleSam extends CommandLineProgram {
         final ProgressLogger progress = new ProgressLogger(log, (int) 1e7, "Read");
 
         for (final SAMRecord rec : in) {
-            if (rec.isSecondaryOrSupplementary()) continue;
-            ++total;
+            if ( (DISCARD_LEVEL == DiscardLevel.SECONDARY && rec.getNotPrimaryAlignmentFlag()) || (DISCARD_LEVEL == DiscardLevel.ALL && rec.isSecondaryOrSupplementary())) continue;
+
+            if (!rec.isSecondaryOrSupplementary()) ++total;
 
             final String key = rec.getReadName();
-            final Boolean previous = decisions.remove(key);
-            final boolean keeper;
 
-            if (previous == null) {
-                keeper = r.nextDouble() <= PROBABILITY;
-                if (rec.getReadPairedFlag()) decisions.put(key, keeper);
-            }
-            else {
-                keeper = previous;
-            }
-
-            if (keeper) {
+            if (decision.decide(key)) {
                 out.addAlignment(rec);
-                ++kept;
+                if (!rec.isSecondaryOrSupplementary()) ++kept;
             }
 
             progress.record(rec);
         }
 
         out.close();
-        log.info("Finished! Kept " + kept + " out of " + total + " reads.");
+        log.info("Finished! Kept " + kept + " out of " + total + " primary reads.");
 
         return 0;
     }
