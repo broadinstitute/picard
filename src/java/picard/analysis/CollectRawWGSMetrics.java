00288314092316f95e2d7dfa91c630d3399cad11
Yossi Farjoun <farjoun@broadinstitute.org>
2014-11-24 10:39:22 -0500

    - Refactoring CollectWgsMetrics in order to be able to easily create CollectRawWGSMetrics. - Create RawWGSMetrics by overriding some default option values and   repackaging the metrics class. - Moved CollectIlluminaSummaryMetrics (will be deleted eventually) - Fixed some test files.
    - Removed the buggy CLP CollectIlluminaSummaryMetrics.java and its test
    
    responding to reviewer comments.

---
+++ src/java/picard/analysis/CollectRawWGSMetrics.java
@@ -0,0 +1,37 @@
+package picard.analysis;
+
+import picard.cmdline.CommandLineProgramProperties;
+import picard.cmdline.Option;
+import picard.cmdline.programgroups.Metrics;
+
+/**
+ * Computes a number of metrics that are useful for evaluating coverage and performance of whole genome sequencing experiments, same implementation as CollectWgsMetrics, with different defaults: lacks baseQ and mappingQ filters and has much higher coverage cap.
+ *
+ * @author farjoun
+ */
+@CommandLineProgramProperties(
+        usage = "Computes a number of metrics that are useful for evaluating coverage and performance of " +
+                "whole genome sequencing experiments. Defaults are different than for CollectWgsMetrics.",
+        usageShort = "Writes whole genome sequencing-related metrics for a SAM or BAM file",
+        programGroup = Metrics.class
+)
+public class CollectRawWGSMetrics extends CollectWgsMetrics{
+
+    @Option(shortName="MQ", doc="Minimum mapping quality for a read to contribute coverage.")
+    public int MINIMUM_MAPPING_QUALITY = 0;
+
+    @Option(shortName="Q", doc="Minimum base quality for a base to contribute coverage.")
+    public int MINIMUM_BASE_QUALITY = 3;
+
+    @Option(shortName="CAP", doc="Treat bases with coverage exceeding this value as if they had coverage at this value.")
+    public int COVERAGE_CAP = 100000;
+
+    // rename the class so that in the metric file it is annotated differently.
+    public static class RawWgsMetrics extends WgsMetrics {}
+
+    @Override
+    protected WgsMetrics generateWgsMetrics() {
+        return new RawWgsMetrics();
+    }
+
+}
