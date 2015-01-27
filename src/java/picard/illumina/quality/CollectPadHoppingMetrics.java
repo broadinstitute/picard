90d673ac0448d44e18d1f55337c873aad7c047fe
David Benjamin <davidben@broadinstitute.org>
2015-01-27 08:50:03 -0500

    Whoops, can't shuffle a final ArrayList! (at least I hope that's the issue)

--- src/java/picard/illumina/quality/CollectPadHoppingMetrics.java
+++ src/java/picard/illumina/quality/CollectPadHoppingMetrics.java
@@ -171,7 +171,7 @@ public class CollectPadHoppingMetrics extends CommandLineProgram {
         final ExecutorService pool = Executors.newFixedThreadPool(numProcessors);
 
         //get a random subset of all tiles
-        final List<Integer> allTiles = factory.getAvailableTiles();
+        List<Integer> allTiles = factory.getAvailableTiles();
         Collections.shuffle(allTiles);
         List<Integer> tilesToProcess = new ArrayList<Integer>();
         for (int n = 0; n < allTiles.size() && n < TILES_TO_PROCESS; n++)
