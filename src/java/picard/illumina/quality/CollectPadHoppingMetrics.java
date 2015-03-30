c5c55325d0d25d6129176554659edd85793179d1
David Benjamin <davidben@broadinstitute.org>
2015-03-30 15:11:54 -0400

    Subclasses and helper classes no longer satic

--- src/java/picard/illumina/quality/CollectPadHoppingMetrics.java
+++ src/java/picard/illumina/quality/CollectPadHoppingMetrics.java
@@ -21,6 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 package picard.illumina.quality;
 
 import htsjdk.samtools.metrics.MetricBase;
@@ -57,10 +58,10 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * Collect metrics regarding pad-hopping in the Illumina Hi-Seq X.
- * Terminology: in all Illumina Hi-Seq machines, a "cluster" is a discrete area on a flow cell that contains
- * bridge-amplified DNA fragments, hopefully all clones of a single original insert  In the Hi-Seq X, each cluster
- * is constrained within a hexagonal "pad".  In "pad-hopping", groups of nearby pads have duplicate DNA fragments.
- * In the following code, such a contiguous group will be called a "bunch".
+ * In all Illumina Hi-Seq machines, a "cluster" is a discrete area on a flow cell that contains
+ * bridge-amplified clones of a DNA fragment.  In the Hi-Seq X, clusters are constrained to pads
+ * arranged in a hexagonal lattice.  In pad-hopping, groups of nearby pads have duplicate DNA.
+ * In the following, such groups will be called a "bunches".
  *
  * @author David Benjamin
  */
@@ -79,34 +80,33 @@ public class CollectPadHoppingMetrics extends CommandLineProgram {
             " <OUTPUT>" + summaryMetricsExtension, optional = false)
     public File OUTPUT;
 
-    @Option(shortName = "P", doc = "The fraction of pad-hopping events to output explicitly. Output file will be <OUTPUT>" + detailedMetricsExtension + " (if PROB_EXPLICIT_OUTPUT != 0)", optional = true)
+    @Option(doc = "The fraction of pad-hopping events to output in detailed metrics.", optional = true)
     public double PROB_EXPLICIT_OUTPUT = 0;
 
     @Option(doc = "Lane number.", shortName = StandardOptionDefinitions.LANE_SHORT_NAME)
     public Integer LANE;
 
-    @Option(shortName = "NP", doc = "Run this many PerTilePadHoppingMetricsExtractor in parallel.  If NUM_PROCESSORS = 0, number of cores is automatically set to " +
-            "the number of cores available on the machine. If NUM_PROCESSORS < 0 then the number of cores used will be " +
-            "the number available on the machine less NUM_PROCESSORS.", optional = true)
-    public int NUM_PROCESSORS = 1;
+    @Option(doc = "Run this many PerTilePadHoppingMetricsExtractor in parallel.  If N_PROCESSORS = 0, use all available cores. " +
+            "If N_PROCESSORS < 0 use all but |N_PROCESSORS| cores.", optional = true)
+    public int N_PROCESSORS = 1;
 
-    @Option(shortName = "TP", doc = "Number of tiles on which to calculate pad-hopping metrics.", optional = true)
-    public int TILES_TO_PROCESS = 1;
+    @Option(doc = "Number of tiles on which to calculate pad-hopping metrics.  Default of 8 gives a good lane average.", optional = true)
+    public int N_TILES = 8;
 
-    @Option(shortName = "TI", doc = "Index of first tile (0 to 95)", optional = true)
-    public int TILE_INDEX = 0;
+    @Option(doc = "Index of first tile (0 to 95).  Default -1 is an evenly-spaced sample over the lane", optional = true)
+    public int TILE_INDEX = -1;
 
-    @Option(doc = "Number of cycles to look at. At time of writing PF status gets determined at cycle 24 so numbers greater than this will yield strange results. " +
-            "In addition, PF status is currently determined at cycle 24, so running this with any other value is neither tested nor recommended.", optional = true)
+    @Option(doc = "Number of cycles for these basecalls. PF status gets determined at cycle 24." , optional = true)
     public int N_CYCLES = 24;
 
     @Option(shortName = "NB", doc = "Number of bases to look at.  Due to sequencing error comparing fewer bases" +
             " may give a more correct estimate.", optional = true)
     public int N_BASES = 24;
 
-    @Option(shortName = "ND", doc = "Max distance (in Illumina's internal cluster coordinate units) for two custers " +
-            "to be considered adjacent.  The distance is 20 +/- 1 for all tiles of all Hi Seq X flowcells.", optional = true)
-    public double MAX_NEIGHBOR_DISTANCE = Double.POSITIVE_INFINITY;
+    @Option(shortName = "ND", doc = "Max distance in pixels between duplicate clusters to be considered pad-hopping." +
+            "Adjacent clusters are separated by ~20 on the HiSeqX.  An accurate and fast approximation is to" +
+            " consider all duplicates on the same tile to be pad-hopping, hence the infinite default.", optional = true)
+    public double CUTOFF = Double.POSITIVE_INFINITY;
 
     private static final Log LOG = Log.getInstance(CollectPadHoppingMetrics.class);
 
@@ -125,26 +125,13 @@ public class CollectPadHoppingMetrics extends CommandLineProgram {
     protected String[] customCommandLineValidation() {
         final List<String> errors = new ArrayList<String>();
 
-        if (N_CYCLES < 0)
-            errors.add("Number of Cycles to look at must be greater than 0");
+        if (N_BASES < 1) errors.add("Must consider at least one base (and really fewer than 6 is nonsensical)");
+        if (N_BASES > N_CYCLES) errors.add("N_BASES cannot exceed N_CYCLES");
 
-        if (N_BASES < 1)
-            errors.add("Must consider at least one base (and really fewer than 6 is nonsensical)");
+        if (N_TILES < 1 || N_TILES > 96) errors.add("Must process at least 1 and at most 96 tiles");
 
-        if (N_BASES > N_CYCLES)
-            errors.add("N_BASES cannot exceed N_CYCLES");
-
-        if (TILES_TO_PROCESS < 1)
-            errors.add("Must process at least one tile");
-
-        if (TILES_TO_PROCESS > 96)
-            errors.add("There are only 96 tiles per lane of the Illumina HiSeqX");
-
-        if (TILE_INDEX < 0)
-            errors.add("Must choose a non-negative tile index");
-
-        if (TILE_INDEX > 95)
-            errors.add("Tile index may be at most 95 (there are 96 tiles on the HiSeqX)");
+        if (TILE_INDEX < -1) errors.add("Must choose a non-negative tile index or -1 to take a lane average");
+        if (TILE_INDEX > 95) errors.add("Tile index may be at most 95 (there are 96 tiles on the HiSeqX)");
 
         if (PROB_EXPLICIT_OUTPUT > 1 || PROB_EXPLICIT_OUTPUT < 0)
             errors.add("PROB_EXPLICIT_OUTPUT must be a probability, i.e., 0 <= PROB_EXPLICIT_OUTPUT <= 1");
@@ -176,16 +163,28 @@ public class CollectPadHoppingMetrics extends CommandLineProgram {
         IOUtil.assertFileIsWritable(summaryMetricsFileName);
         if (PROB_EXPLICIT_OUTPUT > 0) IOUtil.assertFileIsWritable(detailedMetricsFileName);
 
-        final int numProcessors = NUM_PROCESSORS + ((NUM_PROCESSORS > 0) ? 0 : Runtime.getRuntime().availableProcessors());
+        final int numProcessors = N_PROCESSORS + ((N_PROCESSORS > 0) ? 0 : Runtime.getRuntime().availableProcessors());
         final ExecutorService pool = Executors.newFixedThreadPool(numProcessors);
         LOG.info("Processing with " + numProcessors + " PerTilePadHoppingMetricsExtractor(s).");
 
         List<Integer> allTiles = new ArrayList<Integer>(factory.getAvailableTiles());
         Collections.sort(allTiles);
 
-        int firstTile = TILE_INDEX;
-        int lastTile = Math.min(allTiles.size(), firstTile + TILES_TO_PROCESS);
-        final List<Integer> tilesToProcess = allTiles.subList(firstTile, lastTile);
+        List<Integer> tilesToProcess;
+        //default case of evenly-spaced tiles to average over the lane
+        if (TILE_INDEX == -1) {
+            int TOTAL_TILES = 96;
+            int offset = TOTAL_TILES/(N_TILES * 2);
+            tilesToProcess = new ArrayList<Integer>();
+            for (int i = 0; i < N_TILES; i++)
+                tilesToProcess.add(allTiles.get(offset + (TOTAL_TILES*i)/ N_TILES));
+        }
+        else {
+            int firstTile = TILE_INDEX;
+            int lastTile = Math.min(allTiles.size(), firstTile + N_TILES);
+            tilesToProcess = allTiles.subList(firstTile, lastTile);
+        }
+
         LOG.info("Computing pad hopping metrics for " + tilesToProcess.size() + " tiles.");
 
         final List<PerTilePadHoppingMetricsExtractor> extractors = new ArrayList<PerTilePadHoppingMetricsExtractor>(tilesToProcess.size());
@@ -194,7 +193,7 @@ public class CollectPadHoppingMetrics extends CommandLineProgram {
             tileToDetailedMetrics.put(tile, new ArrayList<PadHoppingDetailMetric>());
 
             extractors.add(new PerTilePadHoppingMetricsExtractor(tile, tileToSummaryMetrics.get(tile),
-                    tileToDetailedMetrics.get(tile), factory, PROB_EXPLICIT_OUTPUT, MAX_NEIGHBOR_DISTANCE, N_BASES));
+                    tileToDetailedMetrics.get(tile), factory, PROB_EXPLICIT_OUTPUT, CUTOFF, N_BASES));
         }
         try {
             for (final PerTilePadHoppingMetricsExtractor extractor : extractors)
@@ -241,11 +240,9 @@ public class CollectPadHoppingMetrics extends CommandLineProgram {
         return 0;
     }
 
-    /** Extracts metrics from a HiSeqX tile.
-     * Different tiles use different files so each Extractor has its own thread to avoid waiting for
-     * each other's file I/O.
+    /** Extracts metrics from a HiSeqX tile on its own thread
      */
-    private static class PerTilePadHoppingMetricsExtractor implements Runnable {
+    private class PerTilePadHoppingMetricsExtractor implements Runnable {
 
         private final int tile;
         private final PadHoppingSummaryMetric summaryMetric;
@@ -276,16 +273,13 @@ public class CollectPadHoppingMetrics extends CommandLineProgram {
             try {
                 LOG.info("Extracting pad-hopping metrics for tile " + tile);
 
-                //a possible source of improved performance is that this sets up a List<Point> for
-                //EVERY read, even ones that are not duplicated
                 Map<String, List<Point>> duplicateSets = new HashMap<String, List<Point>>();
 
                 for (final ClusterData cluster : provider) {
                     if (! cluster.isPf() ) continue;
                     summaryMetric.READS++;
 
-                    //getBases() returns byte[]. Converting to String loses performance but is more convenient for hashing
-                    //I profiled and creating a wrapper for byte[] using Arrays.hashcode gained very little
+                    //getBases() returns byte[]. Converting to String costs some speed but makes hashing easier
                     final String allBases = new String(cluster.getRead(0).getBases());
                     final String bases = allBases.substring(0, nBases);
 
@@ -310,7 +304,6 @@ public class CollectPadHoppingMetrics extends CommandLineProgram {
                         for (Bunch bunch : bunchFinder.getBunches()) {
                             if (bunch.size() == 1) continue;
                             summaryMetric.PAD_HOPPING_DUPLICATES += bunch.numDuplicates();
-                            //randomly add pad-hopping events to detailed metrics
                             if (random.nextDouble() < pWriteDetailed)
                                 detailedMetrics.add(new PadHoppingDetailMetric(tile, bases, bunch));
                         }
@@ -326,30 +319,12 @@ public class CollectPadHoppingMetrics extends CommandLineProgram {
         }
     }
 
-    /**
-     * A Bunch is little more than a typedef for a list of Points.  It contains a few extra methods for characterizing
-     * pad-hopping and stores the read common to all clusters in the bunch.
-     *
-     * Depending on how much we deeply we wish to study pad-hopping, we could add more methods.
-     */
-    private static class Bunch extends ArrayList<Point> {
-
+    private class Bunch extends ArrayList<Point> {
         public Bunch() { }
-
         public int numDuplicates() { return size() - 1; }
-
-        public Point center() {
-            int totalX = 0;
-            int totalY = 0;
-            for (Point p : this) {
-                totalX += p.getX();
-                totalY += p.getY();
-            }
-            return new Point(totalX / size(), totalY / size());
-        }
     }
 
-    private static class BunchFinder {
+    private class BunchFinder {
         private ArrayList<Bunch> bunches;
         private int N;  //total number of points
 
@@ -383,24 +358,21 @@ public class CollectPadHoppingMetrics extends CommandLineProgram {
     }
 
     /** a metric class for describing pad-hopping bunches **/
-    public static class PadHoppingDetailMetric extends MetricBase {
+    public class PadHoppingDetailMetric extends MetricBase {
         public Integer TILE;
 
         public String BASES;
 
-        //The number of clusters in this pad-hopping bunch
         public int SIZE;
 
-        //represent all points as a single space-separated string
         public String POINTS_STRING;
-
-
 
         public PadHoppingDetailMetric(final Integer tile, final String bases, final List<Point> points) {
             TILE = tile;
             BASES = bases;
             SIZE = points.size();
 
+            //Output points as a space-free string for easy parsing
             StringBuilder builder = new StringBuilder();
             for (final Point p : points) {
                 builder.append(p.getX());
@@ -415,11 +387,10 @@ public class CollectPadHoppingMetrics extends CommandLineProgram {
         public PadHoppingDetailMetric() { }
     }
 
-    public static class PadHoppingSummaryMetric extends MetricBase {
+    public class PadHoppingSummaryMetric extends MetricBase {
         /** The Tile that is described by this metric. Can be a string (like "All") to mean some marginal over tiles. * */
         public String TILE = null;
 
-        /** The total number of reads examined */
         public int READS = 0;
 
         /** Duplicates due to pad-hopping in this tile.  In a bunch of N clusters, N - 1 are duplicates */
@@ -427,7 +398,6 @@ public class CollectPadHoppingMetrics extends CommandLineProgram {
 
         public double PCT_PAD_HOPPING_DUPLICATES = 0.0;
 
-        // constructor takes a String for tile since we want to have one instance with tile="All". This tile will contain the summary of all the tiles
         public PadHoppingSummaryMetric(final String tile) {
             TILE = tile;
         }
@@ -442,7 +412,7 @@ public class CollectPadHoppingMetrics extends CommandLineProgram {
 
         public void calculateDerivedFields() {
             if (READS != 0)
-                PCT_PAD_HOPPING_DUPLICATES = (double) PAD_HOPPING_DUPLICATES / this.READS;
+                PCT_PAD_HOPPING_DUPLICATES = (double) PAD_HOPPING_DUPLICATES / READS;
         }
     }
 
