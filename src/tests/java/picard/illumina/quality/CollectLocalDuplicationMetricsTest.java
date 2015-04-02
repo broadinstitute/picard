387cd5a7bade9a4e4d769a0d43be571f92046bc6
David Benjamin <davidben@broadinstitute.org>
2015-04-02 13:38:05 -0400

    Wrote the test

--- src/tests/java/picard/illumina/quality/CollectLocalDuplicationMetricsTest.java
+++ src/tests/java/picard/illumina/quality/CollectLocalDuplicationMetricsTest.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 
 public class CollectLocalDuplicationMetricsTest {
     /** Use the data already set up for testingCollectIlluminBasecallingMetrics */
-    private static final File TEST_DATA_DIR = new File("testdata/picard/illumina/CollectIlluminaBasecallingMetrics");
+    private static final File TEST_DATA_DIR = new File("testdata/picard/illumina");
 
     private File rootTestDir;
 
@@ -40,37 +40,35 @@ public class CollectLocalDuplicationMetricsTest {
 
     @Test
     public void testSummaryMetrics() throws Exception {
-        final MetricsFile<LocalDuplicationSummaryMetrics, Integer> metricsFile = runSummaryMetrics(1, 24, "25T8B25T/Data/Intensities/BaseCalls");
+        final MetricsFile<LocalDuplicationSummaryMetrics, Integer> metricsFile0 = runSummaryMetrics(1, 24, "125T125T/Data/Intensities/BaseCalls",0);
+        final MetricsFile<LocalDuplicationSummaryMetrics, Integer> metricsFile1000 = runSummaryMetrics(1, 24, "125T125T/Data/Intensities/BaseCalls",1000);
+        final MetricsFile<LocalDuplicationSummaryMetrics, Integer> metricsFileWholeTile = runSummaryMetrics(1, 24, "125T125T/Data/Intensities/BaseCalls",Double.POSITIVE_INFINITY);
 
-        /** Need to fill in the appropriate assertions here */
-        final LocalDuplicationSummaryMetrics metric1 = metricsFile.getMetrics().get(0);
-        Assert.assertEquals(metric1.LANE, "1");
+        //getMetrics().get(0) returns the first ("All") metric
+        final LocalDuplicationSummaryMetrics metric0 = metricsFile0.getMetrics().get(0);
+        Assert.assertEquals(metric0.LANE, 1);
+        Assert.assertEquals(metric0.TILE, "All");
+        Assert.assertEquals(metric0.READS, 1863);
+        Assert.assertEquals(metric0.LOCAL_DUPLICATES, 0);
+        Assert.assertEquals(metric0.PCT_LOCAL_DUPLICATES, ((double) metric0.LOCAL_DUPLICATES) / metric0.READS);
 
-        final LocalDuplicationSummaryMetrics metric2 = metricsFile.getMetrics().get(1);
-        Assert.assertEquals(metric2.LANE, "1");
+        final LocalDuplicationSummaryMetrics metric1000 = metricsFile1000.getMetrics().get(0);
+        Assert.assertEquals(metric1000.LANE, 1);
+        Assert.assertEquals(metric1000.TILE, "All");
+        Assert.assertEquals(metric1000.READS, 1863);
+        Assert.assertEquals(metric1000.LOCAL_DUPLICATES, 45);
+        Assert.assertEquals(metric1000.PCT_LOCAL_DUPLICATES, ((double) metric1000.LOCAL_DUPLICATES) / metric1000.READS);
 
-        final LocalDuplicationSummaryMetrics metric3 = metricsFile.getMetrics().get(2);
-        Assert.assertEquals(metric3.LANE, "1");
-
-        final LocalDuplicationSummaryMetrics metric4 = metricsFile.getMetrics().get(3);
-        Assert.assertEquals(metric4.LANE, "1");
-
-        final LocalDuplicationSummaryMetrics metric5 = metricsFile.getMetrics().get(4);
-        Assert.assertEquals(metric5.LANE, "1");
-
-        final LocalDuplicationSummaryMetrics laneMetric = metricsFile.getMetrics().get(34);
-        Assert.assertEquals(laneMetric.LANE, "1");
-
+        final LocalDuplicationSummaryMetrics metricWholeTile = metricsFileWholeTile.getMetrics().get(0);
+        Assert.assertEquals(metricWholeTile.LANE, 1);
+        Assert.assertEquals(metricWholeTile.TILE, "All");
+        Assert.assertEquals(metricWholeTile.READS, 1863);
+        Assert.assertEquals(metricWholeTile.LOCAL_DUPLICATES, 118);
+        Assert.assertEquals(metricWholeTile.PCT_LOCAL_DUPLICATES, ((double) metricWholeTile.LOCAL_DUPLICATES) / metricWholeTile.READS);
     }
 
-    @Test
-    public void testDetailMetrics() throws Exception {
-        final MetricsFile<LocalDuplicationDetailMetrics, Integer> metricsFile = runDetailMetrics(1, 24, "125T125T/Data/Intensities/BaseCalls");
-        final LocalDuplicationDetailMetrics laneMetric = metricsFile.getMetrics().get(0);
 
-    }
-
-    private MetricsFile<LocalDuplicationSummaryMetrics, Integer> runSummaryMetrics(final int lane, final int numBases, final String basecallsDirName) throws Exception {
+    private MetricsFile<LocalDuplicationSummaryMetrics, Integer> runSummaryMetrics(final int lane, final int numBases, final String basecallsDirName, final double maxSeparation) throws Exception {
         final File metricsFile = File.createTempFile("ldsm", null);
         final String basename = metricsFile.getPath();
         final String extension = "." + CollectLocalDuplicationMetrics.SUMMARY_METRICS_EXTENSION;
@@ -85,6 +83,7 @@ public class CollectLocalDuplicationMetricsTest {
         argsList.add("LANE=" + lane);
         argsList.add("OUTPUT=" + basename);
         argsList.add("NUM_BASES=" + numBases);
+        argsList.add("MAX_SEPARATION=" + maxSeparation);
         argsList.add("PROB_EXPLICIT_OUTPUT=0");
         argsList.add("TILE_INDEX=0");
         argsList.add("NUM_TILES=" + CollectLocalDuplicationMetrics.TILES_PER_LANE);
@@ -96,36 +95,6 @@ public class CollectLocalDuplicationMetricsTest {
 
         final MetricsFile<LocalDuplicationSummaryMetrics,Integer> retval =
                 new MetricsFile<LocalDuplicationSummaryMetrics,Integer>();
-        retval.read(new FileReader(outputMetricsFile));
-        return retval;
-    }
-
-    private MetricsFile<LocalDuplicationDetailMetrics, Integer> runDetailMetrics(final int lane, final int numBases, final String basecallsDirName) throws Exception {
-        final File metricsFile = File.createTempFile("lddm", null);
-        final String basename = metricsFile.getPath();
-        final String extension = "." + CollectLocalDuplicationMetrics.DETAILED_METRICS_EXTENSION;
-        final File outputMetricsFile = new File(basename + extension);
-        metricsFile.deleteOnExit();
-        outputMetricsFile.deleteOnExit();
-
-        File basecallsDir = new File(rootTestDir.getPath(),basecallsDirName);
-
-        ArrayList<String> argsList = new ArrayList<String>();
-        argsList.add("BASECALLS_DIR=" + basecallsDir.getPath());
-        argsList.add("LANE=" + lane);
-        argsList.add("OUTPUT=" + basename);
-        argsList.add("NUM_BASES=" + numBases);
-        argsList.add("PROB_EXPLICIT_OUTPUT=1.0");
-        argsList.add("TILE_INDEX=0");
-        argsList.add("NUM_TILES=" + CollectLocalDuplicationMetrics.TILES_PER_LANE);
-
-        final String[] args = new String[argsList.size()];
-        argsList.toArray(args);
-
-        Assert.assertEquals(new CollectLocalDuplicationMetrics().instanceMain(args),0);
-
-        final MetricsFile<LocalDuplicationDetailMetrics,Integer> retval =
-                new MetricsFile<LocalDuplicationDetailMetrics,Integer>();
         retval.read(new FileReader(outputMetricsFile));
         return retval;
     }
