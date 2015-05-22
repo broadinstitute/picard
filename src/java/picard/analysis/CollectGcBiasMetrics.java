66c23d588005e95de94c2d76018369658cc0abc9
Yossi Farjoun <farjoun@gmail.com>
2015-05-21 23:46:02 -0400

    fix for bug in collectGcBiasMetrics

--- src/java/picard/analysis/CollectGcBiasMetrics.java
+++ src/java/picard/analysis/CollectGcBiasMetrics.java
@@ -107,7 +107,12 @@ public class CollectGcBiasMetrics extends SinglePassSamProgram {
     protected void setup(final SAMFileHeader header, final File samFile) {
         IOUtil.assertFileIsWritable(CHART_OUTPUT);
         if (SUMMARY_OUTPUT != null) IOUtil.assertFileIsWritable(SUMMARY_OUTPUT);
-        saveHeader = header.getReadGroups().get(0).getLibrary();
+
+        try{
+            saveHeader = header.getReadGroups().get(0).getLibrary();
+        } catch (Exception ignored){
+            saveHeader = "LibraryNameUnavailable";
+        }
     }
 
     ////////////////////////////////////////////////////////////////////////////
