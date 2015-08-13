b3471a943b0cbbf72dae3d55c215fccf86b99758
Eric Banks <ebanks@broadinstitute.org>
2015-08-13 12:35:33 -0400

    Tool to collect summary metrics from a stream of query-name sorted reads.

---
+++ src/java/picard/analysis/CollectWgsMetricsFromQuerySorted.java
@@ -0,0 +1,352 @@
+package picard.analysis;
+
+import htsjdk.samtools.*;
+import htsjdk.samtools.metrics.MetricsFile;
+import htsjdk.samtools.util.*;
+import picard.cmdline.CommandLineProgram;
+import picard.cmdline.CommandLineProgramProperties;
+import picard.cmdline.Option;
+import picard.cmdline.StandardOptionDefinitions;
+import picard.cmdline.programgroups.Metrics;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Computes a number of metrics that are useful for evaluating coverage and performance of sequencing experiments.
+ *
+ * @author ebanks
+ */
+@CommandLineProgramProperties(
+        usage = "Computes a number of metrics that are useful for evaluating coverage and performance of " +
+                "sequencing experiments.",
+        usageShort = "Writes sequencing-related metrics for a SAM or BAM file",
+        programGroup = Metrics.class
+)
+public class CollectWgsMetricsFromQuerySorted extends CommandLineProgram {
+
+    @Option(shortName = StandardOptionDefinitions.INPUT_SHORT_NAME, doc = "Input SAM or BAM file.")
+    public File INPUT;
+
+    @Option(shortName = StandardOptionDefinitions.OUTPUT_SHORT_NAME, doc = "Output metrics file.")
+    public File OUTPUT;
+
+    @Option(shortName = "MQ", doc = "Minimum mapping quality for a read to contribute coverage.", overridable = true)
+    public int MINIMUM_MAPPING_QUALITY = 20;
+
+    @Option(shortName = "Q", doc = "Minimum base quality for a base to contribute coverage.", overridable = true)
+    public int MINIMUM_BASE_QUALITY = 20;
+
+    private final Log log = Log.getInstance(CollectWgsMetricsFromQuerySorted.class);
+
+    /** Metrics for evaluating the performance of whole genome sequencing experiments. */
+    public static class MySeqMetrics extends CollectWgsMetrics.WgsMetrics {
+        /** The total number of bases, before any filters are applied. */
+        public long TOTAL_BASES = 0;
+        /** The number of usable bases, after all filters are applied. */
+        public long TOTAL_USABLE_BASES = 0;
+    }
+
+    public static void main(final String[] args) {
+        new CollectWgsMetricsFromQuerySorted().instanceMainWithExit(args);
+    }
+
+    @Override
+    protected int doWork() {
+        IOUtil.assertFileIsReadable(INPUT);
+        IOUtil.assertFileIsWritable(OUTPUT);
+
+        // progress tracker
+        final ProgressLogger progress = new ProgressLogger(log, 50000000, "Processed", "read pairs");
+
+        // the SAM reader
+        final SamReader reader = SamReaderFactory.makeDefault().open(INPUT);
+        final PeekableIterator<SAMRecord> iterator = new PeekableIterator<SAMRecord>(reader.iterator());
+
+        // the metrics to keep track of
+        final MySeqMetrics metrics = new MySeqMetrics();
+        long basesExcludedByDupes = 0;
+        long basesExcludedByMapq = 0;
+        long basesExcludedByPairing = 0;
+        long basesExcludedByBaseq = 0;
+        long basesExcludedByOverlap = 0;
+
+        // Loop through all the loci by read pairs
+        ReadPair pairToAnalyze = getNextReadPair(iterator);
+        while (pairToAnalyze != null) {
+
+            final boolean isProperPair = (pairToAnalyze.read2 != null);
+
+            // how many bases do we have?
+            final int read1bases = pairToAnalyze.read1.getReadLength();
+            final int read2bases = isProperPair ? pairToAnalyze.read2.getReadLength() : 0;
+            final int totalReadBases = read1bases + read2bases;
+
+            // now compute metrics...
+            metrics.TOTAL_BASES += totalReadBases;
+
+            if (!isProperPair || pairToAnalyze.read1.getMateUnmappedFlag() || pairToAnalyze.read2.getMateUnmappedFlag()) {
+                basesExcludedByPairing += totalReadBases;
+            } else if (pairToAnalyze.read1.getDuplicateReadFlag()) {
+                basesExcludedByDupes += totalReadBases;
+            } else {
+
+                // determine the bad bases from the reads
+                final BaseExclusionHelper read1exclusions = determineBaseExclusions(pairToAnalyze.read1);
+                final BaseExclusionHelper read2exclusions = determineBaseExclusions(pairToAnalyze.read2);
+                basesExcludedByMapq += read1exclusions.basesExcludedByMapq + read2exclusions.basesExcludedByMapq;
+                basesExcludedByBaseq += read1exclusions.lowBQcount + read2exclusions.lowBQcount;
+
+                // keep track of the total usable bases
+                int usableBaseCount = totalReadBases;
+                usableBaseCount -= (read1exclusions.basesExcludedByMapq + read1exclusions.lowBQcount);
+                usableBaseCount -= (read2exclusions.basesExcludedByMapq + read2exclusions.lowBQcount);
+
+                // subtract out bad bases from overlaps between the reads, but only if both reads pass mapping quality thresholds
+                if (read1exclusions.basesExcludedByMapq == 0 && read2exclusions.basesExcludedByMapq == 0) {
+                    final int overlapCount = getOverlappingBaseCount(read1exclusions, read2exclusions);
+                    basesExcludedByOverlap += overlapCount;
+                    usableBaseCount -= overlapCount;
+                }
+
+                metrics.TOTAL_USABLE_BASES += usableBaseCount;
+            }
+
+            // record progress
+            progress.record(pairToAnalyze.read1);
+
+            // iterate
+            pairToAnalyze = getNextReadPair(iterator);
+        }
+
+        // finalize metrics
+        setUnusedMetrics(metrics);
+        metrics.GENOME_TERRITORY = reader.getFileHeader().getSequenceDictionary().getReferenceLength();
+        metrics.MEAN_COVERAGE = metrics.TOTAL_USABLE_BASES / (double)metrics.GENOME_TERRITORY;
+        metrics.PCT_EXC_DUPE = basesExcludedByDupes / (double)metrics.TOTAL_BASES;
+        metrics.PCT_EXC_MAPQ = basesExcludedByMapq / (double)metrics.TOTAL_BASES;
+        metrics.PCT_EXC_UNPAIRED = basesExcludedByPairing / (double)metrics.TOTAL_BASES;
+        metrics.PCT_EXC_BASEQ = basesExcludedByBaseq / (double)metrics.TOTAL_BASES;
+        metrics.PCT_EXC_OVERLAP = basesExcludedByOverlap / (double)metrics.TOTAL_BASES;
+        final double totalExcludedBases = metrics.TOTAL_BASES - metrics.TOTAL_USABLE_BASES;
+        metrics.PCT_EXC_TOTAL = totalExcludedBases / metrics.TOTAL_BASES;
+
+        final MetricsFile<MySeqMetrics, Integer> out = getMetricsFile();
+        out.addMetric(metrics);
+        out.write(OUTPUT);
+
+        return 0;
+    }
+
+    /**
+     * Get the count of low quality and/or softclip bases in the given read
+     *
+     * @param exclusions  the helper object
+     * @return non-negative int
+     */
+    private int getLowQualityOrSoftclipBaseCount(final BaseExclusionHelper exclusions) {
+        final byte[] quals = exclusions.read.getBaseQualities();
+
+        int badCount = exclusions.firstUnclippedBaseIndex + (quals.length - exclusions.firstTrailingClippedBaseIndex);
+        for (int i = exclusions.firstUnclippedBaseIndex; i < exclusions.firstTrailingClippedBaseIndex; i++) {
+            if (quals[i] < MINIMUM_BASE_QUALITY)
+                badCount++;
+        }
+        return badCount;
+    }
+
+    /**
+     * set the values of the unused metrics to -1
+     *
+     * @param metrics the metrics object
+     */
+    private void setUnusedMetrics(final MySeqMetrics metrics) {
+        metrics.SD_COVERAGE = -1;
+        metrics.MEDIAN_COVERAGE = -1;
+        metrics.MAD_COVERAGE = -1;
+        metrics.PCT_5X = -1;
+        metrics.PCT_10X = -1;
+        metrics.PCT_15X = -1;
+        metrics.PCT_20X = -1;
+        metrics.PCT_25X = -1;
+        metrics.PCT_30X = -1;
+        metrics.PCT_40X = -1;
+        metrics.PCT_50X = -1;
+        metrics.PCT_60X = -1;
+        metrics.PCT_70X = -1;
+        metrics.PCT_80X = -1;
+        metrics.PCT_90X = -1;
+        metrics.PCT_100X = -1;
+        metrics.PCT_EXC_CAPPED = -1;
+    }
+
+    /**
+     * Get the count of overlapping bases for the given reads
+     *
+     * @param read1exclusions  the 1st read exclusions
+     * @param read2exclusions  the 2nd read exclusions
+     * @return non-negative int
+     */
+    private int getOverlappingBaseCount(final BaseExclusionHelper read1exclusions, final BaseExclusionHelper read2exclusions) {
+        // make life easy by ensuring that reads come in coordinate order
+        if ( read2exclusions.read.getAlignmentStart() < read1exclusions.read.getAlignmentStart() ) {
+            return getOverlappingBaseCount(read2exclusions, read1exclusions);
+        }
+
+        // must be overlapping
+        if ( read1exclusions.read.getAlignmentEnd() < read2exclusions.read.getAlignmentStart() ||
+                !read1exclusions.read.getReferenceIndex().equals(read2exclusions.read.getReferenceIndex()) )
+            return 0;
+
+        final byte[] read1quals = read1exclusions.read.getBaseQualities();
+        final byte[] read2quals = read2exclusions.read.getBaseQualities();
+        final int indexOfOverlapInFirstRead = getOffsetAtReferencePosition(read1exclusions.read, read2exclusions.read.getAlignmentStart()) - 1;
+        final int maxPossibleOverlap = read1exclusions.firstTrailingClippedBaseIndex - indexOfOverlapInFirstRead;
+        // the overlap cannot actually be larger than the usable bases in read2
+        final int actualOverlap = Math.min(maxPossibleOverlap, read2exclusions.firstTrailingClippedBaseIndex - read2exclusions.firstUnclippedBaseIndex);
+        int numHighQualityOverlappingBases = 0;
+
+        for (int i = 0; i < actualOverlap; i++) {
+            // we count back from the end of the aligned bases (i.e. not included soft-clips) in read1 and from the front of read2
+            final int posInRead1 = read1exclusions.firstTrailingClippedBaseIndex - actualOverlap + i;
+            final int posInRead2 = read2exclusions.firstUnclippedBaseIndex + i;
+
+            // we only want to count it if they are both high quality (i.e. not already counted among bad bases)
+            if (read1quals[posInRead1] >= MINIMUM_BASE_QUALITY && read2quals[posInRead2] >= MINIMUM_BASE_QUALITY) {
+                numHighQualityOverlappingBases++;
+            }
+        }
+
+        return numHighQualityOverlappingBases;
+    }
+
+    // TODO -- delete after https://github.com/samtools/htsjdk/pull/306 is merged
+    private static int getOffsetAtReferencePosition(final SAMRecord rec, final int pos) {
+
+        int offset = 0;
+
+        if (pos != 0) {
+            for (final AlignmentBlock alignmentBlock : rec.getAlignmentBlocks()) {
+                if (CoordMath.getEnd(alignmentBlock.getReferenceStart(), alignmentBlock.getLength()) >= pos) {
+                    if (pos < alignmentBlock.getReferenceStart()) {
+                        break;
+                    } else {
+                        offset = pos - alignmentBlock.getReferenceStart() + alignmentBlock.getReadStart();
+                        break;
+                    }
+                } else {
+                    offset += alignmentBlock.getLength();
+                }
+            }
+        }
+        return offset;
+    }
+
+    /**
+     * Get the next read pair (where both have the same read name).
+     * If we encounter an unpaired read, the second read in the pair will be set to null.
+     *
+     * @param iterator iterator of reads
+     * @return ReadPair object holding the reads, or null if there are no more reads in the iterator
+     */
+    private ReadPair getNextReadPair(final PeekableIterator<SAMRecord> iterator) {
+
+        final ReadPair readPair = new ReadPair();
+        readPair.read1 = getNextUsableRead(iterator, false);
+        if (readPair.read1 == null) {
+            return null;
+        }
+
+        final SAMRecord peekedNextRead = getNextUsableRead(iterator, true);
+        if (peekedNextRead != null && peekedNextRead.getReadName().equals(readPair.read1.getReadName())) {
+            readPair.read2 = getNextUsableRead(iterator, false);
+        }
+
+        return readPair;
+    }
+
+    /**
+     * Return the next usable read in the iterator
+     *
+     * @param iterator the iterator to pull from
+     * @param justPeek if true, just peek the next usable read rather than pulling it (note: it may remove unusable reads from the iterator)
+     * @return the next read or null if none are left
+     */
+    private SAMRecord getNextUsableRead(final PeekableIterator<SAMRecord> iterator, final boolean justPeek) {
+
+        while (iterator.hasNext()) {
+            // trash the next read if it fails PF, is secondary, or is supplementary
+            final SAMRecord nextRead = iterator.peek();
+            if (nextRead.getReadFailsVendorQualityCheckFlag() || nextRead.isSecondaryOrSupplementary()) {
+                iterator.next();
+            }
+            // otherwise, return it
+            else {
+                return justPeek ? nextRead : iterator.next();
+            }
+        }
+
+        // no good reads left
+        return null;
+    }
+
+    private static class ReadPair {
+        public SAMRecord read1 = null;
+        public SAMRecord read2 = null;
+    }
+
+    /**
+     * Determine how many bases are excluded because of low mapping or base quality.
+     *
+     * @param read the read
+     * @return non-null object
+     */
+    private BaseExclusionHelper determineBaseExclusions(final SAMRecord read) {
+        final BaseExclusionHelper exclusions = new BaseExclusionHelper(read);
+
+        if (read.getMappingQuality() < MINIMUM_MAPPING_QUALITY) {
+            exclusions.basesExcludedByMapq = read.getReadLength();
+        } else {
+            exclusions.lowBQcount = getLowQualityOrSoftclipBaseCount(exclusions);
+        }
+
+        return exclusions;
+    }
+
+    private static class BaseExclusionHelper {
+        public SAMRecord read;
+        public int firstUnclippedBaseIndex;
+        public int firstTrailingClippedBaseIndex;
+        public int basesExcludedByMapq = 0;
+        public int lowBQcount = 0;
+
+        public BaseExclusionHelper(final SAMRecord read) {
+            this.read = read;
+
+            final List<CigarElement> cigarElements = read.getCigar().getCigarElements();
+            firstUnclippedBaseIndex = 0;
+            for (final CigarElement element : cigarElements) {
+                final CigarOperator op = element.getOperator();
+
+                if (op == CigarOperator.SOFT_CLIP) {
+                    firstUnclippedBaseIndex = element.getLength();
+                } else if (op != CigarOperator.HARD_CLIP) {
+                    break;
+                }
+            }
+
+            firstTrailingClippedBaseIndex = read.getReadLength();
+            for (int i = cigarElements.size() - 1; i >= 0; --i) {
+                final CigarElement element = cigarElements.get(i);
+                final CigarOperator op = element.getOperator();
+
+                if (op == CigarOperator.SOFT_CLIP) {
+                    firstTrailingClippedBaseIndex -= element.getLength();
+                } else if (op != CigarOperator.HARD_CLIP) {
+                    break;
+                }
+            }
+        }
+    }
+}
+
