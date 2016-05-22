7add4a1fa5588b4b60ca9b234c2f0a1ecf28ce08
Yossi Farjoun <farjoun@gmail.com>
2016-05-21 20:58:46 -0400

    sprucing-up ScatterIntervalsByNsTest
    fixed logging.
    
    - space added as requested

--- src/java/picard/util/ScatterIntervalsByNs.java
+++ src/java/picard/util/ScatterIntervalsByNs.java
@@ -1,5 +1,6 @@
 package picard.util;
 
+import htsjdk.samtools.util.SequenceUtil;
 import picard.cmdline.CommandLineProgram;
 import picard.cmdline.CommandLineProgramProperties;
 import picard.cmdline.programgroups.Intervals;
@@ -18,7 +19,10 @@ import htsjdk.samtools.SAMSequenceRecord;
 import htsjdk.samtools.util.StringUtil;
 
 import java.io.File;
-import java.lang.Boolean;import java.lang.Override;import java.lang.String;import java.util.Collections;
+import java.lang.Boolean;
+import java.lang.Override;
+import java.lang.String;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -38,11 +42,11 @@ import java.util.Set;
         programGroup = Intervals.class
 )
 public class ScatterIntervalsByNs extends CommandLineProgram {
-    static final String USAGE_SUMMARY = "Writes an interval list based on splitting the reference by Ns.  ";
-    static final String USAGE_DETAILS = "This tool identifies positions in the reference where the basecalls are Ns and writes out an " +
-            "interval list using the resulting coordinates (excluding the N bases). This can be used to create an interval list for " +
+    static final String USAGE_SUMMARY = "Writes an interval list based on splitting a reference by Ns.  ";
+    static final String USAGE_DETAILS = "This tool identifies positions in a reference where the bases are 'no-calls' and writes out an " +
+            "interval-list using the resulting coordinates. This can be used to create an interval list for " +
             "whole genome sequence (WGS) for e.g. scatter-gather purposes, as an alternative to using fixed-length intervals. The number " +
-            "of contiguous Ns that can be tolerated before creating a break is adjustable from the command line.<br />" +
+            "of contiguous nocalls that can be tolerated before creating a break is adjustable from the command line.<br />" +
             "<h4>Usage example:</h4>" +
             "<pre>" +
             "java -jar picard.jar ScatterIntervalsByNs \\<br />" +
@@ -64,7 +68,7 @@ public class ScatterIntervalsByNs extends CommandLineProgram {
     public int MAX_TO_MERGE = 1;
 
     //not using an enum since Interval.name is a String, and am using that to define the type of the Interval
-    static final String
+    private static final String
             ACGTmer = "ACGTmer",
             Nmer    = "Nmer";
 
@@ -74,19 +78,19 @@ public class ScatterIntervalsByNs extends CommandLineProgram {
         ACGT(ACGTmer),
         BOTH(Nmer, ACGTmer);
 
-        private final Set acceptedTypes;
+        private final Set<String> acceptedTypes;
 
         public Boolean accepts(final String string) {return acceptedTypes.contains(string);}
 
         OutputType(final String... strings) {
-            acceptedTypes = new HashSet<String>();
+            acceptedTypes = new HashSet<>();
             Collections.addAll(acceptedTypes, strings);
         }
     }
 
     private static final Log log = Log.getInstance(ScatterIntervalsByNs.class);
-    final ProgressLogger locusProgress = new ProgressLogger(log, (int) 1e7, "examined", "loci");
-    final ProgressLogger intervalProgress = new ProgressLogger(log, (int) 10, "found", "intervals");
+    private static final ProgressLogger locusProgress = new ProgressLogger(log, (int) 1e7, "examined", "loci");
+    private static final ProgressLogger intervalProgress = new ProgressLogger(log, (int) 10, "found", "intervals");
 
     public static void main(final String[] args) {
         new ScatterIntervalsByNs().instanceMainWithExit(args);
@@ -111,11 +115,7 @@ public class ScatterIntervalsByNs extends CommandLineProgram {
         final IntervalList outputIntervals = new IntervalList(intervals.getHeader().clone());
         log.info(String.format("Collecting requested type of intervals (%s)", OUTPUT_TYPE));
 
-        for (final Interval i : intervals.getIntervals()) {
-            if (OUTPUT_TYPE.accepts(i.getName())) {
-                outputIntervals.add(i);
-            }
-        }
+        intervals.getIntervals().stream().filter(i -> OUTPUT_TYPE.accepts(i.getName())).forEach(outputIntervals::add);
 
         log.info("Writing Intervals.");
         outputIntervals.write(OUTPUT);
@@ -130,8 +130,8 @@ public class ScatterIntervalsByNs extends CommandLineProgram {
      * Generate an interval list that alternates between Ns and ACGTs *
      * ****************************************************************
      */
-    public static IntervalList segregateReference(final ReferenceSequenceFile refFile, final int maxNmerToMerge) {
-        final List<Interval> preliminaryIntervals = new LinkedList<Interval>();
+    static IntervalList segregateReference(final ReferenceSequenceFile refFile, final int maxNmerToMerge) {
+        final List<Interval> preliminaryIntervals = new LinkedList<>();
         final SAMFileHeader header = new SAMFileHeader();
         header.setSequenceDictionary(refFile.getSequenceDictionary());
         header.setSortOrder(SAMFileHeader.SortOrder.coordinate);
@@ -143,11 +143,12 @@ public class ScatterIntervalsByNs extends CommandLineProgram {
             final byte[] bytes = ref.getBases();
             StringUtil.toUpperCase(bytes);
 
-            boolean nBlockIsOpen = (bytes[0] == 'N');
+            boolean nBlockIsOpen = SequenceUtil.isNoCall(bytes[0]);
             int start = 0;
 
             for (int i = 0; i < bytes.length; ++i) {
-                final boolean currentBaseIsN = (bytes[i] == 'N');
+                locusProgress.record(rec.getSequenceName(), i);
+                final boolean currentBaseIsN = SequenceUtil.isNoCall(bytes[i]);
 
                 //create intervals when switching, i.e "nBlockIsOpen" disagrees with "currentBaseIsN"
                 if (nBlockIsOpen != currentBaseIsN) {
@@ -180,7 +181,7 @@ public class ScatterIntervalsByNs extends CommandLineProgram {
             {
                 // create the new ACGTmer interval
                 final Interval temp = new Interval(
-                        preliminaryIntervals.get(0).getSequence(),
+                        preliminaryIntervals.get(0).getContig(),
                         preliminaryIntervals.get(0).getStart(),
                         preliminaryIntervals.get(2).getEnd(), false, ACGTmer);
 
@@ -191,7 +192,9 @@ public class ScatterIntervalsByNs extends CommandLineProgram {
                 //and replace them with the newly created one
                 preliminaryIntervals.add(0, temp);
             } else { //if cannot merge top three intervals, transfer the top intervals to finalIntervals
-                finalIntervals.add(preliminaryIntervals.remove(0));
+                final Interval remove = preliminaryIntervals.remove(0);
+                finalIntervals.add(remove);
+                intervalProgress.record(remove.getContig(),remove.getStart());
             }
         }
         return finalIntervals;
