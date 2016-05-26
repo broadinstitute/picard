2b32ecea5f9a05ec09fe01c1ea5ce3f2ba006818
Yossi Farjoun <farjoun@gmail.com>
2016-05-26 07:03:41 -0400

    - added coordinate-sorted tests covering bugfix

--- src/tests/java/picard/sam/markduplicates/AsIsMarkDuplicatesTester.java
+++ src/tests/java/picard/sam/markduplicates/AsIsMarkDuplicatesTester.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright (c) 2014 The Broad Institute
+ * Copyright (c) 2016 The Broad Institute
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
