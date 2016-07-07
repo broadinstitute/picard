cae7f30162074ed7815be7f876b826f00c62164d
kasont <kason@broadinstitute.org>
2016-07-07 13:11:29 -0400

    Adding informative error for SamToFastqTest. #496

--- src/test/java/picard/sam/SamToFastqTest.java
+++ src/test/java/picard/sam/SamToFastqTest.java
@@ -164,31 +164,38 @@ public class SamToFastqTest extends CommandLineProgramTest {
     }
 
     @Test(dataProvider =  "okFiles")
-    public void testOkInterleavedFile(final String samFilename) throws IOException {
-        final File samFile = new File(TEST_DATA_DIR,samFilename);
-        final File pairFile = newTempFastqFile("pair");
+    public void testOkInterleavedFile(final String samFilename) {
+        try {
+            final File samFile = new File(TEST_DATA_DIR, samFilename);
+            final File pairFile = newTempFastqFile("pair");
 
-        convertFile(new String[]{
-                "INPUT=" + samFile.getAbsolutePath(),
-                "FASTQ=" + pairFile.getAbsolutePath(),
-                "INTERLEAVE=true"
-        });
+            convertFile(new String[]{
+                    "INPUT=" + samFile.getAbsolutePath(),
+                    "FASTQ=" + pairFile.getAbsolutePath(),
+                    "INTERLEAVE=true"
+            });
 
-        final Set<String> outputHeaderSet = createFastqReadHeaderSet(pairFile);
-        // Create map of mate pairs from SAM records
-        final Map<String,MatePair> map = createSamMatePairsMap(samFile) ;
-        Assert.assertEquals(map.size() * 2, outputHeaderSet.size());
+            final Set<String> outputHeaderSet = createFastqReadHeaderSet(pairFile);
+            // Create map of mate pairs from SAM records
+            final Map<String, MatePair> map = createSamMatePairsMap(samFile);
+            Assert.assertEquals(map.size() * 2, outputHeaderSet.size());
 
-        // Ensure that each mate of each pair in SAM file is in the correct fastq pair file
-        for (final Map.Entry<String,MatePair> entry : map.entrySet() ) {
-            final MatePair mpair = entry.getValue();
-            Assert.assertNotNull(mpair.mate1); // ensure we have two mates
-            Assert.assertNotNull(mpair.mate2);
-            Assert.assertEquals(mpair.mate1.getReadName(),mpair.mate2.getReadName());
-            final String readName = mpair.mate1.getReadName() ;
-            Assert.assertTrue(outputHeaderSet.contains(readName+"/1")); // ensure mate is in correct file
-            Assert.assertTrue(outputHeaderSet.contains(readName+"/2"));
+            // Ensure that each mate of each pair in SAM file is in the correct fastq pair file
+            for (final Map.Entry<String, MatePair> entry : map.entrySet()) {
+                final MatePair mpair = entry.getValue();
+                Assert.assertNotNull(mpair.mate1); // ensure we have two mates
+                Assert.assertNotNull(mpair.mate2);
+                Assert.assertEquals(mpair.mate1.getReadName(), mpair.mate2.getReadName());
+                final String readName = mpair.mate1.getReadName();
+                Assert.assertTrue(outputHeaderSet.contains(readName + "/1")); // ensure mate is in correct file
+                Assert.assertTrue(outputHeaderSet.contains(readName + "/2"));
+            }
         }
+            catch(IOException e){
+                e.printStackTrace();
+                throw new PicardException("IOException File Not paired", e);
+
+            }
     }
 
 
