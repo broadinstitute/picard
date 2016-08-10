4fd5cae9430bc7de4de346d1c88e664381402194
kasont <kason@broadinstitute.org>
2016-08-10 09:18:53 -0400

    Kt fingerprintchecker fix (#596)
    * Removal of GenotypeReader from fingerprintChecker.
    
    * Re:Removal of GenotypeReader from fingerprintChecker.
    
    * Re:Removal of GenotypeReader from fingerprintChecker.
    
    * Re:Removal of GenotypeReader from fingerprintChecker.
    
    * Re:Removal of GenotypeReader from fingerprintChecker.
    
    * Re:Removal of GenotypeReader from fingerprintChecker.

--- src/main/java/picard/fingerprint/FingerprintChecker.java
+++ src/main/java/picard/fingerprint/FingerprintChecker.java
@@ -29,18 +29,14 @@ import htsjdk.samtools.SamReader;
 import htsjdk.samtools.SamReaderFactory;
 import htsjdk.samtools.filter.NotPrimaryAlignmentFilter;
 import htsjdk.samtools.filter.SamRecordFilter;
-import htsjdk.samtools.util.Interval;
-import htsjdk.samtools.util.IntervalList;
-import htsjdk.samtools.util.Log;
-import htsjdk.samtools.util.SamLocusIterator;
+import htsjdk.samtools.util.*;
 import htsjdk.samtools.SAMFileReader;
 import htsjdk.samtools.SAMReadGroupRecord;
-import htsjdk.samtools.util.SequenceUtil;
-import htsjdk.samtools.util.StringUtil;
 import htsjdk.variant.variantcontext.Allele;
 import htsjdk.variant.variantcontext.Genotype;
 import htsjdk.variant.variantcontext.GenotypeLikelihoods;
 import htsjdk.variant.variantcontext.VariantContext;
+import htsjdk.variant.vcf.VCFFileReader;
 import picard.PicardException;
 
 import java.io.File;
@@ -132,11 +128,11 @@ public class FingerprintChecker {
      */
     public Map<String,Fingerprint> loadFingerprints(final File fingerprintFile, final String specificSample) {
         final IntervalList loci = this.haplotypes.getIntervalList();
-        final GenotypeReader reader = new GenotypeReader();
-        final GenotypeReader.VariantIterator iterator = reader.read(fingerprintFile, loci);
+        final VCFFileReader reader = new VCFFileReader(fingerprintFile);
+        final CloseableIterator<VariantContext> iterator = reader.iterator();  
 
         SequenceUtil.assertSequenceDictionariesEqual(this.haplotypes.getHeader().getSequenceDictionary(),
-                                                     iterator.getSequenceDictionary());
+                                                        reader.getSequenceDictionary(fingerprintFile));
 
         final Map<String, Fingerprint> fingerprints = new HashMap<>();
         Set<String> samples = null;
