f5b9f505531d1b65b3eabd47a45ad14ff1681e3d
Daniel Gómez-Sánchez <magicDGS@users.noreply.github.com>
2017-02-22 16:22:57 +0100

    Use default HTSJDK method in CreateSequenceDictionary  (#724)
    * using HTSJDK equivalents for 
    * removed unused import

--- src/test/java/picard/sam/CreateSequenceDictionaryTest.java
+++ src/test/java/picard/sam/CreateSequenceDictionaryTest.java
@@ -24,7 +24,6 @@
 package picard.sam;
 
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import picard.cmdline.CommandLineProgramTest;
 import picard.PicardException;
@@ -46,23 +45,6 @@ public class CreateSequenceDictionaryTest extends CommandLineProgramTest {
 
     public String getCommandLineProgramName() {
         return CreateSequenceDictionary.class.getSimpleName();
-    }
-
-    @DataProvider
-    public Object[][] fastaNames() {
-        return new Object[][] {
-                {"break.fa", "break.dict"},
-                {"break.txt.txt", "break.txt.dict"},
-                {"break.fasta.fasta", "break.fasta.dict"},
-                {"break.fa.gz", "break.dict"},
-                {"break.txt.gz.txt.gz", "break.txt.gz.dict"},
-                {"break.fasta.gz.fasta.gz", "break.fasta.gz.dict"}
-        };
-    }
-
-    @Test(dataProvider = "fastaNames")
-    public void testGetDefaultDictionaryForReferenceSequence(final String fastaFile, final String expectedDict) throws Exception {
-        Assert.assertEquals(CreateSequenceDictionary.getDefaultDictionaryForReferenceSequence(new File(fastaFile)), new File(expectedDict));
     }
 
     @Test
