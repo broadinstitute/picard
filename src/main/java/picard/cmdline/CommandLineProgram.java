1e99fa163b4a7daec13db984e9847ada896f5fe8
Dan Moran <danmoran@broadinstitute.org>
2017-06-16 14:09:10 -0400

    Set default compression level to 1.

--- src/main/java/picard/cmdline/CommandLineProgram.java
+++ src/main/java/picard/cmdline/CommandLineProgram.java
@@ -85,7 +85,7 @@ public abstract class CommandLineProgram {
     public ValidationStringency VALIDATION_STRINGENCY = ValidationStringency.DEFAULT_STRINGENCY;
 
     @Option(doc = "Compression level for all compressed files created (e.g. BAM and GELI).", common=true)
-    public int COMPRESSION_LEVEL = BlockCompressedStreamConstants.DEFAULT_COMPRESSION_LEVEL;
+    public int COMPRESSION_LEVEL = 1; // The default IntelDeflater is slow at levels > 1
 
     @Option(doc = "When writing SAM files that need to be sorted, this will specify the number of records stored in RAM before spilling to disk. Increasing this number reduces the number of file handles needed to sort a SAM file, and increases the amount of RAM needed.", optional=true, common=true)
     public Integer MAX_RECORDS_IN_RAM = SAMFileWriterImpl.getDefaultMaxRecordsInRam();
