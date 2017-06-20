d936b99030af76cca1448f9541d6e01d3d4e9eeb
Tim Fennell <tfenne@tfenne.com>
2017-06-20 08:59:42 -0400

    Small documentation update to BedToIntervalList. (#842)

--- src/main/java/picard/util/BedToIntervalList.java
+++ src/main/java/picard/util/BedToIntervalList.java
@@ -64,9 +64,16 @@ public class BedToIntervalList extends CommandLineProgram {
             "     -Strand - Indicates +/- strand for the interval (either + or -) <br /> " +
             "     -Interval name - (Each interval should have a unique name) " +
             "</pre>" +
-            "  <br /><br />" +
-            "This tool requires sequence dictionary file (with \".dict\" extension), which can be created from a reference sequence " +
-            "using Picard's CreateSequenceDictionary tool."+
+            "<br/>" +
+            "This tool requires a sequence dictionary, provided with the SEQUENCE_DICTIONARY or SD argument. " +
+            "The value given to this argument can be any of the following:" +
+            "<pre>" +
+            "    - A file with .dict extension generated using Picard's CreateSequenceDictionaryTool</br>" +
+            "    - A reference.fa or reference.fasta file with a reference.dict in the same directory</br>" +
+            "    - Another IntervalList with @SQ lines in the header from which to generate a dictionary</br>" +
+            "    - A VCF that contains #contig lines from which to generate a sequence dictionary</br>" +
+            "    - A SAM or BAM file with @SQ lines in the header from which to generate a dictionary</br>" +
+            "</pre>" +
             "<h4>Usage example:</h4>" +
             "<pre>" +
             "java -jar picard.jar BedToIntervalList \\<br />" +
@@ -80,7 +87,8 @@ public class BedToIntervalList extends CommandLineProgram {
     @Option(shortName = StandardOptionDefinitions.INPUT_SHORT_NAME, doc = "The input BED file")
     public File INPUT;
 
-    @Option(shortName = StandardOptionDefinitions.SEQUENCE_DICTIONARY_SHORT_NAME, doc = "The sequence dictionary")
+    @Option(shortName = StandardOptionDefinitions.SEQUENCE_DICTIONARY_SHORT_NAME,
+            doc = "The sequence dictionary, or BAM/VCF/IntervalList from which a dictionary can be extracted.")
     public File SEQUENCE_DICTIONARY;
 
     @Option(shortName = StandardOptionDefinitions.OUTPUT_SHORT_NAME, doc = "The output Picard Interval List")
