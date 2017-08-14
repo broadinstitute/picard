2b3dc6c324e462611027079b3e10a0e11ca02828
jsotobroad <jsotobroad@broadinstitute.org>
2017-08-14 13:51:30 -0400

    argument instead of option

--- src/main/java/picard/sam/util/ReadOutputCommandLineProgram.java
+++ src/main/java/picard/sam/util/ReadOutputCommandLineProgram.java
@@ -1,7 +1,7 @@
 package picard.sam.util;
 
+import org.broadinstitute.barclay.argparser.Argument;
 import picard.cmdline.CommandLineProgram;
-import picard.cmdline.Option;
 
 /**
  * Abstract class that holds parameters and methods common to classes that want to add PG tags to reads in SAM/BAM files
@@ -9,6 +9,6 @@ import picard.cmdline.Option;
  */
 public abstract class ReadOutputCommandLineProgram extends CommandLineProgram {
 
-    @Option(doc = "Add PG tag to each read in a SAM or BAM", common = true)
+    @Argument(doc = "Add PG tag to each read in a SAM or BAM", common = true)
     public Boolean ADD_PG_TAG_TO_READS = true;
 }
