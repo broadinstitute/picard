fccdbdb5e226f2a721e5229eb4d9aa4ff893dfeb
Sarah Walker <skwalker@broadinstitute.org>
2017-08-30 17:50:03 -0400

    comments part 2

--- src/main/java/picard/vcf/GenotypeConcordance.java
+++ src/main/java/picard/vcf/GenotypeConcordance.java
@@ -685,7 +685,8 @@ public class GenotypeConcordance extends CommandLineProgram {
             return destination.substring(0, insertIdx) + toInsert + destination.substring(insertIdx);
         }
         else {
-            if (destination.length() > 0 && destination.charAt(destination.length()-1) == '*') {
+            // Just return the destination string if it ends on a spanning deletion, do not append to a spanning deletion
+            if (destination.length() > 0 && destination.substring(destination.length()-1).equals(Allele.SPAN_DEL_STRING)) {
                 return destination;
             } else {
                 return destination + toInsert;
