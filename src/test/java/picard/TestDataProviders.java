1b36e40c13903236a5ba6506e4a7f8c0065bea5c
Yossi Farjoun <farjoun@gmail.com>
2017-09-29 13:26:01 +0300

    - remove Containers by using @NoInjection annotation

--- src/test/java/picard/TestDataProviders.java
+++ src/test/java/picard/TestDataProviders.java
@@ -3,6 +3,7 @@ package picard;
 import org.testng.Assert;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.NoInjection;
 import org.testng.annotations.Test;
 import picard.cmdline.ClassFinder;
 
@@ -23,32 +24,15 @@ import java.util.List;
  * @author Yossi Farjoun
  */
 public class TestDataProviders {
-    private static class Container {
-        public final Method method;
-        public final Class<?> clazz;
-
-        // Encapsulation (of the Method) is required due to what seems to be a bug in TestNG.
-
-        public Container(Method method, Class clazz) {
-            this.method = method;
-            this.clazz = clazz;
-        }
-
-        @Override
-        public String toString() {
-            return clazz.getName() + "::" + method.getName();
-        }
-    }
 
     @Test
     public void IndependentTestOfDataProviderTest() throws IllegalAccessException, InvocationTargetException, InstantiationException {
         testAllDataProvidersdata();
     }
 
-
     @DataProvider(name = "DataprovidersThatDontTestThemselves")
     public Iterator<Object[]> testAllDataProvidersdata() throws IllegalAccessException, InstantiationException, InvocationTargetException {
-        int i = 0;
+
         List<Object[]> data = new ArrayList<>();
         final ClassFinder classFinder = new ClassFinder();
         classFinder.find("picard", Object.class);
@@ -57,20 +41,20 @@ public class TestDataProviders {
             if (Modifier.isAbstract(testClass.getModifiers())) continue;
             for (final Method method : testClass.getMethods()) {
                 if (method.isAnnotationPresent(DataProvider.class)) {
-                    data.add(new Object[]{new Container(method, testClass)});
+                    data.add(new Object[]{method, testClass});
                 }
             }
         }
         Assert.assertTrue(data.size() > 1);
-        Assert.assertEquals(data.stream().filter(c -> ((Container) c[0]).method.getName().equals("testAllDataProvidersdata")).count(), 1);
+        Assert.assertEquals(data.stream().filter(c -> ((Method) c[0]).getName().equals("testAllDataProvidersdata")).count(), 1);
 
         return data.iterator();
     }
 
+    // @NoInjection annotations required according to this test:
+    // https://github.com/cbeust/testng/blob/master/src/test/java/test/inject/NoInjectionTest.java
     @Test(dataProvider = "DataprovidersThatDontTestThemselves")
-    public void testDataProviderswithDP(final Container container) throws IllegalAccessException, InstantiationException, InvocationTargetException {
-        final Method method = container.method;
-        final Class clazz = container.clazz;
+    public void testDataProviderswithDP(@NoInjection final Method method, final Class clazz) throws IllegalAccessException, InstantiationException, InvocationTargetException {
         System.err.println("Method: " + method + " Class: " + clazz.getName());
 
         Object instance = clazz.newInstance();
