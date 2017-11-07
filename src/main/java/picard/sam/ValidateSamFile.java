6642cbcaf5afec8ffb09394c677960ce4109a699
Eric Banks <ebanks@broadinstitute.org>
2017-11-07 10:55:45 -0500

    Adds option to validator to skip mate validation
    As part of this, we need to update picard to use the latest htsjdk

--- src/main/java/picard/sam/ValidateSamFile.java
+++ src/main/java/picard/sam/ValidateSamFile.java
@@ -132,6 +132,13 @@ public class ValidateSamFile extends CommandLineProgram {
             "This number can be found by executing the 'ulimit -n' command on a Unix system.")
     public int MAX_OPEN_TEMP_FILES = 8000;
 
+    @Argument(shortName = "SMV",
+            doc = "If true, this tool will not attempt to validate mate information. " +
+            "In general cases, this option should not be used.  However, in cases where samples have very " +
+            "high duplication or chimerism rates (> 10%), the mate validation process often requires extremely " +
+            "large amounts of memory to run, so this flag allows you to forego that check.")
+    public boolean SKIP_MATE_VALIDATION = false;
+
     public static void main(final String[] args) {
         System.exit(new ValidateSamFile().instanceMain(args));
     }
@@ -209,6 +216,9 @@ public class ValidateSamFile extends CommandLineProgram {
                 validator.setVerbose(false, 0);
             } else {
                 validator.setVerbose(true, MAX_OUTPUT);
+            }
+            if (SKIP_MATE_VALIDATION) {
+                validator.setSkipMateValidation(SKIP_MATE_VALIDATION);
             }
             if (IS_BISULFITE_SEQUENCED) {
                 validator.setBisulfiteSequenced(IS_BISULFITE_SEQUENCED);
