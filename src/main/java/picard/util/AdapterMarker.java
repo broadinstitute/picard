e5638f433483018ac526aa2ed0628f01b090201b
jcarey <jcarey@broadinstitute.org>
2017-12-08 09:54:25 -0500

    Remove unused import

--- src/main/java/picard/util/AdapterMarker.java
+++ src/main/java/picard/util/AdapterMarker.java
@@ -35,7 +35,6 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.TreeMap;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Stream;
