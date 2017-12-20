205168306159b4e117aaa9a22a2fc3cc18641c41
Mark Fleharty <fleharty@broadinstitute.org>
2017-12-20 10:15:00 -0500

    Adding comments for UmiAwareMarkDuplicates

--- src/main/java/picard/sam/markduplicates/UmiAwareMarkDuplicatesWithMateCigar.java
+++ src/main/java/picard/sam/markduplicates/UmiAwareMarkDuplicatesWithMateCigar.java
@@ -33,10 +33,42 @@ import org.broadinstitute.barclay.argparser.Argument;
 import org.broadinstitute.barclay.argparser.CommandLineProgramProperties;
 import org.broadinstitute.barclay.help.DocumentedFeature;
 import picard.cmdline.programgroups.Alpha;
+import picard.sam.markduplicates;
 
 import java.io.File;
 
 /**
+ *
+ * <p>This tool locates and tags duplicate reads in a BAM or SAM file, where duplicate reads are
+ * defined as originating from a single fragment of DNA. It is based on the MarkDuplicatesWithMateCigar tool, with added logic
+ * to leverage Unique Molecular Identifier (UMI) information.</p>
+ * <p>In addition to assuming that all members of a duplicate set must have the same start and end position, it imposes that
+ * they must also have sufficiently similar UMIs. In this context, 'sufficiently similar' is parameterized by the command line
+ * argument MAX_EDIT_DISTANCE_TO_JOIN, which sets the edit distance between UMIs that will be considered to be part of the same
+ * original molecule. This logic allows for sequencing errors in UMIs.</p>
+ * <p> If UMIs contain dashes, the dashes will be ignored. If UMIs contain Ns, these UMIs will not contribute to UMI metrics
+ * associated with each record. If the MAX_EDIT_DISTANCE_TO_JOIN allows, UMIs with Ns will be included in the duplicate set and
+ * the UMI metrics associated with each duplicate set. Ns are counted as an edit distance from other bases {ATCG}, but are not
+ * considered different from each other.</p>
+ * <p>This tool is NOT intended to be used on data without UMIs; for marking duplicates in non-UMI data, see {@link MarkDuplicates} or
+ * {@link MarkDuplicatesWithMateCigar}. Mixed data (where some reads have UMIs and others do not) is not supported.</p>
+ *
+ * <p>Note also that this tool will not work with alignments that have large gaps or deletions, such as those from RNA-seq data.
+ * This is due to the need to buffer small genomic windows to ensure integrity of the duplicate marking, while large skips
+ * (ex. skipping introns) in the alignment records would force making that window very large, thus exhausting memory. </p>
+ *
+ * <p>Note: Metrics labeled as percentages are actually expressed as fractions!</p>
+ * <h4>Usage example:</h4>
+ * <pre>
+ *
+ * java -jar picard.jar UmiAwareMarkDuplicatesWithMateCigar \\<br />
+ *      I=input.bam \\<br />
+ *      O=output.bam \\<br />
+ *      M=output_duplicate_metrics.txt \\<br />
+ *      UMI_METRICS=output_umi_metrics.txt
+ * </pre>
+ * <hr />
+ *
  * This is a simple tool to mark duplicates making use of UMIs in the reads.
  *
  * It makes use of the fact that duplicate sets with UMIs can be broken up into subsets based on
@@ -44,7 +76,7 @@ import java.io.File;
  * for UMIs that are different but within a given edit distance to be considered to be part of the
  * same duplicate set.
  *
- * Users should continue to use MarkDuplicates in general, the main motivation for this tool is to provide a way to
+ * Users should continue to use {@link MarkDuplicates} in general, the main motivation for this tool is to provide a way to
  * mark duplicates using information from UMIs.
  *
  * @author fleharty
@@ -81,9 +113,9 @@ public class UmiAwareMarkDuplicatesWithMateCigar extends SimpleMarkDuplicatesWit
             "" +
             "java -jar picard.jar UmiAwareMarkDuplicatesWithMateCigar \\<br />" +
             "      I=input.bam \\<br />" +
-            "      O=umi_aware_mark_dups_w_mate_cig.bam \\<br />" +
-            "      M=umi_aware_mark_dups_w_mate_cig_duplicate_metrics.txt \\<br />" +
-            "      UMI_METRICS=umi_aware_mark_dups_w_mate_cig_umi_metrics.txt" +
+            "      O=output.bam \\<br />" +
+            "      M=output_duplicate_metrics.txt \\<br />" +
+            "      UMI_METRICS=output_umi_metrics.txt" +
             "</pre>" +
             "<hr />";
 
