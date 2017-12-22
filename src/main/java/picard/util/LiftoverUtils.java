c9b71d07a3954f533b675c62ac5ba50e49617c9c
Eric Banks <ebanks@broadinstitute.org>
2017-12-22 11:59:48 -0500

    Fixes for LiftoverVcf
    1. Fixing the list arguments in LiftoverVcf so that they can be altered
    
    2. Fix bug in liftover where reverse complement failures die unnecessarily

--- src/main/java/picard/util/LiftoverUtils.java
+++ src/main/java/picard/util/LiftoverUtils.java
@@ -118,11 +118,11 @@ public class LiftoverUtils {
             return null;
         }
 
-        List<Allele> origAlleles = new ArrayList<>(source.getAlleles());
-        VariantContextBuilder vcb = new VariantContextBuilder(source);
+        final List<Allele> origAlleles = new ArrayList<>(source.getAlleles());
+        final VariantContextBuilder vcb = new VariantContextBuilder(source);
         vcb.chr(target.getContig());
 
-        // By convention, indels as left aligned and include the base prior to that indel.
+        // By convention, indels are left aligned and include the base prior to that indel.
         // When reverse complementing, will be necessary to include this additional base.
         // This check prevents the presumably rare situation in which the indel occurs on the first position of the sequence
         final boolean addToStart = source.isIndel() && target.getStart() > 1;
@@ -134,7 +134,13 @@ public class LiftoverUtils {
         vcb.stop(stop);
 
         vcb.alleles(reverseComplementAlleles(origAlleles, target, refSeq, source.isIndel(), addToStart));
+
         if (source.isIndel()) {
+            // check that the reverse complemented bases match the new reference
+            final String refString = StringUtil.bytesToString(refSeq.getBases(), start - 1, stop - start + 1);
+            if (!referenceAlleleMatchesReference(vcb.getAlleles(), refString)) {
+                return null;
+            }
             leftAlignVariant(vcb, start, stop, vcb.getAlleles(), refSeq);
         }
 
@@ -293,6 +299,17 @@ public class LiftoverUtils {
         return swappedBuilder.make();
     }
 
+    /**
+     * Checks whether the reference allele in the provided variant context actually matches the reference sequence
+     *
+     * @param alleles list of alleles from which to find the reference allele
+     * @param referenceSequence the ref sequence
+     * @return true if they match, false otherwise
+     */
+    protected static boolean referenceAlleleMatchesReference(final List<Allele> alleles, final String referenceSequence) {
+        final Allele refAllele = alleles.stream().filter(Allele::isReference).findAny().orElse(null);
+        return (refAllele != null && referenceSequence.equalsIgnoreCase(refAllele.getBaseString()));
+    }
 
     /**
      *    Normalizes and left aligns a {@link VariantContextBuilder}.
@@ -312,14 +329,13 @@ public class LiftoverUtils {
         int theStart = start;
         int theEnd = end;
 
-        final String refString = StringUtil.bytesToString(referenceSequence.getBases(), start - 1, end - start + 1);
         //make sure that referenceAllele matches reference
-        final Allele refAllele = alleles.stream().filter(Allele::isReference).findAny().orElseThrow(() -> new RuntimeException("How did that happen? No reference allele?"));
-
-        if (!refString.equalsIgnoreCase(refAllele.getBaseString())) {
+        final String refString = StringUtil.bytesToString(referenceSequence.getBases(), start - 1, end - start + 1);
+        if (!referenceAlleleMatchesReference(alleles, refString)) {
+            final Allele refAllele = alleles.stream().filter(Allele::isReference).findAny().orElse(null);
             throw new IllegalArgumentException(
                     String.format("Reference allele doesn't match reference: Allele= %s, at %s:%d-%d, ref=%s",
-                            refAllele.toString(), referenceSequence.getName(), start, end, refString));
+                            refAllele == null ? "null" : refAllele.toString(), referenceSequence.getName(), start, end, refString));
         }
 
         // 1. while changes in alleles do
--- src/main/java/picard/vcf/LiftoverVcf.java
+++ src/main/java/picard/vcf/LiftoverVcf.java
@@ -110,10 +110,10 @@ public class LiftoverVcf extends CommandLineProgram {
 
     @Argument(doc = "INFO field annotations that behave like an Allele Frequency and should be transformed with x->1-x " +
             "when swapping reference with variant alleles.", optional = true)
-    public Collection<String> TAGS_TO_REVERSE = LiftoverUtils.DEFAULT_TAGS_TO_REVERSE;
+    public Collection<String> TAGS_TO_REVERSE = new ArrayList<>(LiftoverUtils.DEFAULT_TAGS_TO_REVERSE);
 
     @Argument(doc = "INFO field annotations that should be deleted when swapping reference with variant alleles.", optional = true)
-    public Collection<String> TAGS_TO_DROP = LiftoverUtils.DEFAULT_TAGS_TO_DROP;
+    public Collection<String> TAGS_TO_DROP = new ArrayList<>(LiftoverUtils.DEFAULT_TAGS_TO_DROP);
 
     // When a contig used in the chain is not in the reference, exit with this value instead of 0.
     public static int EXIT_CODE_WHEN_CONTIG_NOT_IN_REFERENCE = 1;
@@ -310,9 +310,8 @@ public class LiftoverVcf extends CommandLineProgram {
                 refSeq = refSeqs.get(target.getContig());
 
                 final VariantContext liftedVC = LiftoverUtils.liftVariant(ctx, target, refSeq, WRITE_ORIGINAL_POSITION);
-                if (liftedVC == null) {
-                    throw new IllegalArgumentException("Unexpectedly found null VC. This should have not happened.");
-                } else {
+                // the liftedVC can be null if the liftover fails for various reasons
+                if (liftedVC != null) {
                     tryToAddVariant(liftedVC, refSeq, ctx);
                 }
             }
