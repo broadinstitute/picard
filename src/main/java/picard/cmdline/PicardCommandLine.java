287790dc347089d20b31fe9b20d617857609b39e
SathemBite <toni.lesnikov.98@mail.ru>
2018-01-24 22:35:52 +0300

    Added dispaying information about beta and experimental picard instruments(#1)

--- src/main/java/picard/cmdline/PicardCommandLine.java
+++ src/main/java/picard/cmdline/PicardCommandLine.java
@@ -2,8 +2,10 @@ package picard.cmdline;
 
 import htsjdk.samtools.util.Log;
 import htsjdk.samtools.util.StringUtil;
+import org.broadinstitute.barclay.argparser.BetaFeature;
 import org.broadinstitute.barclay.argparser.CommandLineProgramGroup;
 import org.broadinstitute.barclay.argparser.CommandLineProgramProperties;
+import org.broadinstitute.barclay.argparser.ExperimentalFeature;
 
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
@@ -55,7 +57,7 @@ import java.util.stream.Collectors;
  */
 public class PicardCommandLine {
     private static final Log log = Log.getInstance(PicardCommandLine.class);
-    
+
     private static String initializeColor(final String color) {
         if (CommandLineDefaults.COLOR_STATUS) return color;
         else return "";
@@ -75,6 +77,10 @@ public class PicardCommandLine {
 
     /** The name of this unified command line program **/
     private final static String COMMAND_LINE_NAME = PicardCommandLine.class.getSimpleName();
+
+    /** Prefixes for class that annotated by @ExperimentalFeature and @BetaFeature **/
+    private final static String BETA_PREFIX = "**BETA FEATURE - WORK IN PROGRESS** ";
+    private final static String EXPERIMENTAL_PREFIX = "**EXPERIMENTAL FEATURE - USE AT YOUR OWN RISK** ";
 
     /** The packages we wish to include in our command line **/
     protected static List<String> getPackageList() {
@@ -258,9 +264,9 @@ public class PicardCommandLine {
                 }
                 if (!commandListOnly) {
                     if (clazz.getSimpleName().length() >= 45) {
-                        builder.append(String.format("%s    %s    %s%s%s\n", KGRN, clazz.getSimpleName(), KCYN, property.oneLineSummary(), KNRM));
+                        builder.append(String.format("%s    %s    %s%s%s%s\n", KGRN, clazz.getSimpleName(), KCYN, getFeaturePrefix(clazz), property.oneLineSummary(), KNRM));
                     } else {
-                        builder.append(String.format("%s    %-45s%s%s%s\n", KGRN, clazz.getSimpleName(), KCYN, property.oneLineSummary(), KNRM));
+                        builder.append(String.format("%s    %-45s%s%s%s\n", KGRN, clazz.getSimpleName(), KCYN, getFeaturePrefix(clazz), property.oneLineSummary(), KNRM));
                     }
                 }
                 else {
@@ -277,6 +283,20 @@ public class PicardCommandLine {
             System.err.print(builder.toString());
         }
     }
+
+    public static String getFeaturePrefix(Class clazz){
+        if (clazz.getAnnotation(ExperimentalFeature.class) != null){
+            return EXPERIMENTAL_PREFIX;
+        }
+
+        if (clazz.getAnnotation(BetaFeature.class) != null){
+            return BETA_PREFIX;
+        }
+
+        return "";
+    }
+
+
 
     /** similarity floor for matching in printUnknown **/
     private final static int HELP_SIMILARITY_FLOOR = 7;
