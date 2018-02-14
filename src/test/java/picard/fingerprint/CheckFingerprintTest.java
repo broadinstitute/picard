a977f7b9629c695a05067cb3b6a3084137b55b9b
Yossi Farjoun <farjoun@gmail.com>
2018-02-13 20:52:40 -0500

    typo rebasing.

--- src/test/java/picard/fingerprint/CheckFingerprintTest.java
+++ src/test/java/picard/fingerprint/CheckFingerprintTest.java
@@ -20,7 +20,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-amtools.SAMException;
 
 public class CheckFingerprintTest extends CommandLineProgramTest {
 
