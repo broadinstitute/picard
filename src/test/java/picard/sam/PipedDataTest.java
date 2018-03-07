cfbf36b7460a8a22217e5df80e8d1e30223f632a
tlangs <tlangs@broadinstitute.org>
2018-03-07 18:16:19 -0500

    Actually using pipes now

--- src/test/java/picard/sam/PipedDataTest.java
+++ src/test/java/picard/sam/PipedDataTest.java
@@ -1,17 +1,8 @@
 package picard.sam;
 
-import htsjdk.samtools.*;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import picard.cmdline.CommandLineProgramTest;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.*;
 
 public class PipedDataTest extends CommandLineProgramTest {
 
@@ -21,43 +12,40 @@ public class PipedDataTest extends CommandLineProgramTest {
         return SortSam.class.getSimpleName();
     }
 
-    private static final File BAM = new File("testdata/picard/sam/test.bam");
-
-
     @Test
-    public void testPipedData() throws IOException, InterruptedException, ExecutionException {
+    public void testPipedData() {
 
-        ExecutorService exec = Executors.newCachedThreadPool();
+        String classPath = "\"" + System.getProperty("java.class.path") + "\" ";
 
-        Path tmpFile = Files.createTempFile("sortSamPipeTest", "tmp");
-        Path tmpOutputFile = Files.createTempFile("sortSamPipeTest", "output");
-
-        // Read the sam file and do some simple operation
-        Callable<Integer> readSam = () -> {
-            SamReader reader = SamReaderFactory.makeDefault().open(BAM);
-            final SAMFileWriter outputSam = new SAMFileWriterFactory().makeBAMWriter(reader.getFileHeader(),
-                    true, tmpFile);
-            for (SAMRecord record : reader) {
-                record.setReadName(record.getReadName().toUpperCase());
-                outputSam.addAlignment(record);
-            }
-            outputSam.close();
-            return 0;
+        String[] readCommand = {
+                "/bin/bash",
+                "-c",
+                "java -classpath " +
+                        classPath +
+                        "picard.cmdline.PicardCommandLine " +
+                        "ViewSam " +
+                        "I=testdata/picard/sam/test.bam " +
+                        "ALIGNMENT_STATUS=All " +
+                        "PF_STATUS=All " +
+                        "| " +
+                        "java -classpath " +
+                        classPath +
+                        "picard.cmdline.PicardCommandLine " +
+                        "SortSam " +
+                        "I=/dev/stdin " +
+                        "O=/dev/null " +
+                        "SORT_ORDER=queryname"
         };
+        try {
+            ProcessBuilder readProcess = new ProcessBuilder(readCommand);
+            readProcess.inheritIO();
 
-        // Read the sam file output by htsjdk tool.
-        Callable<Integer> sortSam = () -> {
-            final String[] args = new String[]{
-                    "INPUT=" + tmpFile.toAbsolutePath().toString(),
-                    "OUTPUT=" + tmpOutputFile.toAbsolutePath().toString(),
-                    "SORT_ORDER=queryname"
-            };
-            return runPicardCommandLine(args);
-        };
+            Process read = readProcess.start();
+            Assert.assertEquals(read.waitFor(), 0);
 
-        // Run them at the same time to simulate piping and waiting for EOF
-        List<Future<Integer>> futures = exec.invokeAll(Arrays.asList(readSam, sortSam));
+        } catch (Exception e) {
+            Assert.fail("Failed to pipe data from htsjdk to picard", e);
+        }
 
-        Assert.assertEquals(futures.get(1).get(), new Integer(0));
     }
 }
