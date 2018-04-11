b08f0b248c2dbabe747d77f4279f205874d138d2
tlangs <tlangs@broadinstitute.org>
2018-04-11 14:10:44 -0400

    Better bash

--- build_push_docker.sh
+++ build_push_docker.sh
@@ -10,17 +10,20 @@ fi
 
 declare -r TAG=${1}
 
+declare -r PICARD_TAG=broadinstitute/picard:${TAG}
+declare -r PICARD_CLOUD_TAG=us.gcr.io/broad-gotc-prod/picard-cloud:${TAG}
+
 echo "Will build and push the following docker images:"
-echo "broadinstitute/picard:${TAG}"
-echo "broadinstitute/picard-cloud:${TAG}"
+echo ${PICARD_TAG}
+echo ${PICARD_CLOUD_TAG}
 
 read -p "Is this really what you want to do? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    docker build -t broadinstitute/picard:${TAG} --build-arg build_command=shadowJar --build-arg jar_name=picard.jar .
-    docker build -t us.gcr.io/broad-gotc-prod/picard-cloud:${TAG} --build-arg build_command=cloudJar --build-arg jar_name=picardcloud.jar .
+    docker build -t ${PICARD_TAG} --build-arg build_command=shadowJar --build-arg jar_name=picard.jar .
+    docker build -t ${PICARD_CLOUD_TAG} --build-arg build_command=cloudJar --build-arg jar_name=picardcloud.jar .
 
-    docker push broadinstitute/picard:${TAG}
-    docker push us.gcr.io/broad-gotc-prod/picard-cloud:${TAG}
+    docker push ${PICARD_TAG}
+    gcloud docker -- push ${PICARD_CLOUD_TAG}
 fi
