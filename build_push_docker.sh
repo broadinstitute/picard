ec5b63052d3e213375ae131db2ca7dec09e7ca7c
tlangs <tlangs@broadinstitute.org>
2018-04-11 14:01:18 -0400

    Push picard-cloud to gcr

--- build_push_docker.sh
+++ build_push_docker.sh
@@ -19,9 +19,8 @@ echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     docker build -t broadinstitute/picard:${TAG} --build-arg build_command=shadowJar --build-arg jar_name=picard.jar .
-    docker build -t broadinstitute/picard-cloud:${TAG} --build-arg build_command=cloudJar --build-arg jar_name=picardcloud.jar .
+    docker build -t us.gcr.io/broad-gotc-prod/picard-cloud:${TAG} --build-arg build_command=cloudJar --build-arg jar_name=picardcloud.jar .
 
     docker push broadinstitute/picard:${TAG}
-    docker push broadinstitute/picard-cloud:${TAG}
+    docker push us.gcr.io/broad-gotc-prod/picard-cloud:${TAG}
 fi
-
