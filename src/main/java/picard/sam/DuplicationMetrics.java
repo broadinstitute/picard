9d3945723b52f2f55a2d01001f37f0f427251c22
Yossi Farjoun <farjoun@broadinstitute.org>
2018-04-12 09:52:09 -0400

    Yf fix infinite loop md (#1147)
    - white-space changes and comments
    - added a failing test (with the example from github #1146)
    - removed '=' sign from while loop. Once equality to zero is achieved, it's good enough for the bisection method.
    - responding to review comments

--- src/main/java/picard/sam/DuplicationMetrics.java
+++ src/main/java/picard/sam/DuplicationMetrics.java
@@ -32,7 +32,9 @@ import picard.analysis.MergeableMetricBase;
  * within a stream of SAMRecords.
  */
 public class DuplicationMetrics extends MergeableMetricBase {
-    /** The library on which the duplicate marking was performed. */
+    /**
+     * The library on which the duplicate marking was performed.
+     */
     @MergeByAssertEquals
     public String LIBRARY;
 
@@ -43,23 +45,33 @@ public class DuplicationMetrics extends MergeableMetricBase {
     @MergeByAdding
     public long UNPAIRED_READS_EXAMINED;
 
-    /** The number of mapped read pairs examined. (Primary, non-supplemental) */
+    /**
+     * The number of mapped read pairs examined. (Primary, non-supplemental)
+     */
     @MergeByAdding
     public long READ_PAIRS_EXAMINED;
 
-    /** The number of reads that were either secondary or supplementary */
+    /**
+     * The number of reads that were either secondary or supplementary
+     */
     @MergeByAdding
     public long SECONDARY_OR_SUPPLEMENTARY_RDS;
 
-    /** The total number of unmapped reads examined. (Primary, non-supplemental) */
+    /**
+     * The total number of unmapped reads examined. (Primary, non-supplemental)
+     */
     @MergeByAdding
     public long UNMAPPED_READS;
 
-    /** The number of fragments that were marked as duplicates. */
+    /**
+     * The number of fragments that were marked as duplicates.
+     */
     @MergeByAdding
     public long UNPAIRED_READ_DUPLICATES;
 
-    /** The number of read pairs that were marked as duplicates. */
+    /**
+     * The number of read pairs that were marked as duplicates.
+     */
     @MergeByAdding
     public long READ_PAIR_DUPLICATES;
 
@@ -70,11 +82,15 @@ public class DuplicationMetrics extends MergeableMetricBase {
     @MergeByAdding
     public long READ_PAIR_OPTICAL_DUPLICATES;
 
-    /** The fraction of mapped sequence that is marked as duplicate. */
+    /**
+     * The fraction of mapped sequence that is marked as duplicate.
+     */
     @NoMergingIsDerived
     public Double PERCENT_DUPLICATION;
 
-    /** The estimated number of unique molecules in the library based on PE duplication. */
+    /**
+     * The estimated number of unique molecules in the library based on PE duplication.
+     */
     @NoMergingIsDerived
     public Long ESTIMATED_LIBRARY_SIZE;
 
@@ -87,13 +103,13 @@ public class DuplicationMetrics extends MergeableMetricBase {
         this.ESTIMATED_LIBRARY_SIZE = estimateLibrarySize(this.READ_PAIRS_EXAMINED - this.READ_PAIR_OPTICAL_DUPLICATES,
                 this.READ_PAIRS_EXAMINED - this.READ_PAIR_DUPLICATES);
 
-        PERCENT_DUPLICATION = (UNPAIRED_READ_DUPLICATES + READ_PAIR_DUPLICATES *2) /(double) (UNPAIRED_READS_EXAMINED + READ_PAIRS_EXAMINED *2);
+        PERCENT_DUPLICATION = (UNPAIRED_READ_DUPLICATES + READ_PAIR_DUPLICATES * 2) / (double) (UNPAIRED_READS_EXAMINED + READ_PAIRS_EXAMINED * 2);
     }
 
     /**
      * Fills in the ESTIMATED_LIBRARY_SIZE based on the paired read data examined where
      * possible and the PERCENT_DUPLICATION.
-     *
+     * <p>
      * Deprecated, use {@link #calculateDerivedFields()} instead.
      */
     @Deprecated
@@ -104,49 +120,56 @@ public class DuplicationMetrics extends MergeableMetricBase {
     /**
      * Estimates the size of a library based on the number of paired end molecules observed
      * and the number of unique pairs observed.
-     *
+     * <p>
      * Based on the Lander-Waterman equation that states:
-     *     C/X = 1 - exp( -N/X )
+     * C/X = 1 - exp( -N/X )
      * where
-     *     X = number of distinct molecules in library
-     *     N = number of read pairs
-     *     C = number of distinct fragments observed in read pairs
+     * X = number of distinct molecules in library
+     * N = number of read pairs
+     * C = number of distinct fragments observed in read pairs
      */
     public static Long estimateLibrarySize(final long readPairs, final long uniqueReadPairs) {
         final long readPairDuplicates = readPairs - uniqueReadPairs;
 
         if (readPairs > 0 && readPairDuplicates > 0) {
-            long n = readPairs;
-            long c = uniqueReadPairs;
 
-            double m = 1.0, M = 100.0;
+            double m = 1.0;
+            double M = 100.0;
 
-            if (c >= n || f(m*c, c, n) < 0) {
+            if (uniqueReadPairs >= readPairs || f(m * uniqueReadPairs, uniqueReadPairs, readPairs) < 0) {
                 throw new IllegalStateException("Invalid values for pairs and unique pairs: "
-                        + n + ", " + c);
-
+                        + readPairs + ", " + uniqueReadPairs);
             }
 
-            while( f(M*c, c, n) >= 0 ) M *= 10.0;
-
-            for (int i=0; i<40; i++ ) {
-                double r = (m+M)/2.0;
-                double u = f( r * c, c, n );
-                if ( u == 0 ) break;
-                else if ( u > 0 ) m = r;
-                else if ( u < 0 ) M = r;
+            // find value of M, large enough to act as other side for bisection method
+            while (f(M * uniqueReadPairs, uniqueReadPairs, readPairs) > 0) {
+                M *= 10.0;
             }
 
-            return (long) (c * (m+M)/2.0);
-        }
-        else {
+            // use bisection method (no more than 40 times) to find solution
+            for (int i = 0; i < 40; i++) {
+                double r = (m + M) / 2.0;
+                double u = f(r * uniqueReadPairs, uniqueReadPairs, readPairs);
+                if (u == 0) {
+                    break;
+                } else if (u > 0) {
+                    m = r;
+                } else if (u < 0) {
+                    M = r;
+                }
+            }
+
+            return (long) (uniqueReadPairs * (m + M) / 2.0);
+        } else {
             return null;
         }
     }
 
-    /** Method that is used in the computation of estimated library size. */
+    /**
+     * Method that is used in the computation of estimated library size.
+     */
     private static double f(double x, double c, double n) {
-        return c/x - 1 + Math.exp(-n/x);
+        return c / x - 1 + Math.exp(-n / x);
     }
 
     /**
@@ -154,14 +177,14 @@ public class DuplicationMetrics extends MergeableMetricBase {
      * x higher coverage than the observed coverage.
      *
      * @param estimatedLibrarySize the estimated number of molecules in the library
-     * @param x the multiple of sequencing to be simulated (i.e. how many X sequencing)
-     * @param pairs the number of pairs observed in the actual sequencing
-     * @param uniquePairs the number of unique pairs observed in the actual sequencing
+     * @param x                    the multiple of sequencing to be simulated (i.e. how many X sequencing)
+     * @param pairs                the number of pairs observed in the actual sequencing
+     * @param uniquePairs          the number of unique pairs observed in the actual sequencing
      * @return a number z <= x that estimates if you had pairs*x as your sequencing then you
-     *         would observe uniquePairs*z unique pairs.
+     * would observe uniquePairs*z unique pairs.
      */
     public static double estimateRoi(long estimatedLibrarySize, double x, long pairs, long uniquePairs) {
-        return estimatedLibrarySize * ( 1 - Math.exp(-(x*pairs)/estimatedLibrarySize) ) / uniquePairs;
+        return estimatedLibrarySize * (1 - Math.exp(-(x * pairs) / estimatedLibrarySize)) / uniquePairs;
     }
 
     /**
@@ -172,15 +195,18 @@ public class DuplicationMetrics extends MergeableMetricBase {
         if (ESTIMATED_LIBRARY_SIZE == null) {
             try {
                 calculateDerivedFields();
-                if (ESTIMATED_LIBRARY_SIZE == null) return null;
+                if (ESTIMATED_LIBRARY_SIZE == null) {
+                    return null;
+                }
+            } catch (IllegalStateException ise) {
+                return null;
             }
-            catch (IllegalStateException ise) { return null; }
         }
 
         long uniquePairs = READ_PAIRS_EXAMINED - READ_PAIR_DUPLICATES;
-        Histogram<Double> histo = new Histogram<Double>();
+        Histogram<Double> histo = new Histogram<>();
 
-        for (double x=1; x<=100; x+=1) {
+        for (double x = 1; x <= 100; x += 1) {
             histo.increment(x, estimateRoi(ESTIMATED_LIBRARY_SIZE, x, READ_PAIRS_EXAMINED, uniquePairs));
         }
 
@@ -191,7 +217,7 @@ public class DuplicationMetrics extends MergeableMetricBase {
     // Usage = DuplicationMetrics READ_PAIRS READ_PAIR_DUPLICATES
     public static void main(String[] args) {
         DuplicationMetrics m = new DuplicationMetrics();
-        m.READ_PAIRS_EXAMINED  = Integer.parseInt(args[0]);
+        m.READ_PAIRS_EXAMINED = Integer.parseInt(args[0]);
         m.READ_PAIR_DUPLICATES = Integer.parseInt(args[1]);
         m.calculateDerivedFields();
         System.out.println("Percent Duplication: " + m.PERCENT_DUPLICATION);
