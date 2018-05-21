ed0838ee224c55eb0f9f434a9a082222f7eb0eb8
Yossi Farjoun <farjoun@broadinstitute.org>
2018-05-21 13:36:55 -0400

    - TIL how to make @links more pretty!

--- src/main/java/picard/analysis/MergeableMetricBase.java
+++ src/main/java/picard/analysis/MergeableMetricBase.java
@@ -39,30 +39,31 @@ import java.util.Collection;
 import java.util.List;
 
 /**
- * An extension of MetricBase that knows how to merge-by-adding fields that are appropriately annotated ({@link MergeByAdding}). It also provides an interface
- * for calculating derived fields {@link #calculateDerivedFields()}  (and an annotation that informs that said fields are derived {@link NoMergingIsDerived}). Finally, it also allows for an annotation
- * that suggests that a field will be used as an ID and thus merging will simply ensure that these fields are equal {@link MergeByAssertEquals}. Other annotations are available,
- * though they all currently imply that no <it>implicit</it> action will be taken {@link MergingIsManual} and {@link NoMergingKeepsValue}.
+ * An extension of MetricBase that knows how to merge-by-adding fields that are appropriately annotated ({@link MergeByAdding MergeByAdding}). It also provides an interface
+ * for calculating derived fields {@link #calculateDerivedFields}  (and an annotation that informs that said fields are derived {@link NoMergingIsDerived NoMergingIsDerived}). Finally, it also allows for an annotation
+ * that suggests that a field will be used as an ID and thus merging will simply ensure that these fields are equal {@link MergeByAssertEquals MergeByAssertEquals}. Other annotations are available,
+ * though they all currently imply that no <it>implicit</it> action will be taken {@link MergingIsManual MergingIsManual} and {@link NoMergingKeepsValue NoMergingKeepsValue}.
  *<p>
- * {@link MergeByAdding} is only enabled for the following types: int, Integer, float, Float, double, Double, short, Short, long, Long, byte, Byte.
+ * {@link MergeByAdding MergeByAdding} is only enabled for the following types: int, Integer, float, Float, double, Double, short, Short, long, Long, byte, Byte.
  * Overflow will be detected (for the short, and byte types) and an exception thrown.
- *<p><
- * Every (non-static) field in this class <it>must</it> be @Annotated by one of: {@link MergeByAdding}, {@link MergeByAssertEquals}, {@link NoMergingIsDerived}, {@link MergingIsManual}, {@link NoMergingKeepsValue}.
- * Static fields may be annotated, but not with {@link MergeByAdding}, there will be no automatic modification of static fields
+ *<p>
+ * Every (non-static) field in this class <it>must</it> be @Annotated by one of: {@link MergeByAdding MergeByAdding},
+ * {@link MergeByAssertEquals MergeByAssertEquals}, {@link NoMergingIsDerived NoMergingIsDerived}, {@link MergingIsManual MergingIsManual}, {@link NoMergingKeepsValue NoMergingKeepsValue}.
+ * Static fields may be annotated, but not with {@link MergeByAdding MergeByAdding}, there will be no automatic modification of static fields
  *
  * <dl>
- * <dt>{@link MergeByAdding}</dt>
+ * <dt>{@link MergeByAdding MergeByAdding}</dt>
  * <dd>When merging another metric into this one, the value of the field in the other class will be added to the value in this.
  * This will happen automatically!</dd>
- * <dt>{@link MergeByAssertEquals}</dt>
+ * <dt>{@link MergeByAssertEquals MergeByAssertEquals}</dt>
  * <dd>When merging another metric into this one, the code will assert that value of the field in the other class is equal to the value in this.</dd>
- * <dt>{@link NoMergingIsDerived}</dt>
+ * <dt>{@link NoMergingIsDerived NoMergingIsDerived}</dt>
  * <dd>When merging another metric into this one, no action will be taken since the value of this field should be derived from the other field.
  * This derivation should happen in the "calculateDerivedFields" method.</dd>
- * <dt>{@link MergingIsManual}</dt>
+ * <dt>{@link MergingIsManual MergingIsManual}</dt>
  * <dd>When merging another metric into this one, the resulting value will be calculated "manually", i.e. by custom code in the merge method.
  * The resulting value can depend on both this and other objects.</dd>
- * <dt>{@link NoMergingKeepsValue}</dt>
+ * <dt>{@link NoMergingKeepsValue NoMergingKeepsValue}</dt>
  * <dd>When merging another metric into this one, the value of the field in this remains unchanged by design.</dd>
  * </dl>
  *
@@ -262,9 +263,9 @@ abstract public class MergeableMetricBase extends MetricBase {
     }
 
     /**
-     * placeholder method that will calculate the derived fields from the other ones. classes that are derived from non-trivial base classes
-     * should consider calling super.calculateDerivedFields() as well. Fields whose value will change due to this method should be
-     * annotated with {@link NoMergingIsDerived}.
+     * Placeholder method that will calculate the derived fields from the other ones.
+     * Classes that are derived from non-trivial derived classes should consider calling super.calculateDerivedFields() as well.
+     * Fields whose value will change due to this method should be annotated with {@link NoMergingIsDerived NoMergingKeepsValue}.
      */
     public void calculateDerivedFields() {}
 }
