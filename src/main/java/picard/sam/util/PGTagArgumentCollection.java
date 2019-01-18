20ae078301d3979acb19fcada7e165cff291ab2f
Louis Bergelson <louisb@broadinstitute.org>
2019-01-18 10:46:29 -0500

    changing ADD_PG_TAG_TO_READ to boolean (#1262)
    * changing ADD_PG_TAG_TO_READ to boolean
    
    * PGTagArgumentCollection.ADD_PG_TAG_TO_READS was a Boolean which
    allowed it to be null, changing it to a boolean primitive
    * this fixes https://github.com/broadinstitute/picard/issues/1261

--- src/main/java/picard/sam/util/PGTagArgumentCollection.java
+++ src/main/java/picard/sam/util/PGTagArgumentCollection.java
@@ -8,6 +8,6 @@ import org.broadinstitute.barclay.argparser.Argument;
 public class PGTagArgumentCollection {
 
     @Argument(doc = "Add PG tag to each read in a SAM or BAM", common = true)
-    public Boolean ADD_PG_TAG_TO_READS = true;
+    public boolean ADD_PG_TAG_TO_READS = true;
 
 }
