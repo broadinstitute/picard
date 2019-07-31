d8166ac2f484ae5657f1edf1a9002e8f89334719
George Grant <ggrant@broadinstitute.org>
2019-07-31 08:49:50 -0400

    CreateVerifyIDIntensityContaminationMetricsFile A little program to create a standard picard metrics file from the output of VerifyIDIntensity.

---
+++ src/main/java/picard/arrays/VerifyIDIntensityContaminationMetrics.java
@@ -0,0 +1,17 @@
+package picard.arrays;
+
+import htsjdk.samtools.metrics.MetricBase;
+
+public class VerifyIDIntensityContaminationMetrics extends MetricBase {
+    /** The ID of this entry */
+    public int ID;
+
+    /** The percent mixture (contamination) of the sample for ID */
+    public double PCT_MIX;
+
+    /** The log likelihood */
+    public long LLK;
+
+    /** The log likelihood 0 */
+    public long LLK0;
+}
