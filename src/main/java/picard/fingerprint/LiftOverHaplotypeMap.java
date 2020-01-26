621f8055bca87ce98aeb481c90a7abd172fa1bf4
Yossi Farjoun <farjoun@broadinstitute.org>
2020-01-26 15:05:42 +0200

    copyright and finals

--- src/main/java/picard/fingerprint/LiftOverHaplotypeMap.java
+++ src/main/java/picard/fingerprint/LiftOverHaplotypeMap.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright (c) 2017 The Broad Institute
+ * Copyright (c) 2020 The Broad Institute
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -45,17 +45,14 @@ import java.io.File;
 /**
  * Liftover SNPs in HaplotypeMaps from one reference to another
  *
- * @author Tim Fennel
  */
 @CommandLineProgramProperties(
         summary = "Lifts over a haplotype database from one reference to another. Based on UCSC liftOver.\n" +
-                " Uses a UCSC chain file to guid the liftOver.",
+                " Uses a UCSC chain file to guide the liftOver.",
         oneLineSummary = "Lifts over a haplotype database from one reference to another",
         programGroup = Fingerprinting.class
 )
 public class LiftOverHaplotypeMap extends CommandLineProgram {
-
-    // Usage and parameters
 
     @Argument(doc = "Haplotype database to be lifted over.", shortName = StandardOptionDefinitions.INPUT_SHORT_NAME)
     public File INPUT;
@@ -63,16 +60,12 @@ public class LiftOverHaplotypeMap extends CommandLineProgram {
     @Argument(doc = "Where to write lifted-over haplotype database.", shortName = StandardOptionDefinitions.OUTPUT_SHORT_NAME)
     public File OUTPUT;
 
-    @Argument(doc = "Sequence dictionary to write into the output haplotype database.",
+    @Argument(doc = "Sequence dictionary to write into the output haplotype database. (Any file from which a dictionary is extractable.)",
             shortName = StandardOptionDefinitions.SEQUENCE_DICTIONARY_SHORT_NAME)
     public File SEQUENCE_DICTIONARY;
 
-    @Argument(doc = "Chain file that guides LiftOver.")
+    @Argument(doc = "Chain file that guides LiftOver. (UCSC format)")
     public File CHAIN;
-
-    public static void main(final String[] argv) {
-        new LiftOverHaplotypeMap().instanceMainWithExit(argv);
-    }
 
     public static final int LIFTOVER_FAILED_FOR_ONE_OR_MORE_SNPS = 101;
 
@@ -89,18 +82,18 @@ public class LiftOverHaplotypeMap extends CommandLineProgram {
 
         final HaplotypeMap fromMap = new HaplotypeMap(INPUT);
 
-        SAMSequenceDictionary dict = SAMSequenceDictionaryExtractor.extractDictionary(SEQUENCE_DICTIONARY);
+        final SAMSequenceDictionary dict = SAMSequenceDictionaryExtractor.extractDictionary(SEQUENCE_DICTIONARY);
 
         liftOver.validateToSequences(dict);
-        SAMFileHeader newHeader = new SAMFileHeader(dict);
+        final SAMFileHeader newHeader = new SAMFileHeader(dict);
         final HaplotypeMap toMap = new HaplotypeMap(newHeader);
 
         boolean anyFailed = false;
 
         for (final HaplotypeBlock fromHaplotypeBlock : fromMap.getHaplotypes()) {
-            HaplotypeBlock toHaplotypeBlock = new HaplotypeBlock(fromHaplotypeBlock.getMaf());
+            final HaplotypeBlock toHaplotypeBlock = new HaplotypeBlock(fromHaplotypeBlock.getMaf());
 
-            for (Snp snp : fromHaplotypeBlock.getSnps()) {
+            for (final Snp snp : fromHaplotypeBlock.getSnps()) {
                 final Interval interval = liftOver.liftOver(
                         new Interval(snp.getChrom(), snp.getPos(), snp.getPos()));
                 if (interval != null) {
