89c0122bc1e31b4665a27f2e1e3a23fe38c19307
Nils Homer <nilshomer@gmail.com>
2020-02-28 16:03:47 -0700

    Make the VCF option in CollectSamErrorMetrics optional.

--- src/main/java/picard/sam/SamErrorMetric/CollectSamErrorMetrics.java
+++ src/main/java/picard/sam/SamErrorMetric/CollectSamErrorMetrics.java
@@ -145,7 +145,7 @@ public class CollectSamErrorMetrics extends CommandLineProgram {
     public ReadBaseStratification.Stratifier STRATIFIER_VALUE;
 
     @Argument(shortName = "V", doc = "VCF of known variation for sample. program will skip over polymorphic sites in this VCF and " +
-            "avoid collecting data on these loci.")
+            "avoid collecting data on these loci.", optional = true)
     public String VCF;
 
     @Argument(shortName = "L", doc = "Region(s) to limit analysis to. Supported formats are VCF or interval_list. Will intersect inputs if multiple are given. ", optional = true)
@@ -180,7 +180,7 @@ public class CollectSamErrorMetrics extends CommandLineProgram {
     @Argument(
             fullName = "INTERVAL_ITERATOR",
             doc = "Iterate through the file assuming it consists of a pre-created subset interval of the full genome.  " +
-                    "This enables fast processing of files with reads at disperate parts of the genome.  " +
+                    "This enables fast processing of files with reads at disparate parts of the genome.  " +
                     "Requires that the provided VCF file is indexed. ",
             optional = true
     )
@@ -274,7 +274,10 @@ public class CollectSamErrorMetrics extends CommandLineProgram {
      * Otherwise, will initialize the {@link #vcfIterator}.
      */
     private void initializeVcfDataSource() throws IOException {
-        if ( INTERVAL_ITERATOR ) {
+        if ( VCF == null ) {
+            vcfIterator = new PeekableIterator<>(Collections.emptyIterator());
+        }
+        else if ( INTERVAL_ITERATOR ) {
             vcfFileReader = new VCFFileReader(IOUtil.getPath(VCF), true);
             // Make sure we can query our file for interval mode:
             if (!vcfFileReader.isQueryable()) {
@@ -282,8 +285,7 @@ public class CollectSamErrorMetrics extends CommandLineProgram {
             }
         }
         else {
-            vcfIterator = new PeekableIterator<>(
-                    VCF == null ? Collections.emptyIterator() : new VCFFileReader(IOUtil.getPath(VCF), true).iterator());
+            vcfIterator = new PeekableIterator<>(new VCFFileReader(IOUtil.getPath(VCF), true).iterator());
         }
     }
 
@@ -470,7 +472,7 @@ public class CollectSamErrorMetrics extends CommandLineProgram {
         // Make sure we can read our files:
         try {
             IOUtil.assertFileIsReadable(IOUtil.getPath(INPUT));
-            IOUtil.assertFileIsReadable(IOUtil.getPath(VCF));
+            if (VCF != null) IOUtil.assertFileIsReadable(IOUtil.getPath(VCF));
             IOUtil.assertFileIsReadable(REFERENCE_SEQUENCE);
             IOUtil.assertFilesAreReadable(INTERVALS);
         }
