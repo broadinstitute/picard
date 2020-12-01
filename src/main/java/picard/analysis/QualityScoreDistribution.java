3578fc05b4e079bcf2e34ecc6f42e75b57bcddbb
sophiacrennan <crennan.s@husky.neu.edu>
2020-12-01 17:42:15 -0500

    fix a few if statements

--- src/main/java/picard/analysis/QualityScoreDistribution.java
+++ src/main/java/picard/analysis/QualityScoreDistribution.java
@@ -100,7 +100,7 @@ public class QualityScoreDistribution extends SinglePassSamProgram {
 
     @Override
     protected String[] customCommandLineValidation() {
-        if (checkRInstallation(CHART_OUTPUT != null)) {
+        if (!checkRInstallation(CHART_OUTPUT != null)) {
             return new String[]{"R is not installed on this machine. It is required for creating the chart."};
         }
         return super.customCommandLineValidation();
