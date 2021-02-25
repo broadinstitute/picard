60b803a8085f6ee58e493c8ea39fc418463173e1
Christopher Kachulis <ckachuli@broadinstitute.org>
2021-02-25 10:45:22 -0500

    add tool

---
+++ src/main/java/picard/fingerprint/ConvertHaplotypeDatabaseToVcf.java
@@ -0,0 +1,43 @@
+package picard.fingerprint;
+
+import org.broadinstitute.barclay.argparser.Argument;
+import org.broadinstitute.barclay.argparser.CommandLineProgramProperties;
+import picard.PicardException;
+import picard.cmdline.CommandLineProgram;
+import picard.cmdline.StandardOptionDefinitions;
+import picard.cmdline.programgroups.DiagnosticsAndQCProgramGroup;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+@CommandLineProgramProperties(
+        summary = "Convert Haplotype database file to vcf",
+        oneLineSummary = "Convert Haplotype database file to vcf",
+        programGroup = DiagnosticsAndQCProgramGroup.class
+)
+public class ConvertHaplotypeDatabaseToVcf extends CommandLineProgram {
+
+    @Argument(doc = "Haplotype database to be converted to VCF.", shortName = StandardOptionDefinitions.INPUT_SHORT_NAME)
+    public File INPUT;
+
+    @Argument(doc = "Where to write converted haplotype database VCF.", shortName = StandardOptionDefinitions.OUTPUT_SHORT_NAME)
+    public File OUTPUT;
+
+    @Override
+    protected boolean requiresReference() {
+        return true;
+    }
+
+    @Override
+    protected int doWork() {
+        final HaplotypeMap haplotypeMap = new HaplotypeMap(INPUT);
+
+        try {
+            haplotypeMap.writeAsVcf(OUTPUT, REFERENCE_SEQUENCE);
+        } catch (final FileNotFoundException ex) {
+            throw new PicardException("uggh", ex);
+        }
+
+        return 0;
+    }
+}
