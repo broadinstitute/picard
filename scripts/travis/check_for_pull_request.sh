2d12e0f9dae5175954c69d2c9740c9d0ede29dfc
jessicaway <jway@broadinstitute.org>
2021-04-01 14:46:34 -0400

    revert

--- scripts/travis/check_for_pull_request.sh
+++ scripts/travis/check_for_pull_request.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 
 #query github for information about a specific pull request
-PULL_REQUESTS=$(curl -v -H "Authorization: token $GITHUB_API_TOKEN" https://api.github.com/repos/broadinstitute/picard/pulls\?state\=open\&head="broadinstitute:${TRAVIS_BRANCH}")
-if [[ $( grep -c "commits" <<< ${PULL_REQUESTS} ) -gt 0 ]]; then
-  echo "pull requests non-empty"
-  return 0
-else
-  echo "pull requests empty"
-  return 1
-fi
+curl -v -H "Authorization: token $GITHUB_API_TOKEN" https://api.github.com/repos/broadinstitute/gatk/pulls\?state\=open\&head="broadinstitute:${TRAVIS_BRANCH}"
