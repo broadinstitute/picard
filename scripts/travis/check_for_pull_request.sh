18756657634da6c086f3e6eedb85ac181c5ccaf9
jessicaway <jway@broadinstitute.org>
2021-04-01 14:30:40 -0400

    add some logging to the bash

--- scripts/travis/check_for_pull_request.sh
+++ scripts/travis/check_for_pull_request.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
 #query github for information about a specific pull request
-curl -v -H "Authorization: token $GITHUB_API_TOKEN" https://api.github.com/repos/broadinstitute/picard/pulls\?state\=open\&head="broadinstitute:${TRAVIS_BRANCH}"
+PULL_REQUESTS=$(curl -v -H "Authorization: token $GITHUB_API_TOKEN" https://api.github.com/repos/broadinstitute/picard/pulls\?state\=open\&head="broadinstitute:${TRAVIS_BRANCH}")
+if [[ $( grep -c "commits" <<< ${PULL_REQUESTS} ) -gt 0 ]]; then
+  echo "pull requests non-empty"
+  return 0
+else
+  echo "pull requests empty"
+  return 1
+fi
