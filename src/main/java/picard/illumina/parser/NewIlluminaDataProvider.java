61d72e4683787072a5562693b9ceb99f2da710da
jay Carey <jay@fulcrumgenomics.com>
2021-04-28 14:03:43 -0400

    Re-expand imports.

--- src/main/java/picard/illumina/parser/NewIlluminaDataProvider.java
+++ src/main/java/picard/illumina/parser/NewIlluminaDataProvider.java
@@ -8,7 +8,13 @@ import picard.illumina.parser.readers.CbclReader;
 import picard.illumina.parser.readers.LocsFileReader;
 
 import java.io.File;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.TreeSet;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
