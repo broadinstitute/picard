1f9e9be08cf4bd7ad30cd05797674f016a939872
rickymagner <81349869+rickymagner@users.noreply.github.com>
2023-12-14 18:47:05 -0500

    Add flag to keep zero length intervals when converting bed -> interval_list

--- src/main/java/picard/util/BedToIntervalList.java
+++ src/main/java/picard/util/BedToIntervalList.java
@@ -107,9 +107,13 @@ public class BedToIntervalList extends CommandLineProgram {
     @Argument(doc = "If true, entries that are on contig-names that are missing from the provided dictionary will be dropped.")
     public boolean DROP_MISSING_CONTIGS = false;
 
+    @Argument(doc = "If true, write length zero intervals in input bed file to resulting interval list file.")
+    public boolean KEEP_ZERO_LENGTH_INTERVALS = false;
+
     private final Log LOG = Log.getInstance(getClass());
     private int missingIntervals = 0;
     private int missingRegion = 0;
+    private int zeroLengthIntervals = 0;
 
     @Override
     protected int doWork() {
@@ -168,8 +172,18 @@ public class BedToIntervalList extends CommandLineProgram {
                 }
 
                 final boolean isNegativeStrand = bedFeature.getStrand() == Strand.NEGATIVE;
-                final Interval interval = new Interval(sequenceName, start, end, isNegativeStrand, name);
-                intervalList.add(interval);
+
+                // Use end+1 since bed start gets shifted by 1 using 1-based coordinates
+                if ((start == end+1) && !KEEP_ZERO_LENGTH_INTERVALS) {
+                    LOG.info(String.format("Skipping writing zero length interval at %s:%d-%d.", sequenceName, start, end));
+                } else {
+                    final Interval interval = new Interval(sequenceName, start, end, isNegativeStrand, name);
+                    intervalList.add(interval);
+                }
+
+                if (start == end+1) {
+                    zeroLengthIntervals++;
+                }
 
                 progressLogger.record(sequenceName, start);
             }
@@ -182,6 +196,19 @@ public class BedToIntervalList extends CommandLineProgram {
                     LOG.warn(String.format("There were %d missing regions with a total of %d bases", missingIntervals, missingRegion));
                 }
             }
+
+            if (!KEEP_ZERO_LENGTH_INTERVALS) {
+                if (zeroLengthIntervals == 0) {
+                    LOG.info("No input regions had length zero, so none were skipped.");
+                } else {
+                    LOG.info(String.format("Skipped writing a total of %d entries with length zero in the input file.", zeroLengthIntervals));
+                }
+            } else {
+                if (zeroLengthIntervals > 0) {
+                    LOG.warn(String.format("Input file had %d entries with length zero. Run with the KEEP_ZERO_LENGTH_INTERVALS flag set to false to remove these.", zeroLengthIntervals));
+                }
+            }
+
             // Sort and write the output
             IntervalList out = intervalList;
             if (SORT) {
