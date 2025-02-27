6537367e10e65c040745ae73320cc7c0b088b4d4
Chris Kachulis <ckachuli@broadinstitute.org>
2025-02-27 09:43:22 -0500

    make headers ArrayList

--- src/main/java/picard/vcf/MergeVcfs.java
+++ src/main/java/picard/vcf/MergeVcfs.java
@@ -52,6 +52,7 @@ import picard.nio.PicardHtsPath;
 
 import java.io.File;
 import java.nio.file.Path;
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
@@ -170,7 +171,7 @@ public class MergeVcfs extends CommandLineProgram {
         final List<Path> inputPaths = INPUT.stream().map(PicardHtsPath::toPath).collect(Collectors.toList());
         final List<Path> unrolledPaths = IOUtil.unrollPaths(inputPaths, FileExtensions.VCF_LIST.toArray(new String[]{}));
         final Collection<CloseableIterator<VariantContext>> iteratorCollection = new ArrayList<>(unrolledPaths.size());
-        final Collection<VCFHeader> headers = new HashSet<>(unrolledPaths.size());
+        final Collection<VCFHeader> headers = new ArrayList<>(unrolledPaths.size());
         VariantContextComparator variantContextComparator = null;
         SAMSequenceDictionary sequenceDictionary = null;
 
