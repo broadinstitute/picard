408a2be55dc6dd28cea17deb9d1507a219b1e017
Yossi Farjoun <yossi@fulcrumgenomics.com>
2026-02-03 12:27:23 -0500

    clarify usage

--- src/main/java/picard/sam/SamErrorMetric/ReadBaseStratification.java
+++ src/main/java/picard/sam/SamErrorMetric/ReadBaseStratification.java
@@ -848,7 +848,7 @@ public class ReadBaseStratification {
         BINNED_CYCLE(() -> binnedReadCycleStratifier, "The binned machine cycle. Similar to CYCLE, but binned into 5 evenly spaced ranges across the size of the read.  This stratifier may produce confusing results when used on datasets with variable sized reads."),
         INSERT_END_DISTANCE(() -> baseInsertEndDistanceStratifier, "Distance from the nearest insert end for FR read pairs. " +
                 "Negative if closer to read 2's 3' end, e.g., 150x2 reads with 100bp overlap will have values (R1) 51, .., 100, -100, .., -51 for the overlapping region. " +
-                "Overlapping bases get matching values. As it is designed for use with OVERLAPPING_ERROR, values in non-overlapping region are unspecified. Ignores non-FR orientations, unpaired, or chimeric reads."),
+                "Overlapping bases get matching values. As it is designed for use with OVERLAPPING_ERROR, value in non-overlapping region is unspecified, and non-FR orientations, unpaired, and chimeric reads are ignored."),
         SOFT_CLIPS(() -> softClipsLengthStratifier, "The number of softclipped bases the read has."),
         INSERT_LENGTH(() -> insertLengthStratifier, "The insert-size they came from (taken from the TLEN field.)"),
         BASE_QUALITY(() -> baseQualityStratifier, "The base quality."),
